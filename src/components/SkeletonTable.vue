8ad23b6db6e42e35c02f742b00fb261964c4f246
Adam Richie-Halford <richiehalford@gmail.com>
2023-01-11 11:42:05 -0800

    Add SkeletonTable.vue TrialResult.vue

---
+++ src/components/SkeletonTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <DataTable :value="skeletonData" responsiveLayout="scroll" :scrollable="true" scrollHeight="flex">
+    <Column
+      v-for="col of Object.keys(skeletonData[0])"
+      :field="col"
+      :key="col"
+    >
+      <template #header>
+        <Skeleton></Skeleton>
+      </template>
+      <template #body>
+        <Skeleton></Skeleton>
+      </template>
+    </Column>
+  </DataTable>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const skeletonData = ref([
+  {col0: '', col1: '', col2: '', col3: ''},
+  {col0: '', col1: '', col2: '', col3: ''},
+  {col0: '', col1: '', col2: '', col3: ''},
+  {col0: '', col1: '', col2: '', col3: ''},
+]);
+</script>
