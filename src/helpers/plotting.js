e52f978dc918db267b8f783252b42a7f9307f7dd
tonyamur <83736178+tonyamur@users.noreply.github.com>
2023-02-01 21:18:28 -0800

    WIP: first draft of 3 graphs

--- src/helpers/plotting.js
+++ src/helpers/plotting.js
@@ -1,1 +1,35 @@
-export const chart = {};
+
+const getGlobalChartConfig = (scores) => ({
+    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    description: 'default settings to be used for all charts',
+    "title": {"anchor": "middle", "fontSize":22},
+    data: {
+      values: scores,
+    },
+    /*
+    title: {"text": "ROAR Score Distribution by Grade Level", "anchor": "middle", "fontSize":22},
+    width: 600,
+    height: 200,    */
+  });
+  
+  
+
+  export const getDistributionByGrade = (scores) => {
+    return {
+        ...getGlobalChartConfig(scores),
+        description: 'ROAR Score Distribution by Grade Level',
+        title: {"text": "ROAR Score Distribution by Grade Level", },
+        "transform": [
+            {"calculate": "100 * (datum.theta +5)", "as": "swr_score"},
+            //{"filter": "datum.swr_score > 60"}
+            ],
+        mark: 'bar',
+        encoding: {
+            row: { field: "grade" },
+            // thetaEstimate should be changed to ROAR score
+            x: { bin: true, field: 'thetaEstimate' },
+            y: { aggregate: 'count' },
+            color: { field: 'grade' },
+        },
+        };
+  }
--- src/pages/ScoreReport.vue
+++ src/pages/ScoreReport.vue
@@ -1,9 +1,12 @@
 <template>
   <h1>{{ scoreStore.taskId }}</h1>
   <p>This is an introductory paragraph. Score reports are amazing!</p>
-  <div id="distribution-by-grade"></div>
+  //<div id="distribution-by-grade"></div>
   <p>This is another exquisitely written paragraph!</p>
   <div id="normed-percentile-distribution"></div>
+  <p>Yet more amazing explanation!</p>
+  <div id="stacked-support-by-grade"></div>
+
 </template>
 
 <script setup>
@@ -14,21 +17,6 @@ import { getDistributionByGrade } from "@/helpers/plotting.js"
 
 const scoreStore = useScoreStore();
 
-/*
-const normedPercentileDistribution = {
-  ...globalChartConfig,
-  description: 'Distribution of Normed Percentiles (all grades)',
-  
-  mark: 'bar',
-
-  encoding: {
-    // thetaEstimate should be changed to percentile
-    x: { bin: true, field: 'thetaEstimate'},
-    y: { aggregate: 'count' },
-  },
-}
-*/
-
 const debugGlobalChartConfig =  {
     $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
     description: 'default settings to be used for all charts',
@@ -38,15 +26,16 @@ const debugGlobalChartConfig =  {
 
   };
   
-  
 
   const debugDistributionByGrade = {
-        ...debugGlobalChartConfig,
+        //...debugGlobalChartConfig,
         description: 'ROAR Score Distribution by Grade Level',
         title: {"text": "ROAR Score Distribution by Grade Level", "anchor": "middle","fontSize":24},
         "height": 75,
         "width": 600,
-        
+        data: {
+              values: scoreStore.scores,
+              },
         "transform": [
             {"calculate": "100 * (datum.thetaEstimate +5)", "as": "swr_score"},
             //{"filter": "datum.swr_score > 60"}
@@ -62,10 +51,81 @@ const debugGlobalChartConfig =  {
         
   };
 
+
+
+// TODO look at the colors in this example: https://vega.github.io/vega-lite/examples/bar_heatlane.html
+const debugNormedPercentileDistribution = {
+  //...debugGlobalChartConfig,
+  description: 'Distribution of Normed Percentiles (all grades)',
+  title: {"text": "Distribution of Woodcock-Johnson Equivalent Percentiles", "anchor": "middle","fontSize":24},
+  "height": 200,
+  "width": 600,
+  data: {values: scoreStore.scores, },
+  "transform": [
+            //TODO replace fake calculation with real percent conversion
+            {"calculate": "100 * (datum.thetaEstimate +4)/8", "as": "swr_percentile"}, 
+            ],
+  mark: 'bar',
+
+  encoding: {
+    // thetaEstimate should be changed to percentile
+    x: { bin: true, 
+         field: 'swr_percentile', 
+         "title": "Percentile (relative to national norms)",
+         "scale": {"domain": [0,100]},
+         //"bin": {"maxbins": 10, "minstep":5},},
+         //"bin": {"divide":[5,2]},
+         "bin": {"step": 5},
+
+    },
+    y: { aggregate: 'count', "title": "count of students" },    
+    //"color": {"condition": {"test": "datum['swr_score'] < 500", "value": "black"},"value": "red"},
+
+  },
+};
+
+const debugStackedSupportByGrade = {
+/*
+  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+  "data": {"url": "data/barley.json"},
+  "mark": "bar",
+  "encoding": {
+    "x": {"aggregate": "sum", "field": "yield"},
+    "y": {"field": "variety"},
+    "color": {"field": "site"}
+  }
+  */
+
+  description: 'Distribution of Support Classificaiton by Grade Level',
+        title: {"text": "Distribution of Support Classificaiton by Grade Level", "anchor": "middle","fontSize":24},
+        "height": 75,
+        "width": 600,
+        data: {values: scoreStore.scores,},
+        "transform": [
+            {"calculate": "100 * (datum.thetaEstimate +5)", "as": "swr_score"},
+            //{"filter": "datum.swr_score > 60"}
+            ],
+        
+        mark: 'bar',
+        encoding: {
+            //row: { field: "grade" },
+            x: { aggregate: 'count', "title": "# of students" },
+            y: { bin: true, field: 'grade', "title": "grade", }, 
+            
+            //"x": {"aggregate": "grade", /*"field": "support_level"*/},
+            //color: { field: 'grade' },
+            //"color": {"condition": {"test": "datum['swr_score'] < 500", "value": "black"},"value": "red"},
+
+        },
+
+  }
+
 const draw = async () => {
   await embed('#distribution-by-grade', debugDistributionByGrade);
+  await embed('#normed-percentile-distribution', debugNormedPercentileDistribution);
+  await embed('#stacked-support-by-grade', debugStackedSupportByGrade);
+
   //await embed('#distribution-by-grade', getDistributionByGrade(scoreStore.scores));
-  //await embed('#normed-percentile-distribution', normedPercentileDistribution);
 };
 
 onMounted(() => {
