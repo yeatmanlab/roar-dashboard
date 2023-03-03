02782e05b169f929a219fcaf6ea4efbe1aeec973
tonyamur <83736178+tonyamur@users.noreply.github.com>
2023-03-03 10:01:38 -0800

    WIP: Added ROAR-PA report with text and graphs

--- src/components/reports/ReportPA.vue
+++ src/components/reports/ReportPA.vue
@@ -1,134 +1,21 @@
 <template>
-  <div id="phonological-awareness-score-report" class="section level1">
-    <h1>Phonological Awareness Score Report</h1>
-    <div id="introduction-to-the-phonological-awareness-score-report" class="section level2">
-      <h2>Introduction to the Phonological Awareness Score Report</h2>
-      <p>
-        <strong><em>ROAR - Phonological Awareness</em></strong> measures
-        <strong>elision and sound matching</strong> to assess a student's
-        phonological awareness. Students need to achieve mastery of phonological
-        awareness in order to achieve fluency in reading. Research shows that
-        these foundational pre-reading skills are a barrier to learning for many
-        students. Students not receiving support for their foundational reading
-        skills may struggle to catch up in overall reading ability. This
-        assessment is intended to help identify target skills for students who
-        need extra support.
-      </p>
-      <p>
-        <strong>This score report</strong> provides information about the
-        distribution of scores across each grade, the skills each student has
-        mastered, and how each student's skills compare to the national norms
-        for their age group.
-      </p>
-      <p>
-        <strong>The assessment</strong> provides a window into three specific
-        phonological subskills where students may need support: first-sound
-        matching, last-sound matching, and deletion. Students flagged as needing
-        support in one of these subskills will likely benefit from direct
-        instruction and practice with the flagged subskill and related
-        phonological skills. The test is composed of three sections:
-      </p>
-      <ul>
-        <li>
-          <p>
-            <strong>First Sound Matching:</strong> A picture is shown, and the
-            narrator says the word associated with the picture, e.g.
-            <em>“bat.”</em> Next, three more pictures are shown and narrated,
-            e.g. <em>“car, ball, fox.”</em> To answer correctly, the student
-            clicks on the picture that shares the same first sound, e.g.
-            <em>“ball.”</em>
-          </p>
-        </li>
-        <li>
-          <p>
-            <strong>Last Sound Matching:</strong> A picture is shown, and the
-            narrator says the word associated with the picture,
-            e.g. <em>“mat.”</em> Next, three more pictures are shown and
-            narrated, e.g. <em>““mouse, hat, ball”</em>. To answer correctly,
-            the student clicks on the picture that shares the same first sound,
-            e.g. <em>“hat.”</em>
-          </p>
-        </li>
-        <li>
-          <p>
-            <strong>Deletion:</strong> Three pictures are shown, and the
-            narrator asks the student to choose the picture that corresponds to
-            the word described by a deletion, e.g. <em>“What is rainbow without
-              bow?”</em> To answer correctly, the student would choose the picture
-            of rain. In this test, the narrator does not narrate the answer
-            pictures.
-          </p>
-        </li>
-      </ul>
-      <p>The entire assessment takes most students 15-20 minutes.</p>
-    </div>
-    <div id="overview" class="section level2">
-      <h2>Overview</h2>
-      <p>
-        This score report presents information from the recent administration of
-        the ROAR assessment to students in x during x.
-      </p>
-      <ul>
-        <li>59 students completed the Phonological Awareness task.</li>
-        <li>The average age of the student was 9.4.</li>
-        <li>The grades ranged from grade 1 to grade 6.</li>
-        <li>
-          The average ROAR score was 38 (out of 57) (min = 9, max = 56 , sd = 12.46).
-        </li>
-      </ul>
-      <div id="viz-distribution-by-grade"></div>
-    </div>
-
-    <div id="supporting-students-in-developing-phonological-awareness" class="section level2">
-      <h2>Supporting students in developing phonological awareness</h2>
-      <p>
-        This task is comprised of three skills related to phonological
-        awareness: <strong>First Sound Matching (FSM)</strong>, <strong>Last
-          Sound Matching (LSM)</strong>, and <strong>Deletion (DEL).</strong>
-      </p>
-      <p>
-        We have classified students into three categories, based on whether
-        they have <em>Full Mastery</em>, <em>Began to Exhibit Full Mastery</em>,
-        <em>Some Mastery,</em> or <em>No Mastery</em> of these skills.
-      </p>
-      <p>More Specifically:</p>
-      <ul>
-        <li>
-          <p>
-            <em>Full Mastery</em>: Students who have exhibited mastery of all
-            three of these skills.
-          </p>
-        </li>
-        <li>
-          <p>
-            <em>Beginning to Exhibit Full Mastery</em>: Students who are
-            beginning to exhibit mastery of all three skills.
-          </p>
-        </li>
-        <li>
-          <p>
-            <em>Some Mastery</em>: Students who who have demonstrated mastery of
-            one or two of these skills
-          </p>
-        </li>
-        <li>
-          <p>
-            <em>No Mastery</em>: Students who have not yet demonstrated mastery
-            of any of these skills.
-          </p>
-        </li>
-      </ul>
-      <div id="viz-normed-percentile-distribution"></div>
-    </div>
+  <div v-if="scoreStore.scoresReady">
+    <VueShowdown
+      :vue-template="true"
+      :vue-template-data="{ ...scoreStoreRefs }"
+      :markdown="markdownText"
+    />
   </div>
+  <AppSpinner v-else />
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
-import embed from 'vega-embed';
-import marked from 'marked';
+import { onMounted } from "vue";
+import embed from "vega-embed";
 import { useScoreStore } from "@/store/scores";
-import * as markdown from "@/components/reportMarkdownPA";
+import { storeToRefs } from "pinia";
+import markdownText from "@/assets/markdown/reportPA.md?raw";
+import TableRoarScores from "./TableRoarScores.vue";
 
 const scoreStore = useScoreStore();
 const globalChartConfig = {
@@ -137,6 +24,308 @@ const globalChartConfig = {
     values: scoreStore.scores,
   },
 }
+const distributionByGradePA = {
+  // ...globalChartConfig,
+  description: "ROAR Score Distribution by Grade Level",
+  title: { text: "ROAR PA Score Distribution", anchor: "middle", fontSize: 18 },
+  config: { view: { stroke: "#000000", strokeWidth: 1 } },
+  data: { values: scoreStore.scores },
+
+  mark: "bar",
+  height: 50,
+  width: 500,
+
+  encoding: {
+    facet: {
+      field: "grade",
+      type: "nominal",
+      columns: 1,
+      title: "By Grade",
+      header: {
+        titleColor: "navy",
+        titleFontSize: 12,
+        titleAlign: "top",
+        titleAnchor: "middle",
+        labelColor: "navy",
+        labelFontSize: 10,
+        labelFontStyle: "bold",
+        labelAnchor: "middle",
+        labelAngle: 0,
+        labelAlign: "left",
+        labelOrient: "left",
+        labelExpr:
+          "join(['Grade ',if(datum.value == 'Kindergarten', 'K', datum.value ), ], '')",
+        //sort: ['Kindergarten',1,2,3,4,5,6,7,8,9,10,11,12],
+        //sort: "ascending",
+      },
+      sort: ["Kindergarten", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+
+      spacing: 7,
+    },
+    color: {
+      field: "grade",
+      type: "ordinal",
+      sort: ["Kindergarten", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      legend: null,
+    },
+    // thetaEstimate should be changed to ROAR score
+    x: {
+      bin: true,
+      field: "roarTotalCorrect",
+      title: "ROAR PA Total Items Correct (out of 57)",
+      bin: { step: 5 },
+    },
+    y: { aggregate: "count", title: "count", axis: { orient: "right" } },
+  },
+};
+
+const stackedSkillByGradePA = {
+  description: "Distribution of Skill Classification by Grade Level",
+  title: {
+    text: "Distribution of Skill Classification by Grade Level",
+    anchor: "middle",
+    fontSize: 18,
+  },
+  height: 200,
+  width: 600,
+  data: { values: scoreStore.scores },
+  transform: [
+    {
+      calculate:
+        "indexof(['No Mastery', 'Some Mastery', 'Beginning to Exhibit Full Mastery', 'Full Mastery'], datum.skillSummary)",
+      as: "order",
+    },
+  ],
+  mark: "bar",
+  encoding: {
+    x: { aggregate: "count", title: "# of students", axis: { tickMinStep: 5,  } },
+    y: {
+      bin: false,
+      type: "ordinal",
+      field: "grade",
+      title: "grade",
+      axis: { tickBand: "extent", tickMinStep: 1 },
+    },
+    color: {
+      field: "skillSummary",
+      type: "nominal",
+      scale: {
+        domain: [
+          'No Mastery',
+          'Some Mastery',
+          'Beginning to Exhibit Full Mastery',
+           'Full Mastery',
+        ],
+        //range: ["#aa4599","#342288", "#88ccee", "#44aa99"],
+        range: ["#aa4599","#F8E768", "#b4ddd1", "#44aa99"],
+
+      },
+      title: "Skill Classification",
+    },
+    order: { field: "order", type: "nominal" },
+  },
+};
+
+const skillFocusByGradePA = {
+  description: "Skills to Focus On by Grade Level",
+  title: {
+    text: "Skills to Focus On",
+    subtitle: "by grade level",
+    anchor: "middle",
+    fontSize: 18,
+  },
+  height: 400,
+  width: 600,
+  data: { values: scoreStore.scores },
+  transform: [
+      // convert mastery to focus so we can use sum to count the number who need to work on the skill
+      {calculate: "(datum.masteryFSM==0)?1:0", as: "FSM"},
+      {calculate: "(datum.masteryLSM==0)?1:0", as: "LSM"},
+      {calculate: "(datum.masteryDEL==0)?1:0", as: "DEL"},
+  ],
+  "repeat": {"layer": ["FSM", "LSM", "DEL"]},
+  "spec": {
+    "mark": "bar",
+    "encoding": {
+
+      "y": {
+        "field": "grade",
+        "type": "nominal",
+        axis: {labelAngle:"0"},
+      },
+      "x": {
+        "aggregate": "sum",
+        "field": {"repeat": "layer"},
+        "type": "quantitative",
+        "title": "# of students"
+      },
+      "color": {
+        "datum": {"repeat": "layer"}, 
+        "title": "Skill",
+        scale: {range: ["#be95c4","#5e548e", "#0072b2", ]},
+      },
+      "yOffset": {"datum": {"repeat": "layer"}}
+    }
+  },
+  "config": {
+    "mark": {"invalid": null}
+  }
+};
+
+const normedPercentileDistribution1to4PA = {
+  // ...globalChartConfig,
+  description: "Distribution of Normed Percentiles (all grades)",
+  title: {
+    text: "Distribution of CTOPP Equivalent Percentiles",
+    anchor: "middle",
+    fontSize: 18,
+    subtitle: "(grades k to 4)",
+    subtitleFontStyle: "bold",
+    subtitleFontSize: 12,
+  },
+  height: 200,
+  width: 600,
+  data: { values: scoreStore.scores },
+
+  transform: [
+    {  calculate: "datum.percentileRankCTOPP <= 25? 'Extra Support Needed': datum.percentileRankCTOPP <=50? 'Some Support Needed': 'Average or Above Average' ",
+       as: "SupportGrade1to4",
+    },
+    {  calculate: "datum.percentileRankCTOPP <= 15? 'Extra Support Needed': datum.percentileRankCTOPP <=30? 'Some Support Needed': 'Average or Above Average' ",
+       as: "SupportGrade5to12",
+    },
+    {"filter": "(datum.grade == 'Kindergarten') || (datum.grade <= 4)" },  
+  ],
+
+  mark: "bar",
+  encoding: {
+    x: {
+      bin: true,
+      field: "percentileRankCTOPP",
+      title: "Percentile (relative to national norms)",
+      scale: { domain: [0, 100] },
+      bin: { step: 5, "extent":[0,100], },
+      axis: { tickMinStep: 1 },
+    },
+    y: { aggregate: "count", title: "count of students" },
+    order: { field: "order" },
+    color: {
+      field: "SupportGrade1to4",
+      title: "Support",
+      scale: {
+        domain: ["Extra Support Needed", "Some Support Needed", "Average or Above Average"],
+        //domain: ["At Risk", "Some Risk", "Doing Well"],
+        range: ["#cc79a7", "#f0e442", "#0072b2"],
+      },
+      order: { field: "order", type: "nominal" },
+    },
+  },
+};
+
+const normedPercentileDistribution5to12PA = {
+  // ...globalChartConfig,
+  description: "Distribution of Normed Percentiles (all grades)",
+  title: {
+    text: "Distribution of CTOPP Equivalent Percentiles",
+    anchor: "middle",
+    fontSize: 18,
+    subtitle: "(grades 5 to 12)",
+    subtitleFontStyle: "bold",
+    subtitleFontSize: 12,
+  },
+  height: 200,
+  width: 600,
+  data: { values: scoreStore.scores },
+
+  transform: [
+    {  calculate: "datum.percentileRankCTOPP <= 25? 'Extra Support Needed': datum.percentileRankCTOPP <=50? 'Some Support Needed': 'Average or Above Average' ",
+       as: "SupportGrade1to4",
+    },
+    {  calculate: "datum.percentileRankCTOPP <= 15? 'Extra Support Needed': datum.percentileRankCTOPP <=30? 'Some Support Needed': 'Average or Above Average' ",
+       as: "SupportGrade5to12",
+    },
+    {"filter": "datum.grade >= 5" },  
+  ],
+
+  mark: "bar",
+  encoding: {
+    x: {
+      bin: true,
+      field: "percentileRankCTOPP",
+      title: "Percentile (relative to national norms)",
+      scale: { domain: [0, 100] },
+      bin: { step: 5, "extent":[0,100], },
+      axis: { tickMinStep: 1 },
+    },
+    y: { aggregate: "count", title: "count of students" },
+    order: { field: "order" },
+    color: {
+      field: "SupportGrade5to12",
+      title: "Support",
+      scale: {
+        domain: ["Extra Support Needed", "Some Support Needed", "Average or Above Average"],
+        //domain: ["At Risk", "Some Risk", "Doing Well"],
+        range: ["#cc79a7", "#f0e442", "#0072b2"],
+      },
+      order: { field: "order", type: "nominal" },
+    },
+  },
+};
+
+const stackedSupportByGradePA = {
+  description: "Distribution of Support Classification by Grade Level",
+  title: {
+    text: "Distribution of Support Classification by Grade Level",
+    anchor: "middle",
+    fontSize: 18,
+  },
+  height: 200,
+  width: 600,
+  data: { values: scoreStore.scores },
+  transform: [
+    {  calculate: "datum.percentileRankCTOPP <= 25? 'Extra Support Needed': datum.percentileRankCTOPP <=50? 'Some Support Needed': 'Average or Above Average' ",
+       as: "SupportGrade1to4",
+    },
+    {  calculate: "datum.percentileRankCTOPP <= 25? 'Extra Support Needed': datum.percentileRankCTOPP <=50? 'Some Support Needed': 'Average or Above Average' ",
+       as: "SupportGrade5to12",
+    },
+    {
+      calculate:
+      "(datum.grade >=5)? datum.SupportGrade5to12 :datum.SupportGrade1to4 ",
+      as: "Support",
+    },
+    {
+      calculate:
+        "indexof(['Extra Support Needed', 'Some Support Needed', 'Average or Above Average'], datum.Support)",
+      as: "order",
+    },
+  ],
+  mark: "bar",
+  encoding: {
+    x: { aggregate: "count", title: "# of students", axis: { tickMinStep: 5,  } },
+    y: {
+      bin: false,
+      type: "ordinal",
+      field: "grade",
+      title: "grade",
+      axis: { tickBand: "extent", tickMinStep: 1 },
+    },
+    color: {
+      field: "Support",
+      type: "nominal",
+      scale: {
+        domain: [
+          "Extra Support Needed",
+          "Some Support Needed",
+          "Average or Above Average",
+        ],
+        range: ["#cc79a7", "#f0e442", "#0072b2"],
+      },
+      title: "Support",
+    },
+    order: { field: "order", type: "nominal" },
+  },
+};
 
 const distributionByGrade = {
   ...globalChartConfig,
@@ -163,8 +352,14 @@ const normedPercentileDistribution = {
 }
 
 const draw = async () => {
-  await embed('#viz-distribution-by-grade', distributionByGrade);
-  await embed('#viz-normed-percentile-distribution', normedPercentileDistribution);
+  await embed('#viz-distribution-by-grade-pa', distributionByGradePA);
+  await embed("#viz-stacked-skill-by-grade-pa", stackedSkillByGradePA);
+  await embed("#viz-skill-focus-by-grade-pa", skillFocusByGradePA);
+
+  await embed('#viz-normed-percentile-distribution-1-4-pa',normedPercentileDistribution1to4PA);
+  await embed('#viz-normed-percentile-distribution-5-12-pa',normedPercentileDistribution5to12PA);
+  await embed("#viz-stacked-support-by-grade-pa", stackedSupportByGradePA);
+
 };
 
 onMounted(() => {
--- src/pages/ScoreReport.vue
+++ src/pages/ScoreReport.vue
@@ -1,5 +1,6 @@
 <template>
   <ReportSWR v-if="scoreStore.taskId === 'swr'" />
+  <ReportPA v-else-if="scoreStore.taskId === 'roar-pa'" />
   <div v-else>
     We have not yet implemented a score report for your taskId of {{ scoreStore.taskId }}.
   </div>
@@ -8,6 +9,8 @@
 <script setup>
 import { useScoreStore } from "@/store/scores";
 import ReportSWR from "@/components/reports/ReportSWR.vue";
+import ReportPA from "@/components/reports/ReportPA.vue";
+
 
 const scoreStore = useScoreStore();
 </script>
