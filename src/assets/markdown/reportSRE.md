8f264910316840b3918d2daee2a7d25e7c8f9c4d
Jasmine Tran <jasetran@stanford.edu>
2023-03-21 12:42:17 -0700

    roar-sre .vue & .md + scores.js adjustments for sre

---
+++ src/assets/markdown/reportSRE.md
@@ -0,0 +1,76 @@
+# Sentence Reading Efficiency Score Report
+
+## Introduction to ROAR - Sentence Reading Efficiency
+
+***ROAR - Sentence Reading Efficiency*** examines the ability to **silent reading fluency and comprehension for individual sentences**. Students need both accuracy in decoding and fluency at the sentence level in order to achieve fluency in passage reading. Research shows that low fluency is a barrier to comprehension for many students. Students not receiving support for their foundational reading skills may struggle to catch up in overall reading ability. This assessment is well-designed for screening students who may be struggling with reading comprehension in part because of inaccurate decoding of words and/or slow and effortful reading.
+
+**This score report** provides information about the distribution of sentence-reading efficiency scores across each grade, as well as how each student’s skills compare to the national norms for their age group.
+
+**The assessment** provides a measure of reading fluency. During the test, a sentence is displayed on the screen, and students categorize the sentence as true or false. e.g., *“Cats live in the ocean”* is false, and, *“Some kids like ice cream”* is true.
+
+## Overview
+<div id='viz-distribution-by-grade'></div>
+
+This score report presents information from the recent administration of the ROAR assessment.
+
+- {{ sreStats.numStudents }} students in grades {{ sreStats.gradeMin }} to {{ sreStats.gradeMax }} completed the Sentence Reading Efficiency task.
+- The average age of the student was {{ sreStats.ageMean }}.
+- The average ROAR score was {{ sreStats.roarScoreMean }} on a scale of 100 - 900 
+- Scores ranged from {{ sreStats.roarScoreMin }} to {{ sreStats.roarScoreMax }}, with a standard deviation of {{ sreStats.roarScoreStandardDev }}.
+
+## Interpreting the Results
+The Test of Silent Reading Efficiency and Comprehension (TOSREC) is a standardized paper-based test that was
+normed on a nationally representative sample, and [has research support for its use as a screening instrument](https://doi.org/10.1177/1534508411395556). Stanford is currently working to validate ROAR - Sentence Reading
+Efficiency for its ability to predict TOSREC scores; as such, the TOSREC equivalent scores in this report are based
+upon the data we have collected so far. ROAR - Sentence Reading Efficiency provides a highly efficient way to
+screen a group of students for acute literacy needs and strengths without expending the time and resources to
+individually administer lengthy assessments.
+
+The vertical cut scores represent TOSREC thresholds for classifying students into different risk categories: *Above Average*, *Average*, *Below Average*, *Low*, or *Very Low*.
+
+- **Above Average**: Students whose sentence reading efficiency is above the 76th percentile.
+- **Average**: Students whose sentence reading efficiency is between the 75th and 24th percentile.
+- **Below Average**: Students whose sentence reading efficiency is between the 23rd and 9th percentile.
+- **Low**: Students whose sentence reading efficiency is between the 8th and 3rd percentile.
+- **Very Low**: Students whose sentence reading efficiency is below the 2nd percentile.
+
+<div id='viz-tosrec-distribution-across-grade'></div>
+
+Based on the TOSREC classifications:
+- {{ sreStats.tosrecSupport.aboveAverage }} students are classified as “Above Average.”
+- {{ sreStats.tosrecSupport.average }} students are classified as “Average.”
+- {{ sreStats.tosrecSupport.belowAverage }} students are classified as “Below Average.”
+- {{ sreStats.tosrecSupport.low }} students are classified as “Low.”
+- {{ sreStats.tosrecSupport.veryLow }} students are classified as “Very Low.”
+
+<div id='viz-stacked-tosrec-classification'></div>
+
+## Risk Level Information by Grade
+
+## Student Score Information
+Below is a data table of the de-identified students that can be filtered and downloaded. We will provide a list of
+identified IDs as part of a secure file transfer, separately from this report.
+
+## Student Score Information
+Below is a data table of the de-identified students that can be filtered and downloaded. We will provide a list of
+identified IDs as part of a secure file transfer, separately from this report.
+
+### Notes on Interpreting this Score Report
+
+- Results from this rapid screener should be interpreted in the context of other information you have about each student’s reading ability. For example, a student who is not fully engaged during the assessment may receive a score that does not reflect their true ability.
+- Students who need extra support in sentence-reading efficiency may need more support with decoding skills or with the development of fluency.
+- Scores should be interpreted in conjunction with other ROAR assessments:
+    - A student who is flagged as needing support on ROAR Sentence Reading Efficiency but not on ROAR Single Word Reading likely needs more support in developing automaticity. The student is likely reading accurately at the word-level but reading too slowly to maintain understanding of a whole sentence.
+    - A student who is flagged as needing support on ROAR Single Word Recognition likely has decoding issues that are impacting fluency, and would benefit from more support in decoding skills.
+    - A student who is flagged as needing support on ROAR Phonological Awareness may have issues in phonological awareness that are impacting the development of both word decoding and fluency. The student would likely benefit from support in phonological awareness.
+- Ongoing research continues to refine thresholds for identifying students who may benefit from additional support. Defining thresholds for identifying students who may benefit from additional support is part of our ongoing research. As part of this research collaborative, we invite your feedback about the information you see here.
+
+### Teacher Resources for Reading Support
+
+- The Florida Center for Reading Research offers free, online, age-specific, research-based lessons on phonological awareness and other foundational reading skills designed for pre-K through 5th-grade students [here](https://fcrr.org/student-center-activities/teacher-resource-guide).
+
+### Invitation to Collaborate
+
+Please reach out to [Jasmine Tran](jasetran@stanford.edu) and [Tonya Murray](tonyamur@stanford.edu) for questions or clarification on this score report and/or recommendations for how to improve future score reports.
+
+
--- src/store/scores.js
+++ src/store/scores.js
@@ -1,8 +1,150 @@
 import { defineStore } from "pinia";
 import { csvFileToJson, standardDeviation } from "@/helpers";
 
+const getRunInfoCommon = (run) => {
+  // note: new fields should be added to all cases
+  let normedPercentile;
+  switch(run.taskId) {
+    case "swr":
+      normedPercentile = woodcockJohnsonLookup(run.thetaEstimate);
+      return { 
+        roarScore: thetaToRoarScore(run.thetaEstimate),
+        normedPercentile: normedPercentile,
+        //supportLevel: thetaToSupportSWR(run.runInfoOrig.thetaEstimate, run.runInfoOrig.grade),
+        supportLevel: percentileToSupportClassification("swr", normedPercentile, run.grade),
+      };
+      break;
+
+    case "pa":
+    case "sre":
+      normedPercentile = woodcockJohnsonLookup(run.thetaEstimate);
+      return { 
+        roarScore: thetaToRoarScore(run.thetaEstimate),
+        normedPercentile: normedPercentile,
+        supportLevel: percentileToSupportClassification("sre", normedPercentile, run.grade),
+      };
+    case "vocab":
+    default:
+      console.log("TODO: add", run.taskId, " to getRunInfoCommon()");
+      break;
+  }
+
+};
+
+const getRunInfoTask =(run) => {
+  switch(run.taskId) {
+    case "swr":
+      return processSWRRun(run);
+      break;
+    case "pa":
+    case "sre":
+      return processSWRRun(run);
+      break;
+    case "vocab":
+    default:
+      console.log("TODO: add", run.taskId, " to getTaskSpecificScores()");
+      break;
+  }
+};
+
+
+const processSWRRun =(run) => {
+  return {    
+    // fields that vary between tasks
+    subScores: {
+      // These subtask IDs will differ in between tasks
+      fsm: Number,
+      lsm: Number,
+      del: Number,
+    },
+
+    comparisonPercentile: Number,
+    comparisonType: "Woodcock Johnson",
+    normedPercentile: Number,
+    classifications: {
+        // Here each field can differ depending on the task
+        support: String,
+        automaticity: String,
+    }
+  };
+};
+
 export function thetaToRoarScore (thetaEstimate) {
   return (Math.round(100 * (thetaEstimate + 5)));
+};
+
+function differenceInMonths(date1, date2) {
+  const monthDiff = date1.getMonth() - date2.getMonth();
+  const yearDiff = date1.getYear() - date2.getYear();
+
+  return monthDiff + yearDiff * 12;
+}
+export function computeAges(dob, timeStarted) {
+  //const timeStartedDate = str.match(/(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/g);
+  let timeStartedDate = timeStarted.substring(0,10);
+  let dateOfBirth = new Date(dob);
+  let dateOfRun = new Date(timeStartedDate);
+
+  let ageMonths = differenceInMonths(dateOfRun, dateOfBirth);
+  let ageYears = parseFloat((ageMonths/12).toFixed(1));
+
+  return { ageMonths, ageYears };
+};
+
+export function thetaToSupportSWR (percentile, grade) {
+  let support;
+
+  // we report automaticity instead of support for grades K/1 
+  if ((grade == "K") || (grade == "1")) {
+    support = (percentile < 50) ? "Limited" : "Average or Above Average";
+  } else {
+    support = (percentile < 25) ? "Extra Support Needed" : (percentile < 50) ? "Some Support Needed": "Average or Above Average";
+  }
+  //console.log(percentile, " ", grade, " ", support);
+  return support;
+};
+
+export function woodcockJohnsonLookup (thetaEstimate) {
+  // TODO replace this totally fake calculation with a real lookup table based on thetaEstimate and ageMonths
+  return Math.round(100 * (thetaEstimate +4)/8);
+};
+
+export function percentileToSupportClassification(taskId, percentile, grade=1) {
+  let support = "";
+
+  switch(taskId) {
+    case "pa":
+      if ((grade == "K") || (grade <= "4")) {
+        support = (percentile <= 25) ? "Extra Support Needed" : (percentile <= 50) ? "Some Support Needed": "Average or Above Average";
+      } else {
+        support = (percentile <= 15) ? "Extra Support Needed" : (percentile <= 30) ? "Some Support Needed": "Average or Above Average";
+      }
+      break;
+
+    case "swr":
+      if ((grade == "K") || (grade == "1")) {
+        support = (percentile <= 50) ? "Limited" : "Average or Above Average";
+      } else {
+        support = (percentile <= 25) ? "Extra Support Needed" : (percentile <= 50) ? "Some Support Needed": "Average or Above Average";
+      }
+      break;
+
+    case "sre":
+        support = (percentile <= 2 ) ? "Very Low" : (percentile <= 8) ? "Low": (percentile <= 23) ? "Below Average": (percentile <= 75) ? "Average": "Above Average";
+
+    case "vocab":
+      console.log("TODO add sre and vocab cases to percentileToSupportClassification() ")
+      break;
+  }
+
+  return(support);
+};
+
+export function debugTestFunction () {
+  return {
+    field1: 'true',
+    field2: 'false',
+  };
 };
 
 export const useScoreStore = () => {
@@ -21,6 +163,15 @@ export const useScoreStore = () => {
         // TODO: Add error handling to check that there is only one taskId
         return [...new Set(state.appScores.map((row) => row?.taskId))][0];
       },
+
+      debugTempVariable: (state) => 'false',
+      debugTempContainer: (state) => {
+        return {
+          showDebug: 'false',
+          fromFunction: debugTestFunction(),
+        }
+      },
+      
       reportType: (state) => {
         // Lots of complicated logic in here to determine the report type.
         // Options might include
@@ -33,7 +184,15 @@ export const useScoreStore = () => {
       scores: (state) => {
         // If identifiers were not uploaded, simply return the appScores
         if (state.identifiers.length === 0) {
-          return state.appScores;
+          //return { runInfoOrig: state.appScores}; 
+          return state.appScores.map((run) => {
+            return {
+              // original, unaltered, run-level info from the databases (no identifiers)
+              runInfoOrig: { 
+                ...run,
+               },
+            };
+          })
         } else {
           // Try to match appScores with the identifiers, matching on pid
           return state.appScores.map((run) => {
@@ -41,71 +200,120 @@ export const useScoreStore = () => {
               participant.pid === run.pid
             );
             if (matchingIdentifier.length === 0) {
-              return run
-            } else {
+              //return state.run;
               return {
-                ...run,
-                ...matchingIdentifier[0],
+                // original, unaltered, run-level info from the databases (no identifiers)
+                runInfoOrig: { 
+                  ...run,
+                 },
+              };
+            } else {
+              const mergedRun = {
+                        ...run,
+                        ...matchingIdentifier[0]
+                      };
+              return {
+                // original, unaltered, run-level info and identifiers from the databases
+                runInfoOrig: mergedRun,
+
+                // computed values common to all tasks
+                runInfoTest: getRunInfoCommon(mergedRun),
+
+                // TODO move these into getRunInfoCommon:
+                runInfoCommon: {
+                  roarScore: thetaToRoarScore(run.thetaEstimate),
+                  normedPercentile: woodcockJohnsonLookup(run.thetaEstimate),
+                  grade: matchingIdentifier[0].grade,
+                  supportLevel: thetaToSupportSWR(woodcockJohnsonLookup(run.thetaEstimate), matchingIdentifier[0].grade)
+                },
+ 
+                // computed values unique to each task
+                //runInfoTask: getRunInfoTask(run),
               }
             }
           })
         }
       },
-      numStudents: (state) => state.scores.length,
-      ages: (state) => {
-        const ages = state.scores.map((score) => score.age);
+
+/*       runInfoCommon: (state) => {
+        const runs = state.scores.map((score) => score.runInfoOrig);
+        if (runs.length === 0) {
+          return null;
+        } else {
+          return getRunInfoCommon(runs);
+        };
+      }, */
+
+      ageStats: (state) => {
+        const ages = state.scores.map((score) => computeAges(score.runInfoOrig.dob, score.runInfoOrig.timeStarted)); 
         if (ages.length === 0) {
           return null;
         }
+
+        const ageYears = ages.map((age) => age.ageYears);
+        const ageMonths = ages.map((age) => age.ageMonths);
+        console.log(ageYears[0],ageMonths[0]);
         return {
           ages: ages,
-          mean: ages.reduce((a, b) => a + b) / ages.length,
+          ageYearArray: ageYears,
+
+          ageMin: Math.min(...ageYears),
+          ageMax: Math.max(...ageYears),
+          ageMean: (ageYears.reduce((a, b) => a + b) / ages.length).toFixed(1),
         };
       },
-      grades: (state) => {
-        const gradesArray = state.scores.map((score) => score.grade);
-        return {
-          grades: gradesArray,
-          min: Math.min(...gradesArray),
-          max: Math.max(...gradesArray),
-        };
-      },
-      roarScores: (state) => {
-        const roarScoresArray = state.scores.map((score) => score.thetaEstimate);
-        const filterScores = 0;
-        if (roarScoresArray.length === 0) {
-          return {
-            scores: roarScoresArray,
-            min: null,
-            max: null,
-            mean: null,
-            sd: null,
-          };
-        } else {
-          return {
-            scores: roarScoresArray,
-            min: thetaToRoarScore(Math.min(...roarScoresArray)),
-            max: thetaToRoarScore(Math.max(...roarScoresArray)),
-            mean: thetaToRoarScore(roarScoresArray.reduce((a, b) => a + b) / roarScoresArray.length),
-            sd: standardDeviation(roarScoresArray).toFixed(2),
-          };
+      gradeStats: (state) => {
+        // TODO utility function handle "Kindergarten"
+        const grades = state.scores.map((score) => score.runInfoOrig.grade); 
+        if (grades.length === 0) {
+          return null;
         }
-      },
-      classifications: (state) => {
         return {
-          support: {
-            high: null,
-            medium: null,
-            low: null,
+          grades: grades,
+          gradeMin: Math.min(...grades),
+          gradeMax: Math.max(...grades),
+
+        };
+      },
+
+      swrStats: (state) => { 
+        return { 
+          ...state.ageStats,
+          ...state.gradeStats,
+          ...state.roarScoreStats,
+
+        };
+      },
+
+      sreStats: (state) => { 
+        const tosrecSupport = state.scores.map((score) => score.runInfoTest.supportLevel);
+        return { 
+          numStudents: state.scores.length,
+          tosrecSupport: {
+            aboveAverage: tosrecSupport.filter(x => x === "Above Average").length, 
+            average: tosrecSupport.filter(x => x === "Average").length,
+            belowAverage: tosrecSupport.filter(x => x === "Below Average").length,
+            low: tosrecSupport.filter(x => x === "Low").length,
+            veryLow: tosrecSupport.filter(x => x === "Very Low").length
           },
-          automaticity: {
-            high: null,
-            low: null,
-          }
-        }
+          ...state.ageStats,
+          ...state.gradeStats,
+          ...state.roarScoreStats,
 
+        };
+      },
 
-      }
+      roarScoreStats: (state) => {
+        const roarScoresArray = state.scores.map((score) => thetaToRoarScore(score.runInfoOrig.thetaEstimate));
+        return {
+          // Note: all calculations must gracefully handle an array length of 0
+          roarScoreMin: Math.min(...roarScoresArray),
+          roarScoreMax: Math.max(...roarScoresArray),
+          roarScoreMean: Math.round(roarScoresArray.reduce((a, b) => a + b,0) / roarScoresArray.length),
+          roarScoreStandardDev: standardDeviation(roarScoresArray).toFixed(0),
+        };
+      },
+
     },
     actions: {
       mergeSectionsWithIdentifiers: async (csvFile) => {
