36b023c332aca93a5048b3a34871d63bc0fdf082
Daniel Lucas <2110719+danielucas@users.noreply.github.com>
2023-03-28 22:50:12 +0000

    RoarGameCard finessing

--- src/components/RoarGameCard.vue
+++ src/components/RoarGameCard.vue
@@ -1,34 +1,133 @@
 <template>
-	<Card class="p-card-game m-4">
+	<Card :id="ID" :data-completed="completed" class="p-card-game m-4">
 		<template #header>
-		  <img :src="imgSrc" />
+			<div class="p-card-game-details">
+				<template v-if="completed">
+					<div class="p-card-game-success">
+						<i class="pi pi-check-circle"></i>
+						<Tag value="Completed" severity="success"></Tag>
+					</div>
+				</template>
+				<template v-else>
+					<span>Game x of y</span>	
+					<router-link :to="{ path: 'game/' + ID }"></router-link>
+					<div>Play <i class="pi pi-arrow-circle-right"></i></div>
+				</template>
+			</div>
+  			<img :src="imgSrc" />
 		</template>
 		<template #title>
-		  {{title}}
+  			{{title}}
 		</template>
 		<template #content>
-		  {{description}}
+  			{{description}}
 		</template>
 		<template #footer v-if="metadata">
-		  <Tag v-for="(items,index) in metadata" :value="items"></Tag>
+  			<Tag v-for="(items,index) in metadata" :value="items"></Tag>
 		</template>
-	  </Card>
-
+	</Card>
 </template>
 
 <script setup>
 	const props = defineProps({
+		ID: {type: String, required: true},
   		title: {type:String, required: true},
   		description: {type: String, required: true},
   		metadata: {type: Object, default: {}},
-  		imgSrc: {type: String, default: ''}
+  		imgSrc: {type: String, default: ''},
+		completed: {type: Boolean, default: false, required: true}
 	});
-
-	// export default {
-	// 	name: 'Card'
-	// };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+	.p-card-game {
+		--gray: lightgray;
+		--primary: var(--primary-color);
+		--success: #16A34A;
+
+		position: relative;
+		box-shadow: none;
+		border: 1px solid var(--gray);
+		text-align: left;
+	}
+		
+	// Game details
+	// Contains completed status; game count & play button
+	.p-card-game-details {
+		background: var(--gray);
+		padding: 1rem;
+		display: inline-flex;
+		align-items: center;
+		width: 100%;
+		justify-content: space-between;
+		border-top-left-radius: .25rem;
+		border-top-right-radius: .25rem;
+		
+		i {
+			font-size: 1.25rem;
+		}
+		
+		> div {
+			display: inline-flex;
+			gap: .5rem;
+			align-items: center;
+		}
+
+
+	}
 	
+	// link
+	// this component should have only 1 router link
+	a {
+		text-decoration: none;
+		color: inherit;
+		
+		&:before {
+			content: " ";
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+		}
+	}
+	
+	// Footer
+	.p-card-footer {
+		
+		// override CSS from <Tag>
+		.p-tag {
+			background: var(--gray);
+			margin-right: .5rem;
+		}
+
+	}
+	
+	// Not completed
+	[data-completed="false"] {
+		
+		&:hover {
+			border-color: var(--primary);
+			color: var(--primary);
+			
+			.p-card-game-details {
+				background: var(--primary);
+				color: white;
+			}
+		
+		}
+		
+	}
+	
+	
+	// Completed
+	[data-completed="true"] {
+		
+		.p-card-game-details {
+			background: white;
+			color: var(--success);
+		}
+		
+	}
+
 </style>
