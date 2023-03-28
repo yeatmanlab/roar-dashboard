e9ebdd4996f2af8596a7d0cacaee88a33bc60757
Daniel Lucas <2110719+danielucas@users.noreply.github.com>
2023-03-28 20:00:33 +0000

    RoarGameCard component

--- src/pages/Participant.vue
+++ src/pages/Participant.vue
@@ -1,10 +1,21 @@
 <template>
-  Welcome to the participant dashboard!
+  <div id="games">
+    <RoarGameCard title="ROAR PA" description="Some description" imgSrc="https://reading.stanford.edu/wp-content/uploads/2021/10/PA-1024x512.png" :metadata="testData"></RoarGameCard>
+  </div>
 </template>
 
 <script setup>
-
+  import RoarGameCard from '@/components/RoarGameCard.vue';
+  import {ref} from "vue"; 
+  const testData = ref({
+    version: "12345",
+    coins: "1M"
+  });
 </script>
 
 <style scoped>
+  #games {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
 </style>
