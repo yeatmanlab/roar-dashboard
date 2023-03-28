46297ad8e48b20ea9d7328e3d5993c3f584b563a
Adam Richie-Halford <richiehalford@gmail.com>
2023-03-28 12:14:52 -0700

    Adopt signOut method from roarfirekit instance

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import {
-  enableIndexedDbPersistence,
   getFirestore,
   doc,
   collection,
@@ -10,6 +9,7 @@ import { markRaw } from "vue";
 import firebaseConfig from "./config/firebase";
 import { RoarFirekit } from "@bdelab/roar-firekit";
 
+const roarfirekit = markRaw(new RoarFirekit({ roarConfig: firebaseConfig }));
 const firebaseApp = initializeApp(firebaseConfig.app, "legacy-app");
 
 // Use markRaw to wrap the firestore instance for use in components and the
@@ -19,31 +19,10 @@ const firebaseApp = initializeApp(firebaseConfig.app, "legacy-app");
 // https://github.com/firebase/firebase-js-sdk/issues/6087#issuecomment-1233478793
 // for a recommendation specific to Vue 3 and Firestore indexedDbPersistence
 const db = markRaw(getFirestore(firebaseApp));
-
-enableIndexedDbPersistence(db).catch((err) => {
-  if (err.code == "failed-precondition") {
-    console.log(
-      "Couldn't enable indexed db persistence. This is probably because the browser has multiple roar tabs open."
-    );
-    // Multiple tabs open, persistence can only be enabled
-    // in one tab at a a time.
-    // ...
-  } else if (err.code == "unimplemented") {
-    console.log(
-      "Couldn't enable indexed db persistence. This is probably because the browser doesn't support it."
-    );
-    // The current browser does not support all of the
-    // features required to enable persistence
-    // ...
-  }
-});
-// Subsequent queries will use persistence, if it was enabled successfully
-
 const auth = getAuth(firebaseApp);
 const rootDoc = doc(db, "prod", "roar-prod");
 const users = collection(rootDoc, "users");
 const tasks = collection(rootDoc, "tasks");
 const adminCollection = collection(db, "admin");
-const roarfirekit = markRaw(new RoarFirekit({ roarConfig: firebaseConfig }));
 
 export { auth, db, rootDoc, users, tasks, adminCollection, roarfirekit };
