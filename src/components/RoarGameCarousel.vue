f36db759e875901caa1f8f6ef1213012ba5893d9
Elijah Kelly <kellyel@stanford.edu>
2023-03-28 20:49:08 -0700

    Implement inital focusIndex && scrolling

--- src/components/RoarGameCarousel.vue
+++ src/components/RoarGameCarousel.vue
@@ -1,48 +1,61 @@
 <template>
   <div id="games" ref="gameContainer">
     <div v-for="(item, index) in items">
-      <RoarGameCard
-        id="card"
-        ref="cardList"
-        :gameId="item.id"
-        :title="item.title" 
-        :description="item.description" 
-        :imgSrc="item.imgSrc"
-        :metadata="item.metadata" 
-        :completed="item.completed"
-      />
+      <div ref="cardList">
+        <RoarGameCard
+          id="card"
+          :gameId="item.id"
+          :title="item.title" 
+          :description="item.description" 
+          :imgSrc="item.imgSrc"
+          :metadata="item.metadata" 
+          :completed="item.completed"
+        />
+      </div>
     </div>
   </div>
   <Button @click="scrollLeft" >&lt;</Button>
   <Button @click="scrollRight">></Button>
 </template>
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import RoarGameCard from '@/components/RoarGameCard.vue';
 
 const props = defineProps({
-  items: {required: true, default: {}}
+  items: {required: true, default: {}},
+  focusIndex: {required: false, default: 1}
 })
 const gameContainer = ref(null);
 const cardList = ref(null)
-let currentScroll = 0;
+let currentCardIndex = props.focusIndex - 1;
+
+onMounted(() => {
+  scrollToCard(props.focusIndex - 1)
+})
+
+function scrollToCard(index){
+  console.log('trying to scroll to card', index)
+  if(index <= cardList.value.length-1 && index > -1){
+    const scrollObject = cardList.value[index]
+    const scrollOffset = scrollObject.offsetLeft
+    console.log('scrollOffset', scrollOffset)
+    gameContainer.value.scrollTo({
+      left: scrollOffset,
+      behavior: 'smooth'
+    })
+    currentCardIndex = index
+  }
+}
 
 function getScrollWidth() {
-  return 398;
+  return cardList.value[0].scrollWidth;
 }
 
 function scrollLeft() {
-  gameContainer.value.scrollTo({
-    top: 0,
-    left: currentScroll -= getScrollWidth(),
-    behavior: 'smooth'
-  })
+  scrollToCard(currentCardIndex-1)
 }
 function scrollRight() {
-  gameContainer.value.scrollTo({
-    left: currentScroll += getScrollWidth(),
-    behavior: 'smooth'
-  })
+  scrollToCard(currentCardIndex+1)
 }
 </script>
 <style scoped>
