0b7d3fd215b38cb452455515994a8fc4ce72c661
Elijah Kelly <kellyel@stanford.edu>
2023-05-03 12:24:49 -0700

    Initial login page push

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <Toolbar class="mb-1 px-3 py-0">
+  <!-- <Toolbar class="mb-1 px-3 py-0">
     <template #start>
       <router-link :to="{ name: 'Home' }">
         <img src="../assets/roar-icon.png" height="50" alt="The ROAR logo" />
@@ -34,7 +34,23 @@
         <Button label="Log In" icon="pi pi-sign-in" />
       </router-link>
     </template>
-  </Toolbar>
+  </Toolbar> -->
+  <div class="redline" />
+  <div class="navbar-container">
+    <router-link :to="{ name: 'Home' }">
+      <!-- <img src="../assets/roar-icon.png" height="50" style="margin: 2.25rem" alt="The ROAR Logo" /> -->
+      <div class="navbar-logo">
+        <img src="../assets/stanford-roar.svg" height="50" alt="The ROAR Logo" />
+      </div>
+    </router-link>
+    <div class="login-container">
+      <SplitButton v-if="authStore.isAuthenticated" label="Account" icon="pi pi-user" :model="loggedInItems">
+      </SplitButton>
+      <router-link v-else :to="{ name: 'SignIn' }">
+        <Button label="Log In" icon="pi pi-sign-in" />
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -64,5 +80,24 @@ const closeInfo = () => displayInfo.value = false;
 </script>
 
 <style scoped>
-
+.redline {
+  width: 100%;
+  border-top: 7px solid #A80532;
+}
+.navbar-container { 
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-top: 1px solid #E5E5E5;
+  border-bottom: 1px solid #E5E5E5;
+}
+.navbar-logo {
+  margin-top: 1.25rem;
+  margin-left: 2rem;
+  margin-bottom: 1.25rem;
+}
+.login-container {
+  margin-right: 2rem;
+}
 </style>
--- src/pages/SignInOrRegister.vue
+++ src/pages/SignInOrRegister.vue
@@ -1,20 +1,139 @@
 <template>
-  <div class="flex align-items-center justify-content-center">
-    <TabView>
-      <TabPanel header="Sign in">
-        <SignIn :isRegistering="false" />
-      </TabPanel>
-      <TabPanel header="Register">
-        <SignIn :isRegistering="true" />
-      </TabPanel>
-    </TabView>
+  <div>
+    <div>
+      <div class="login-container mx-auto">
+        <Transition name="login" mode="out-in">
+          <div v-if="!isRegistering">
+            <SignIn :isRegistering="false" />
+            <div class="contact-text">
+              <span v-if="!isRegistering">Don't have an account? <button class="text-btn" @click="isRegistering = !isRegistering">Register</button></span>
+            </div>
+          </div>
+          <div v-else>
+            <SignIn :isRegistering="true" />
+            <div class="contact-text">
+              <span>Already have an account? <button class="text-btn" @click="isRegistering = false">Log In</button></span>
+            </div>
+          </div>
+        </Transition>
+      </div>
+      <Transition name="external-auth" mode="out-in">
+        <div v-if="!isRegistering">
+          <p>Other ways to Login</p>
+          <div class="push-top text-center button-container">
+            <Button @click="authWithGoogle"
+              label="Google" class="signin-button" />
+            <Button @click="authWithGoogle"
+              label="Clever" class="signin-button" />
+          </div>
+        </div>
+        <div v-else>
+          <p>Other ways to Register</p>
+          <div class="push-top text-center button-container">
+            <Button @click="authWithGoogle"
+              label="Google" class="signin-button" />
+            <Button @click="authWithGoogle"
+              label="Clever" class="signin-button" />
+          </div>
+        </div>
+      </Transition>
+      <!-- <p>Other ways to {{ isRegistering ? 'Register' : 'Login' }}</p>
+      <div class="push-top text-center button-container">
+        <Button @click="authWithGoogle"
+          label="Google" class="signin-button" />
+        <Button @click="authWithGoogle"
+          label="Clever" class="signin-button" />
+      </div> -->
+    </div>
   </div>
 </template>
 
 <script setup>
 import SignIn from "@/components/auth/SignIn.vue";
+import { useAuthStore } from "@/store/auth";
+import { isMobileBrowser } from "@/helpers";
+import { ref } from 'vue';
+
+const isRegistering = ref(false)
+
+const authStore = useAuthStore();
+const authWithGoogle = () => {
+  if(isMobileBrowser()) {
+    authStore.signInWithGoogleRedirect();
+  } else {
+    authStore.signInWithGooglePopup();
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.login-container {
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 5px;
+  border-color: #E5E5E5;
+  background-color: #FCFCFC;
+  width: 26.875rem;
+  padding-right: 1.5rem;
+  padding-left: 1.5rem;
+  margin-top: 6.5rem;
+  position: relative;
+}
+.contact-text {
+  padding-top: 2.75rem;
+  padding-bottom: 2rem;
+  text-align: left;
+}
+.button-container {
+  display: flex;
+  gap: 1.125rem;
+  justify-content: center;
+}
+.signin-button {
+  background-color: #E5E5E5;
+  border-color: #C4C4C4;
+  color: black;
+  width: 8rem;
+}
+.signin-button:hover {
+  background-color: #b7b5b5;
+  border-color: black;
+  color: black;
+}
+.text-btn {
+  border: none;
+  background-color: inherit;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 16px;
+  color: #2c3e50;
+}
+.login-enter-active,
+.login-leave-active {
+  transition: all 0.5s ease-out;
+}
+.login-enter-from {
+  opacity: 0;
+  transform: translateX(300px);
+}
 
+.login-leave-to {
+  opacity: 0;
+  transform: translateX(-300px);
+}
+
+
+.external-auth-enter-active,
+.external-auth-leave-active {
+  transition: all 0.5s ease-out;
+}
+.external-auth-enter-from {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.external-auth-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
