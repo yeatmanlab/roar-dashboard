80e2cc810b280150c40b5aba98c95b8fe3f1c343
Daniel Lucas <2110719+danielucas@users.noreply.github.com>
2023-05-10 22:54:30 +0000

    Tweaks to sidebar

--- src/components/ParticipantSidebar.vue
+++ src/components/ParticipantSidebar.vue
@@ -5,12 +5,12 @@
       <span style="font-size: 1.75rem">{{ completedGames }}/{{ totalGames }}</span>
       <span>tasks completed!</span>
     </div>
-    <div class="sidebar-info">
-      <div style="font-weight: bold">Student Info</div>
-      <div>Group: Woodside</div>
-      <div>Age: 8</div>
-      <div>Grade: 3</div>
-    </div>
+    <ul class="sidebar-info">
+      <li class="sidebar-title"><strong>Student Info</strong></li>
+      <li>Group: <span class="sidebar-info-item">Woodside</span></li>
+      <li>Age: <span class="sidebar-info-item">8</span></li>
+      <li>Grade: <span class="sidebar-info-item">3</span></li>
+    </ul>
   </div>
 </template>
 <script setup>
@@ -53,23 +53,30 @@ const setChartData = () => {
 };
 </script>
 <style scoped>
-.p-chart {
-  padding: 1rem;
-}
-.sidebar-container {
-  /* background-color: grey; */
-  margin-bottom: auto;
-  width: 200px;
-  border: 1px solid var(--surface-d);
-  border-radius: 5px;
-  height: auto; /* Full height minus the navbar */
-}
-.sidebar-info {
-  border-bottom-right-radius: 5px;
-  border-bottom-left-radius: 5px;
-  border-top: solid 1px var(--surface-d);
-  background-color: var(--surface-b);
-  padding: 1rem;
-  margin-top: 1rem;
-}
+  .p-chart {
+    padding: 1rem;
+  }
+  .sidebar-container {
+    margin-bottom: auto;
+    width: 200px;
+    border: 1px solid var(--surface-d);
+    border-radius: 5px;
+    height: auto;
+  }
+  .sidebar-info {
+    border-bottom-right-radius: 5px;
+    border-bottom-left-radius: 5px;
+    border-top: solid 1px var(--surface-d);
+    background-color: var(--surface-b);
+    padding: 1rem;
+    margin-top: 1rem;
+    list-style: none;
+    margin-bottom: 0;
+    line-height: 1.5;
+    
+    .sidebar-title {
+      border-bottom: 1px solid var(--surface-d);
+      margin-bottom: 1rem;
+    }
+  }
 </style>
