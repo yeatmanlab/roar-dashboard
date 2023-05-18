9b12fe39988e2c0182df9ed89f05fd9614367838
Daniel Lucas <2110719+danielucas@users.noreply.github.com>
2023-05-18 21:25:56 +0000

    Enhance sign in page

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -1,20 +1,25 @@
 <template>
-  <div>
-    <div class="signin-container mx-auto md:flex-none">
-      <SignIn style="padding-bottom: 1.625rem;" />
-      <!--We don't support individual registration yet-->
-      <!-- <div class="contact-text">
-        <span>Don't have an account? <router-link :to="{ name: 'Register' }" class="text-btn">Register</router-link></span>
-      </div> -->
-    </div>
-    <p style="text-align: center;">Other ways to Sign In</p>
-    <div class="push-top text-center button-container">
-      <Button @click="authWithGoogle"
-        label="Google" class="signin-button" />
-      <Button @click="authWithClever"
-        label="Clever" class="signin-button" />
-    </div>
-  </div>
+  <section id="signin">
+    <header>
+      <h1>Sign In to ROAR</h1>
+      <p>To access ROAR games, you must sign in with one of the options below.</p>
+    </header>
+    <section class="signin-option-container signin-option-userpass">
+      <SignIn />
+    </section>
+    <section class="signin-option-container signin-option-providers">
+      <h2>Sign In with</h2>
+      <Button @click="authWithGoogle" label="Sign in with Google" class="signin-button">
+        <img src="../assets/provider-google-logo.svg" height="50" alt="The ROAR Logo" class="signin-button-icon"/>
+        <span>Sign in with Google</span>
+      </Button>
+      <Button @click="authWithClever" class="signin-button">
+        <img src="../assets/provider-clever-logo.svg" height="50" alt="The ROAR Logo" class="signin-button-icon"/>
+        <span>Sign in with Clever</span>
+      </Button>
+    </section>
+  </section>
+
 </template>
 
 <script setup>
@@ -36,46 +41,18 @@ const authWithClever = () => {
 }
 </script>
 
-<style scoped>
-.signin-container {
-  border-style: solid;
-  border-width: 1px;
-  border-radius: 5px;
-  border-color: #E5E5E5;
-  background-color: #FCFCFC;
-  width: 26.875rem;
-  padding-right: 1.5rem;
-  padding-left: 1.5rem;
-  margin-top: 6.5rem;
-  position: relative;
-}
-.contact-text {
-  padding-bottom: 2rem;
-  text-align: left;
-}
-.button-container {
+<style>
+#app {
+  background: var(--primary-color);
   display: flex;
-  gap: 1.125rem;
+  align-content: center;
   justify-content: center;
-  margin-bottom: 6.5rem;
+  height: 100vh;
+  width: 100vw;
 }
-.signin-button {
-  background-color: #E5E5E5;
-  border-color: #C4C4C4;
-  color: black;
-  width: 8rem;
-}
-.signin-button:hover {
-  background-color: #b7b5b5;
-  border-color: black;
-  color: black;
-}
-.text-btn {
-  border: none;
-  background-color: inherit;
-  cursor: pointer;
-  display: inline-block;
-  font-size: 16px;
-  color: #2c3e50;
+
+.redline,
+.navbar-container {
+  display: none !important;
 }
 </style>
