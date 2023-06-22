9098e6e1dc3f41efe634827e730483f849108f82
Elijah Kelly <kellyel@stanford.edu>
2023-06-22 13:49:29 -0700

    console logs for route guards

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -2,8 +2,15 @@ import firebaseConfig from "./config/firebase";
 import { RoarFirekit } from "@bdelab/roar-firekit";
 import { markRaw } from "vue";
 
-export const roarfirekit = new RoarFirekit({ roarConfig: firebaseConfig, enableDbPersistence: false, authPersistence: 'session' });
-
-export function initNewFirekit() {
-  return new RoarFirekit({ roarConfig: firebaseConfig, enableDbPersistence: false, authPersistence: 'session' })
+export async function initNewFirekit() {
+  const firekit = markRaw(new RoarFirekit({ 
+    roarConfig: firebaseConfig, 
+    authPersistence: 'session', 
+    markRawConfig: { 
+      auth: false, 
+      db: true, 
+      functions: false
+    }
+  }));
+  return await firekit.init();
 }
