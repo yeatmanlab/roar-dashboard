f4315544dd9b2e61cfc1173741f3f2ac19800dac
Elijah Kelly <kellyel@stanford.edu>
2023-06-26 13:17:03 -0700

    compatibility

---
+++ src/pages/PlayApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>Hello PlayApp</span>
+  <div id="jspsych-target" />
+</template>
+<script setup>
+import { HotDogApp } from 'roar-repackage';
+import { useAuthStore } from "@/store/auth";
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+const authStore = useAuthStore();
+const appKit = await authStore.roarfirekit.startAssessment();
+// const hda = new HotDogApp(appKit, {});
+  hda.run();
+})
+
+</script>
