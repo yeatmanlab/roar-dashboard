a3e6c0ea2a9fc17aa0b83b6792af94f353679c7c
Elijah Kelly <kellyel@stanford.edu>
2023-06-26 10:39:21 -0700

    start setting up app integration

---
+++ src/components/PlayApp.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="jspych-target" />
+</template>
+<script>
+import { HotDogApp } from 'roar-repackage'
+import { useAuthStore } from "@/store/auth";
+const authStore = useAuthStore();
+const appKit = await authStore.roarfirekit.startAssessment()
+const hda = new HotDogApp()
+</script>
