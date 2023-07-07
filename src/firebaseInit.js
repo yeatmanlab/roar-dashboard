adfca09949a49c2e8e1ebc5f809245c8e48d9ef0
Elijah Kelly <kellyel@stanford.edu>
2023-07-07 15:31:52 -0700

    Update gameTabs to accept new data format

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -3,14 +3,14 @@ import { RoarFirekit } from "@bdelab/roar-firekit";
 import { markRaw } from "vue";
 
 export async function initNewFirekit() {
-  const firekit = markRaw(new RoarFirekit({ 
+  const firekit = new RoarFirekit({ 
     roarConfig: firebaseConfig, 
     authPersistence: 'session', 
     markRawConfig: { 
       auth: false, 
-      db: true, 
+      db: false, 
       functions: false
     }
-  }));
+  });
   return await firekit.init();
 }
