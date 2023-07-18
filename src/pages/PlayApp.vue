d59152651d3e689de6fa27c580ddcf02d3cfb18d
Elijah Kelly <kellyel@stanford.edu>
2023-07-17 20:16:40 -0700

    Setup inital app connection

--- src/pages/PlayApp.vue
+++ src/pages/PlayApp.vue
@@ -1,19 +1,21 @@
 <template>
-  <span>Hello PlayApp</span>
+  <div class="col-full text-center">
+    <h1>Preparing your game!</h1>
+    <AppSpinner />
+  </div>
   <div id="jspsych-target" />
 </template>
 <script setup>
 import { HotDogApp } from 'roar-repackage';
 import { useAuthStore } from "@/store/auth";
 import { onMounted } from 'vue';
+import AppSpinner from '../components/AppSpinner.vue';
 
 onMounted(async () => {
   console.log('Hello PlayApp')
   const authStore = useAuthStore();
   const appKit = await authStore.roarfirekit.startAssessment("4GnqGp4KV8dVNmitVQG8", "roar-repackage");
-  console.log('reached endpoint', appKit)
   const hda = new HotDogApp(appKit, { registered: true }, 'jspsych-target');
-  console.log('hda:', hda)
   hda.run();
 })
 
