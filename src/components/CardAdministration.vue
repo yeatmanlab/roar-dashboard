f518f026212076e6163d8eb47654e91a19f8a7cc
Daniel Lucas <2110719+danielucas@users.noreply.github.com>
2023-07-26 16:24:41 -0700

    Administration stats card

---
+++ src/components/CardAdministration.vue
@@ -0,0 +1,149 @@
+<template>
+	
+	<div :data-administration="id" class="card card-administration">
+		
+		<div class="card-admin-chart">
+			<Chart type="doughnut" :data="chartData" :options="chartOptions"/>
+		</div>
+			
+		<div class="card-admin-body">
+			<h2 class="card-admin-title">{{title}}</h2>
+			<div class="card-admin-details">
+				<p><strong>{{dates.start}}—{{dates.end}}</strong></p>
+				<p><strong>Assigned to: </strong> 
+					<span v-for="assignee in assignees" class="card-inline-list-item">
+					  {{ assignee }}
+					</span>
+				</p>
+			</div>
+			<div class="card-admin-assessments">
+				<p><strong>Assessments</strong></p>
+				<p><span v-for="assessment in assessments" class="card-inline-list-item">{{ assessment }}</span></p>
+			</div>			
+			<div class="card-admin-link">
+				<router-link
+				  :to="{ name: 'ViewAdministration', params: { id: id } }"
+				  v-slot="{href, route, navigate}"
+				  >
+					<button :href="href" @click="navigate" class='p-button p-button-secondary p-button-outlined'>
+					  View all details
+					</button>
+				</router-link>
+			</div>
+		</div>
+
+	</div>
+
+</template>
+
+<script setup>
+	import { ref, onMounted } from "vue";
+	
+	const props = defineProps({
+		id: Number,
+		title: String, 
+		stats: Object,
+		dates: Object,
+		assignees: Array, 
+		assessments: Array,
+	});
+
+	
+	onMounted(() => {
+		const started 		= props.stats.completed;
+  		const completed 	= props.stats.started;
+		const total 		= (props.stats.total - started - completed);
+  		chartData.value 	= setChartData(total, started, completed);
+	});
+	
+	const chartData = ref();
+	const chartOptions = ref({
+		cutout: '60%',
+		showToolTips: true,
+		plugins: {
+	  		legend: {
+				display: false
+	  		},
+	  		tooltip: {
+				enabled: true
+	  		}
+		}
+	});
+	
+	const setChartData = (total, started, completed) => {
+  		let docStyle = getComputedStyle(document.documentElement);
+  		
+  		return {
+			labels: ['Not Started', 'Started', 'Completed'],
+			datasets: [
+	  		{
+				data: [total, started, completed],
+				backgroundColor: [
+		  			docStyle.getPropertyValue('--surface-d'),
+					docStyle.getPropertyValue('--yellow-100'),
+					docStyle.getPropertyValue('--bright-green'),
+		  			
+				],
+				// hoverBackgroundColor: ['green', docStyle.getPropertyValue('--surface-d')]
+	  		}
+			]
+  		};
+	};
+
+</script>
+
+<style lang="scss">
+	.card-administration {
+		text-align: left;
+		width: 100%;
+		background: var(--surface-b);
+		border: 1px solid var(--surface-d);
+		border-radius: var(--border-radius);
+		display: inline-flex;
+		flex-direction: row;
+		gap: 2rem;
+		padding-block: 1rem;
+		
+		.card-admin-chart {
+			padding: 1rem;	
+		}
+		
+		.card-admin-body {
+			padding-top: 2rem;
+			flex: 1 1 auto;
+			display: inline-flex;
+			flex-direction: row;
+			flex-wrap: wrap;
+			align-content: start;
+			
+			p {
+				margin-block: .5rem;
+			}
+		}
+		
+		.card-admin-title {
+			font-weight: bold;
+			width: 100%;
+			padding-bottom: .5rem;
+			border-bottom: 1px solid var(--surface-d);
+			flex: 1 1 100%;
+		}
+		
+		.card-admin-details {
+			width: 45%;
+		}
+				
+		.card-admin-link {
+			margin-top: 2rem;
+			width: 100%;
+		}
+	}
+	
+	.card-inline-list-item {
+		position: relative;
+		
+		&:not(:last-child):after {
+			content: ", "; 
+		}
+	}
+</style>
