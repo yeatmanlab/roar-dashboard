21039320a55175c80d592c22bee8d4d2dfd679f2
Elijah Kelly <kellyel@stanford.edu>
2023-07-31 17:26:08 -0700

    Install packaged versions of apps

--- src/pages/PlayApp.vue
+++ src/pages/PlayApp.vue
@@ -7,50 +7,46 @@
 </template>
 <script setup>
 import { HotDogApp } from 'roar-repackage';
-// import RoarSWR from '@bdelab/roar-swr';
+import RoarSWR from '@bdelab/roar-swr'; // TODO: make this a dynamic import
+import RoarPA from '@bdelab/roar-pa';
+import RoarSRE from '@bdelab/roar-sre';
 import { useAuthStore } from "@/store/auth";
-import { onMounted } from 'vue';
+import { onMounted, toRaw } from 'vue';
 import AppSpinner from '../components/AppSpinner.vue';
 import assets from '../assets/tasks/swr.json';
+import { useRouter } from 'vue-router';
 
-console.log('assets', assets)
+const router = useRouter();
+const currentGameId = (router.currentRoute).value.params.gameId;
 
-// onMounted(async () => {
-//   console.log('Hello PlayApp')
-//   const authStore = useAuthStore();
-//   console.log('about to get appKit')
-//   const appKit = await authStore.roarfirekit.startAssessment("4GnqGp4KV8dVNmitVQG8", "roar-swr")
-//   console.log('appKit is', appKit)
-//   // const appKit = await authStore.roarfirekit.startAssessment("4GnqGp4KV8dVNmitVQG8", "roar-repackage");
-//   // const hda = new HotDogApp(appKit, { registered: true }, 'jspsych-target');
-//   // hda.run();
 
-//   const params = {
-//       userMode: null, 
-//       pid: null, 
-//       studyId: null, 
-//       classId: null, 
-//       schoolId: null, 
-//       taskVariant: null,
-//       skipInstructions: null,
-//       audioFeedback: null,
-//       consent: null,
-//       numAdaptive: null,
-//       numNew: null,
-//       numValidated: null,
-//       labId: null,
-//       gameId: null,
-//       assets: assets,
-//       bucketURI: 'https://storage.googleapis.com/roar-swr'
-//   }
+// TODO: have this in a roarfirekit ready watcher
+onMounted(async () => {
+  // console.log('importing', `@bdelab/roar-${currentGameId}`)
+  // const RoarAppImport = await import(`@bdelab/roar-${currentGameId}`).then((roarApp) => {
+  //   console.log('inside import statement:', roarApp)
+  // });
+  console.log('Hello PlayApp')
+  const authStore = useAuthStore();
+  console.log('about to get appKit')
+  console.log('assessment ID', authStore.assignedAssignments)
+  // TODO: get assessment ID dynamically
+  const appKit = await authStore.roarfirekit.startAssessment("4GnqGp4KV8dVNmitVQG8", "sre")
+  console.log('appKit is', appKit)
 
-//   const roarApp = new RoarSWR(appKit, params);
-//   await roarApp.run().then(() => {
-//     for( let x = 0; x <= 100; x++){
-//       console.log('Then called; the game should be over')
-//     }
-//   })
+  console.log('appKit assessmentPid', appKit._userInfo.assessmentPid)
+  console.log('variant params', appKit._taskInfo.variantParams)
 
-// })
+  const userParams = {
+    pid: appKit._userInfo.assessmentPid,
+    labId: "yeatmanlab", 
+  }
+
+  const gameParams = appKit._taskInfo.variantParams
+
+  // TODO: shouldn't have to change after dynamic import
+  const roarApp = new RoarSRE(appKit, gameParams, userParams);
+  await roarApp.run();
+})
 
 </script>
