cd9b7b5d545217d7aab30d4edc763f3f7c2f2601
Mohammed Shoaib <shoaib98libra@gmail.com>
2023-08-09 23:54:03 -0700

    Remove datatable and instead use the RoarDataTable component

--- src/pages/Administration.vue
+++ src/pages/Administration.vue
@@ -1,51 +1,14 @@
-<script setup lang="ts">
-import { ref, onMounted } from "vue";
+<template>
+  <div>
+    <Button label="Remind" @click="remove"/>
+    <Button label="Get Report" @click="remove"/>
+    <Button label="Delete" @click="remove"/>
+    <RoarDataTable :data="data" :columns="columns" />
+  </div>
+</template>
 
-import DataTable from 'datatables.net-vue3';
-import DataTablesCore from 'datatables.net';
-import 'datatables.net-select';
-
-import moment from "moment";
-import "moment-timezone";
-import { Timestamp } from "firebase-admin/firestore";
- 
-DataTable.use(DataTablesCore);
-
-const DATE_FORMAT = "YYYY-MM-DD";
-const DATE_AND_TIME_FORMAT = "YYYY-MM-DD HH:mm:ss.SSS z";
-
-let dt;
-const columns = ref([
-  {
-    targets: 0,
-    data: null,
-    defaultContent: '',
-    orderable: false,
-    className: 'select-checkbox'
-  },
-  { data: 'pid', title: 'PID' },
-  { data: 'assigned', title: 'Assigned' },
-  { data: 'completed', title: 'Completed' },
-  { 
-    data: 'actions',
-    title: 'Actions',
-    orderable: false,
-    defaultContent: `
-      <button class="bg-green-700 hover:bg-green-800 text-white font-semibold px-4 border border-gray-400 rounded shadow">
-        Remind
-      </button>
-      <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold px-4 rounded inline-flex items-center">
-        Get Report
-      </button>
-      <button
-        @click="remove"
-        class="bg-purple-700 hover:bg-purple-800 text-white font-semibold px-4 rounded">
-        Delete
-      </button>
-      `
-  },
-]);
-
+<script setup>
+import { ref } from 'vue';
 const data = ref([
   { pid: "PID_1245", assigned: 1691469008099, completed: 1691469018099 },
   { pid: "PID_1013", assigned: 1691469013099, completed: 1691469018099 },
@@ -54,15 +17,13 @@ const data = ref([
   { pid: "PID_1467", assigned: 1691469918099, completed: 1691469018099 },
 ]);
 
-const options = {
-  responsive: true,
-  select: {
-    style:    'multi',
-    selector: 'td:first-child'
-  }
-};
+let selectedData = ref([]);
 
-const table = ref();
+const columns = ref([
+  { field: "pid", header: "PID", dataType: "text"},
+  { field: "assigned", header: "Assigned", dataType: "text"},
+  { field: "completed", header: "Completed", dataType: "text"},
+]);
 
 for (let i = 5; i < 500 ; i++) {
   const minDate = new Date(2023, 0, 1);
@@ -84,83 +45,10 @@ for (let i = 0; i < data.value.length; i++) {
     data.value[i][col] = new Date(data.value[i][col]).toDateString();
   }
 }
-
-// Get a DataTables API reference
-onMounted(function () {
-  dt = table.value.dt;
-});
-
-// For each selected row find the data object in `data` array and remove it
-function remove() {
-  dt.rows({ selected: true }).every(function () {
-    let idx = data.value.indexOf(this.data());
-    data.value.splice(idx, 1);
-  });
-}
-
-function convertTimestamp(timestamp: string | number) {
-  // convert from string to a JSON object
-  if (typeof timestamp === "string") {
-    try {
-      timestamp = JSON.parse(timestamp);
-    } catch (err: unknown) {
-      console.log("Error when parsing timestamp to a JSON object!");
-      console.log("Timestamp:", timestamp);
-      if (err instanceof Error) {
-        console.log("Error message:", err.message);
-      }
-    }
-  }
-
-  return timestamp["_seconds"] * 1000 + timestamp["_nanoseconds"] / 1000000;
-}
-
-function convertTimestampToDate(
-  timestamp: string | number,
-  timezone: string = "America/Los_Angeles"
-) {
-  // const milliseconds = convertTimestamp(timestamp);
-  return moment(timestamp).tz(timezone).format(DATE_AND_TIME_FORMAT);
-}
-
-function trimTimeFromDate(date: string) {
-  return moment(date, DATE_AND_TIME_FORMAT).format(DATE_FORMAT); // YYYY-MM-DD
-}
 </script>
 
-<template>
-  <h2 class="admin-title">Administration Title</h2>
-  <DataTable
-    :data="data"
-    :columns="columns"
-    class="display"
-    :options="options"
-    ref="table"
-  />
-  <button
-    @click="remove"
-    class="bg-purple-700 hover:bg-purple-800 text-white font-semibold px-4 rounded">
-    Delete
-  </button>
-</template>
-
 <style>
-@import 'datatables.net-dt';
-@import 'https://cdn.datatables.net/select/1.7.0/css/select.dataTables.min.css';
-@import 'https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css';
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-button {
+.p-button {
   margin: 0px 8px;
-}
-
-.admin-title {
-  font-weight: bold;
-  width: 100%;
-  padding-bottom: .5rem;
-  border-bottom: 1px solid var(--surface-d);
-  flex: 1 1 100%;
 }
 </style>
