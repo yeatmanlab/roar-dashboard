4c70be5b161fc1c3d5b5cbacefe803b0ac74ccd7
Elijah Kelly <kellyel@stanford.edu>
2023-08-16 18:57:02 -0700

    Implement reliable dynamic homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,50 +1,14 @@
 <template>
-  <div class="mt-5 flex flex-row flex-wrap justify-content-center card-container">
-    <Card class="m-4" style="width: 25em">
-      <template #title>
-        Query ROAR Assessment Results
-      </template>
-      <template #content>
-        <p>
-          Click below to interactively query ROAR assessment data. You must be an
-          authenticated user to access this data.
-        </p>
-      </template>
-      <template #footer>
-        <router-link :to="{ name: 'Query' }">
-          <Button icon="pi pi-search" label="Query" />
-        </router-link>
-      </template>
-    </Card>
-    <Card class="m-4" style="width: 25em">
-      <template #title>
-        Generate a Score Report from your own files
-      </template>
-      <template #content>
-        <p>
-          Click below to generate a score report from CSV files on your own computer.
-        </p>
-      </template>
-      <template #footer>
-        <router-link :to="{ name: 'UploadScores' }">
-          <Button icon="pi pi-chart-line" label="Generate Report" />
-        </router-link>
-      </template>
-    </Card>
-  </div>
+  <Participant v-if="!isAdminRef"/>
+  <Administrator v-else-if="isAdminRef" />
 </template>
 
 <script setup>
-const onAdminUpload = async (event) => {
-  toast.add({ severity: 'info', summary: 'Success', detail: 'Identifier File Uploaded', life: 3000 });
-  uploadedFile.value = event.files[0];
-  scoreStore.identifiers = await csvFileToJson(uploadedFile.value);
-}
-const submit = () => {
-  router.push({ name: "ScoreReport" });
-}
+import { ref } from "vue";
+import { useAuthStore } from '@/store/auth';
+import Participant from "./Participant.vue";
+import Administrator from "./Administrator.vue";
+const authStore = useAuthStore();
+const isAdmin = authStore.isUserAdmin();
+const isAdminRef = ref(isAdmin)
 </script>
-
-<style scoped>
-
-</style>
