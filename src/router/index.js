13a1e6e6ace4018f620169446b9872ea8b54bfc8
Elijah Kelly <kellyel@stanford.edu>
2023-08-17 22:15:21 -0700

    Define routes that require admin status

--- src/router/index.js
+++ src/router/index.js
@@ -48,13 +48,13 @@ const routes = [
     path: "/upload-scores",
     name: "UploadScores",
     component: () => import("../pages/UploadFiles.vue"),
-    meta: { pageTitle: "Upload Scores" },
+    meta: { pageTitle: "Upload Scores", requireAdmin: true },
   },
   {
     path: "/query",
     name: "Query",
     component: () => import("../pages/QueryPage.vue"),
-    meta: { pageTitle: "Query" },
+    meta: { pageTitle: "Query", requireAdmin: true },
   },
   {
     path: "/score-report",
@@ -73,7 +73,8 @@ const routes = [
   {
     path: '/mass-upload',
     name: 'MassUploader',
-    component: () => import("../pages/MassUploader.vue")
+    component: () => import("../pages/MassUploader.vue"),
+    meta: {pageTitle: "Register Students", requireAdmin: true}
   },
   {
     path: "/signin",
@@ -106,24 +107,25 @@ const routes = [
     path: "/administrator",
     name: "Administrator",
     component: () => import(/* webpackChunkName: "Administrator" */ "../pages/Administrator.vue"),
-    meta: {pageTitle: "Administrator"}
+    meta: {pageTitle: "Administrator", requireAdmin: true}
   },
   {
     path: "/create-admin",
     name: "CreateAdministration",
     component: () => import(/* webpackChunkName: "CreateAdministration" */ "../components/CreateAdministration.vue"),
-    meta: {pageTitle: "Create an administration"}
+    meta: {pageTitle: "Create an administration", requireAdmin: true}
   },
   { 
     path: "/create-org",
     name: "CreateOrg",
     component: () => import(/* webpackChunkName: "CreateAdministration" */ "../components/CreateOrg.vue"),
-    meta: {pageTitle: "Create an organization"}
+    meta: {pageTitle: "Create an organization", requireAdmin: true}
   },
   {
     path: "/administration/:id",
     name: "ViewAdministration",
     component: () => import(/* webpackChunkName: "CreateAdministration" */ "../pages/Administration.vue"),
+    meta: {pageTitle: "View Administration", requireAdmin: true}
   },
 
   {
@@ -161,10 +163,14 @@ const router = createRouter({
 
 router.beforeEach(async (to, from) => {
   const store = useAuthStore();
+  // Check if user is signed in. If not, go to signin
   if (!to.path.includes("__/auth/handler")
     && (!store.isAuthenticated && to.name !== "SignIn" && to.name !== "AuthClever")) {
-    console.log("You're not logged in. Routing to SignIn")
     return { name: "SignIn" }
+  }
+  // Check if user is an admin. If not, 
+  if (_get(to, 'meta.requireAdmin') && !store.isUserAdmin()) {
+    return { name: "Home" }
   }
 })
 
