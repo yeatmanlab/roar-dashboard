103162794f89a13b165798683abf92c2ec91259f
Daniel Lucas <2110719+danielucas@users.noreply.github.com>
2023-08-18 14:50:54 -0700

    Fix conflicts

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,12 @@
     <meta name="twitter:title" content="ROAR Web Query">
     <meta name="twitter:description" content="A web-based tool to query ROAR assessment data!">
   </AppHead>
-<<<<<<< HEAD
-  <Navbar v-if="$route.name !== 'SignIn'" />
-  <main id="main">
-    <router-view />
-  </main>  
-=======
   <div>
     <Toast />
     <Navbar v-if="!navbarBlacklist.includes($route.name)" />
     <router-view />
   </div>
 
->>>>>>> main
   <!-- <AppSpinner v-show="!showPage" /> -->
 </template>
 
