4d828f6f1f5f061ffe02c598163587525a6bb4bc
Elijah Kelly <kellyel@stanford.edu>
2023-09-13 18:37:33 -0700

    Add extra support info and style header

--- src/pages/ScoreReport.vue
+++ src/pages/ScoreReport.vue
@@ -11,19 +11,21 @@
           </button>
         </template>
 
-        <h2 v-if="orgInfo">{{ orgInfo.name }} Score Report</h2>
+        <h2 v-if="orgInfo" class="report-title">{{ _toUpper(orgInfo.name) }} SCORE REPORT</h2>
 
         <!-- Header blurbs about tasks -->
-        <h2>In this Report...</h2>
+        <h2>IN THIS REPORT...</h2>
         <span>You will receive a breakdown of your classroom's ROAR scores across each of the domains tested. </span>
-        <div v-if="allTasks.includes('pa')">
-          <span class="task-header">ROAR-Phonological Awareness (ROAR-Phoneme)</span> assesses some of the most foundational skills for reading: mapping letters to their corresponding sounds. This skill is crucial for building further reading fluency skills, such as decoding.
-        </div>
-        <div v-if="allTasks.includes('swr')">
-          <span class="task-header">ROAR-Single Word Recognition (ROAR-Word)</span> assesses decoding skills at the word level.
-        </div>
-        <div v-if="allTasks.includes('sre')">
-          <span class="task-header">ROAR-Sentence Reading Efficiency (ROAR-Sentence)</span> assesses reading fluency at the sentence level. 
+        <div class="task-overview-container">
+          <div v-if="allTasks.includes('pa')" class="task-blurb">
+            <span class="task-header">ROAR-Phonological Awareness (ROAR-Phoneme)</span> assesses some of the most foundational skills for reading: mapping letters to their corresponding sounds. This skill is crucial for building further reading fluency skills, such as decoding.
+          </div>
+          <div v-if="allTasks.includes('swr')" class="task-blurb">
+            <span class="task-header">ROAR-Single Word Recognition (ROAR-Word)</span> assesses decoding skills at the word level.
+          </div>
+          <div v-if="allTasks.includes('sre')" class="task-blurb">
+            <span class="task-header">ROAR-Sentence Reading Efficiency (ROAR-Sentence)</span> assesses reading fluency at the sentence level. 
+          </div>
         </div>
 
 
@@ -59,7 +61,7 @@
             </div>
           </div>
         </div>
-        <div style="text-align: center;">Students are classified into three support groups based on nationally-normed percentiles. Blank spaces indicate that the assessment was not completed.</div>
+        <div class="legend-description">Students are classified into three support groups based on nationally-normed percentiles. Blank spaces indicate that the assessment was not completed.</div>
 
         <!-- In depth breakdown of each task-->
         <div v-if="allTasks.includes('pa')" class="task-card">
@@ -77,6 +79,21 @@
           <span style="text-transform: uppercase;">Sentence Reading Efficiency</span>
           <p class="task-description">ROAR - Sentence examines silent reading fluency and comprehension for individual sentences. To become fluent readers, students need to decode words accurately and read sentences smoothly. Poor fluency can make it harder for students to understand what they're reading. Students who don't receive support for their basic reading skills may find it challenging to improve their overall reading ability. This assessment is helpful for identifying students who may struggle with reading comprehension due to difficulties with decoding words accurately or reading slowly and with effort.</p>
         </div>
+
+        <div>
+          <h2 class="extra-info-title">HOW ROAR SCORES INFORM PLANNING TO PROVIDE SUPPORT</h2>
+          <p>Each foundational reading skill is a building block of the subsequent skill. Phonological awareness supports the development of word-level decoding skills. Word-level decoding supports sentence-reading fluency. Sentence-reading fluency supports reading comprehension. For students who need support in reading comprehension, their ROAR results can be used to inform the provision of support. </p>
+          <ol>
+            <li>Students who need support in all categories should begin with support in phonological awareness as the base of all other reading skills.</li>
+            <li>Students who have phonological awareness skills but need support in single-word recognition would likely benefit from targeted instruction in decoding skills to improve accuracy.</li>
+            <li>Students who have phonological awareness and word-decoding skills but need support in sentence-reading would likely benefit from sustained practice in reading for accuracy and fluency. These students demonstrate they can read at the word-level, but they do not appear to read quickly and accurately across the length of a sentence.</li>
+          </ol>
+          <a href="google.com">Click here</a> for more guidance on steps you can take in planning to support your students.
+        </div>
+        <div>
+          <h2 class="extra-info-title">NEXT STEPS</h2>
+          <p>This score report has provided a snapshot of your school's reading performance at the time of administration. By providing classifications for students based on national norms for scoring, you are able to see which students can benefit from varying levels of support. To read more about what to do to support your students, <a href="google.com">read here.</a></p>
+        </div>
       </Panel>
     </section>
   </main>
@@ -86,6 +103,7 @@
 import { computed, ref, onMounted } from 'vue';
 import { storeToRefs } from 'pinia';
 import _capitalize from 'lodash/capitalize';
+import _toUpper from 'lodash/toUpper'
 import _get from 'lodash/get'
 import { useAuthStore } from '@/store/auth';
 import { useQueryStore } from '@/store/query';
@@ -248,6 +266,10 @@ onMounted(async () => {
 .p-button {
   margin: 0px 8px;
 }
+.report-title {
+  font-size: 3.5rem;
+  margin-top: 0;
+}
 .task-header {
   font-weight: bold;
 }
@@ -265,6 +287,15 @@ onMounted(async () => {
   font-size: 1.25rem;
   text-align: left;
 }
+.task-overview-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: .5rem;
+}
+.loading-container {
+  text-align: center;
+}
 .legend-container {
   display: flex;
   flex-direction: row;
@@ -277,6 +308,11 @@ onMounted(async () => {
   flex-direction: row;
   align-items: center;
 }
+.legend-description {
+  text-align: center;
+  margin-top: 1rem;
+  margin-bottom: 3rem;
+}
 .circle {
   border-color: white;
   display: inline-block;
@@ -286,5 +322,8 @@ onMounted(async () => {
   width: 25px;
   vertical-align: middle;
   margin-right: 10px;
+}
+.extra-info-title {
+  font-size: 2rem;
 }
 </style>
