5438a145500a825b78973f3f8769ee79df1eeae3
Adam Richie-Halford <richiehalford@gmail.com>
2023-09-15 16:32:44 -0700

    Remove closing span tag

--- src/components/CardAdministration.vue
+++ src/components/CardAdministration.vue
@@ -41,12 +41,14 @@
 								<Button v-tooltip.top="'See completion details'" severity="secondary" text raised label="Progress"
 									aria-label="Completion details" size="small" />
 							</router-link>
-							<router-link
-							  :to="{ name: 'ScoreReport', params: { administrationId: props.id, orgId: node.data.id, orgType: node.data.orgType } }"
-								v-slot="{ href, route, navigate }">
-									<Button v-tooltip.top="'See Scores'" severity="secondary" text raised label="Scores"
-										aria-label="Scores" size="small" />
-								</span>
+							<router-link :to="{
+								name: 'ScoreReport', params: {
+									administrationId: props.id, orgId: node.data.id, orgType:
+										node.data.orgType
+								}
+							}" v-slot="{ href, route, navigate }">
+								<Button v-tooltip.top="'See Scores'" severity="secondary" text raised label="Scores" aria-label="Scores"
+									size="small" />
 							</router-link>
 						</span>
 					</template>
