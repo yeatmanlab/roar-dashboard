1f2f705aa44aab8601ecc946b60219ef1d81e203
Adam Richie-Halford <richiehalford@gmail.com>
2023-09-22 13:35:09 -0700

    Update firestore rules to allow district/school admins to see administrations, users, orgs, and progress

--- firebase/emulate_admin.sh
+++ firebase/emulate_admin.sh
@@ -2,5 +2,5 @@ PREVIOUS_DIR=$(pwd)
 cd firebase/admin
 # firebase use --clear
 # firebase use demo-gse-roar-admin
-firebase emulators:start --project demo-gse-roar-admin --import export-20230910-122923UTC &
+firebase emulators:start --project demo-gse-roar-admin --import export-20230922-111607UTC &
 cd $PREVIOUS_DIR
