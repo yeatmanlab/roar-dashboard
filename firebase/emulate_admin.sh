9b828d57ced5767785ddf61e0acc7dc98c844a6b
Philip Ziolkowski <philipz4848@gmail.com>
2023-09-28 17:19:12 -0700

    Sre mobile enhancements

--- firebase/emulate_admin.sh
+++
@@ -1,6 +0,0 @@
-PREVIOUS_DIR=$(pwd)
-cd firebase/admin
-# firebase use --clear
-# firebase use demo-gse-roar-admin
-firebase emulators:start --project demo-gse-roar-admin --import export-20230922-111607UTC &
-cd $PREVIOUS_DIR
