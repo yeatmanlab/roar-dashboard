dfe8331d044eb1b9092d870183588ae1292bc2a8
Elijah Kelly <kellyel@stanford.edu>
2023-11-08 13:54:34 -0800

    Ad raw score ranges to report

--- src/pages/ScoreReport.vue
+++ src/pages/ScoreReport.vue
@@ -92,7 +92,8 @@
           <p class="task-description">ROAR - Word evaluates a student's ability to quickly and automatically recognize
             individual words. To read fluently, students must master fundamental skills of decoding and automaticity. This
             test measures a student's ability to detect real and made-up words, which can then translate to a student's
-            reading levels and need for support. </p>
+            reading levels and need for support. The student's score will range between 100-900 and can be viewed by
+            selecting 'Raw Score' on the table above. </p>
         </div>
         <div v-if="allTasks.includes('sre')" class="task-card">
           <div class="task-title">ROAR-SENTENCE</div>
@@ -102,7 +103,8 @@
             Poor fluency can make it harder for students to understand what they're reading. Students who don't receive
             support for their basic reading skills may find it challenging to improve their overall reading ability. This
             assessment is helpful for identifying students who may struggle with reading comprehension due to difficulties
-            with decoding words accurately or reading slowly and with effort.</p>
+            with decoding words accurately or reading slowly and with effort. The student's score will range between 
+            0-130 and can be viewed by selecting 'Raw Score' on the table above.</p>
         </div>
 
         <div>
