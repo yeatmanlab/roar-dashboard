264d9a652e050ba147d1b23896c49cc2f04889b7
Adam Richie-Halford <richiehalford@gmail.com>
2023-11-08 10:44:52 -0800

    Use rest api in clever landing page:

--- src/pages/CleverLanding.vue
+++ src/pages/CleverLanding.vue
@@ -7,25 +7,29 @@
 <script setup>
 import { useAuthStore } from '@/store/auth.js';
 import { useRouter } from 'vue-router';
-import { storeToRefs } from 'pinia';
 import _get from 'lodash/get';
-import AppSpinner from '../components/AppSpinner.vue';
+import AppSpinner from '@/components/AppSpinner.vue';
+import { fetchDocById } from '@/helpers/query/utils';
 
 const router = useRouter();
 const authStore = useAuthStore();
-const { roarfirekit } = storeToRefs(authStore);
 
 let userDataCheckInterval;
 
-function checkForUserType() {
-  if (roarfirekit.value.userData) {
-    const userType = _get(roarfirekit.value, 'userData.userType')
-    if (userType !== 'guest') {
+async function checkForUserType() {
+  try {
+    const userData = await fetchDocById('users', authStore.uid);
+    const userType = _get(userData, 'userType');
+    if (userType && userType !== 'guest') {
       clearInterval(userDataCheckInterval)
       router.push({ name: "Home" })
     }
+  } catch (error) {
+    if (error.code !== 'ERR_BAD_REQUEST') {
+      throw error;
+    }
   }
-}
+};
 
 userDataCheckInterval = setInterval(checkForUserType, 500);
 </script>
