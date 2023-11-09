ec4083f2ba94b979dc1f1370f3126bccd3114b24
Elijah Kelly <kellyel@stanford.edu>
2023-11-09 08:59:21 -0800

    Add letter blurbs to score report

--- src/pages/ScoreReport.vue
+++ src/pages/ScoreReport.vue
@@ -17,6 +17,10 @@
         <h2>IN THIS REPORT...</h2>
         <span>You will receive a breakdown of your classroom's ROAR scores across each of the domains tested. </span>
         <div class="task-overview-container">
+          <div v-if="allTasks.includes('letter')" class="task-blurb">
+            <span class="task-header">ROAR-Letter Sound Matching (ROAR-Letter)</span> assesses knowledge of letter names
+            and sounds.
+          </div>
           <div v-if="allTasks.includes('pa')" class="task-blurb">
             <span class="task-header">ROAR-Phonological Awareness (ROAR-Phoneme)</span> assesses some of the most
             foundational skills for reading: mapping letters to their corresponding sounds. This skill is crucial for
@@ -89,6 +93,16 @@
           :orgId="orgId"
         />
         <!-- In depth breakdown of each task -->
+        <div v-if="allTasks.includes('letter')" class="task-card">
+          <div class="task-title">ROAR-LETTER</div>
+          <span style="text-transform: uppercase;">Letter Names and Letter-Sound Matching</span>
+          <p class="task-description">ROAR-Letter assesses a student’s knowledge of letter names and letter sounds.
+            Knowing letter names supports the learning of letter sounds, and knowing letter sounds supports the learning
+            of letter names. Initial knowledge of letter names and letter sounds on entry to kindergarten has been shown
+            to predict success in learning to read. Learning the connection between letters and the sounds they represent
+            is fundamental for learning to decode and spell words. This assessment provides educators with valuable
+            insights to customize instruction and address any gaps in these foundational skills.</p>
+        </div>
         <div v-if="allTasks.includes('pa')" class="task-card">
           <div class="task-title">ROAR-PHONEME</div>
           <span style="text-transform: uppercase;">Phonological Awareness</span>
