59421a01e36237b225f26b7c831735d3e8031e71
Kyle <ksmontville@gmail.com>
2023-11-11 08:54:01 -0800

    Add env variables

--- cypress/e2e/super-admin/authenticate-oauth-clever.cy.js
+++
@@ -1,9 +0,0 @@
-describe('The user can log in using Clever as an identity provider.', () => {
-  it('passes', () => {
-    cy.visit('https://localhost:5173/')
-
-    cy.get("button").contains("Clever").click()
-
-    //   Need further setup here with OAuth integration (https://docs.cypress.io/guides/end-to-end-testing/auth0-authentication
-  })
-})
