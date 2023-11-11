9f71373b8aabf06e2abd510929a1dbe4245688c1
Kyle <ksmontville@gmail.com>
2023-11-11 13:27:58 -0800

    Add tests.

--- cypress/e2e/super-admin/authenticateOauthGoogle.cy.js
+++ cypress/e2e/super-admin/authenticateOauthGoogle.cy.js
@@ -1,10 +1,13 @@
 describe('The user can log in using Google as an identity provider.', () => {
   it('passes', () => {
+    console.log(Cypress.env("googleRefreshToken"))
+    console.log(Cypress.env("googleClientId"))
+    console.log(Cypress.env("googleClientSecret"))
     cy.visit('/')
 
     cy.get("button").contains("Google").click()
     cy.get("iframe").should('be.visible')
-    // cy.loginByGoogleApi()
+    cy.loginByGoogleApi()
 
   })
 })
---
+++ cypress/e2e/super-admin/authenticateUsernamePassword.cy.js
@@ -0,0 +1,8 @@
+describe('The super admin can log in using a standard username and password.', () => {
+  it('passes', () => {
+    const username = Cypress.env('superAdminUsername')
+    const password = Cypress.env('superAdminPassword')
+
+    cy.login(username, password)
+  })
+})
