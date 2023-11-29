3cc76c3e17076f99e305613c8de4d935f5c41027
Kyle <ksmontville@gmail.com>
2023-11-29 11:31:45 -0800

    Fix createAdministrations spec.

--- cypress/e2e/super-admin/create-administrations/createAdministration.spec.cy.js
+++ cypress/e2e/super-admin/create-administrations/createAdministration.spec.cy.js
@@ -6,18 +6,19 @@ describe('The admin user can create an administration and assign it to a distric
 
     cy.navigateTo('/create-administration')
     cy.get('[data-cy="input-administration-name"]').type(Cypress.env('testAdministrationName'))
-    cy.get('[data-cy="input-calendar"]').click().get('span').contains(today).click().type('{rightarrow}{enter}{esc}')
+    cy.get('[data-cy="input-calendar"]').click().get('.p-datepicker-today > span').contains(today).click().type('{rightarrow}{rightarrow}{enter}{esc}')
 
     cy.selectTestOrgs()
-    cy.get('ul > li').contains("Kyle Test Group").click()
+    cy.get('ul > li').contains("Elijah Test Group").click()
 
   //   Temporary fix until assessments properly load without needing to refresh the list.
     cy.get('[data-cy="button-refresh-assessments"]', {timeout: 10000}).click()
 
     cy.get('span').contains("morphology-default", {timeout: 10000}).dblclick()
+    cy.get('span').contains("kNmRHn8Z6nIejzu4nKWU", {timeout: 10000}).dblclick()
     cy.get('[data-cy="button-create-administration"]', {timeout: 10000}).click()
 
-    cy.get('[data-cy="h2-card-admin-title"', {timeout: 10000}).should('contain.text', Cypress.env("testAdministrationName"))
+    cy.get('[data-cy="h2-card-admin-title"', {timeout: 20000}).should('contain.text', Cypress.env("testAdministrationName"))
     cy.log("Administration successfully created.")
 
   })
