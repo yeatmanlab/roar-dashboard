1e764db6094968d10a4afa75cc4b60cf45e6a8c8
Lucas  Song <lucas7song@gmail.com>
2023-11-29 15:41:42 -0800

    update gameslist json and updated testing logic

--- cypress/e2e/participant/playGames.cy.js
+++ cypress/e2e/participant/playGames.cy.js
@@ -1,4 +1,4 @@
-import { games } from "./gamesList.js";
+import {games } from "./gamesList"
 
 describe("Cypress tests to play games as a participant", () => {
     games.forEach((game) => {
@@ -11,59 +11,65 @@ describe("Cypress tests to play games as a participant", () => {
 
             cy.login(test_login, test_pw);
 
+            cy.get(".p-dropdown-trigger", { timeout: 10000 })
+                .should("be.visible")
+                .click();
+            cy.get(".p-dropdown-item", { timeout: 10000 })
+                .contains("Vocab numTrialsTotal 3")
+                .should("be.visible")
+                .click();
+            // cy.get(".p-dropdown-item").contains("numTrialsTotal").click();
+            // cy.get('.p-dropdown-item', {timeout: 10000}).should('be.visible').click();
+
+            cy.get(".p-tabview").contains(game.name);
+            cy.visit(`/game/${game.id}`);
+
+            // cy.contains("Preparing your game")
+
+            cy.get(".jspsych-btn", { timeout: 60000 })
+                .should("be.visible")
+                .click();
+
+            // handles error where full screen throws a permissions error
             cy.wait(1000);
+            Cypress.on("uncaught:exception", () => {
+                return false;
+            });
 
-            cy.get(".p-dropdown-label").click();
-            cy.get(".p-dropdown-item").contains("numTrialsTotal").click();
+            // if the game prompts some setup, make the choice
+            if (game.setUpChoice) {
+                cy.get(game.setUpChoice, { timeout: 10000 })
+                    .should("be.visible")
+                    .first()
+                    .click();
+            }
 
-            cy.get(".p-tabview").contains(game.name).click();
-            cy.get(".pointer").filter(":visible").click();
-            cy.url().should('include', `/game/${game.id}`) // => true
-            // assert(url.includes(game.id))
-            // cy.contains("GO")
+            // clicks through first introduction pages
+            for (let i = 0; i < game.introIters; i++) {
+                cy.get(game.introBtn).click();
+                cy.wait(1000);
+            }
 
-            // cy.wait(8000);
-            // cy.contains("GO").click();
+            playROARGame(game)
 
-            // // handles error where full screen throws a permissions error
-            // Cypress.once("uncaught:exception", () => {
-            //     return false;
-            // });
-
-            // // if the game prompts some setup, make the choice
-            // if (game.setUpChoice.length > 0) {
-            //     cy.get(game.setUpChoice).first().click();
-            // }
-
-            // // clicks through first introduction pages
-            // cy.get(".continue").click();
-            // cy.wait(1000);
-            // cy.get(".continue").click();
-            // cy.wait(1000);
-            // cy.get(".continue").click();
-            // cy.wait(1000);
-
-            // playROARGame(game.clickableItem, game.correctAnswer, game.numIter);
-
-            // //     // intro game mode
         });
     });
 });
 
-function playROARGame(clickableItem, correctAnswer, numIter) {
-    for (let i = 0; i < numIter; i++) {
-        continueClickingVocabImg(clickableItem, correctAnswer);
+function playROARGame(game) {
+    for (let i = 0; i < game.numIter; i++) {
+        chooseStimulusOrContinue(game)
     }
 }
 
-function continueClickingVocabImg(clickableItem, correctAnswer) {
+function chooseStimulusOrContinue(game) {
     cy.get("body").then((body) => {
-        if (!body.find(".continue").length > 0) {
-            body.find(clickableItem).click();
-            cy.wait(50);
-            continueClickingVocabImg(clickableItem, correctAnswer);
+        if (body.find(game.introBtn).length > 0) {
+            body.find(game.introBtn).click();
         } else {
-            body.find(".continue").click();
+            body.find(game.clickableItem).first().click();
+            cy.wait(400);
+            chooseStimulusOrContinue(game);
         }
     });
 }
