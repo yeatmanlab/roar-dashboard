53a0c6850986282a0e17d6edee324a64369ec3de
Lucas  Song <lucas7song@gmail.com>
2023-12-01 13:02:57 -0800

    add keypress gameplay functions

--- cypress/e2e/participant/buttonGamesList.js
+++ cypress/e2e/participant/buttonGamesList.js
@@ -10,16 +10,16 @@ export const games = [
     //     clickableItem: ".vocab_img",
     //     numIter: 4,
     // },
-    // {
-    //     name: "ROAR Written Vocabulary",
-    //     id: "cva",
-    //     startBtn: ".jspsych-fullscreen-btn",
-    //     introBtn: ".go-button",
-    //     introIters: 2,
-    //     setupChoice: "",
-    //     clickableItem: "button",
-    //     numIter: 4,
-    // },
+    {
+        name: "ROAR Written Vocabulary",
+        id: "cva",
+        startBtn: ".jspsych-btn",
+        introBtn: ".go-button",
+        introIters: 2,
+        setupChoice: "",
+        clickableItem: "button",
+        numIter: 4,
+    },
     // {
     //     name: "Letter",
     //     id: "letter",
@@ -44,41 +44,23 @@ export const games = [
     //     numIter: 4,
     // },
     // // pa has a different start button than the others (.continue i think)
-    {
-        name: "ROAR-Phoneme",
-        id: "pa",
-        startBtn: ".instructionCanvasNS",
-        startBtn2: ".jspsych-btn",
-        setUpChoice: "",
-        introIters: 2,
-        introBtn: ".continue",
-        clickableItem: ".jspsych-audio-button-response-button",
-        correctChoice: "",
-        numIter: 2,
-    },
-    // sre isnt working?
     // {
-    //     name: "ROAR-Sentence",
-    //     id: "sre",
-    //     startBtn: ".jspsych-fullscreen-btn",
-    //     introBtn: ".go-button",
-    //     introIters: 3,
+    //     name: "ROAR-Phoneme",
+    //     id: "pa",
+    //     startBtn: ".instructionCanvasNS",
+    //     startBtn2: ".jspsych-btn",
     //     setUpChoice: "",
-    //     clickableItem: "button",
+    //     introIters: 2,
+    //     introBtn: ".continue",
+    //     clickableItem: ".jspsych-audio-button-response-button",
     //     correctChoice: "",
-    //     numIter: 4,
-    // },
-    // swr isnt working?
-    // {
-    //     name: "ROAR-Word",
-    //     id: "swr",
-    //     startBtn: ".jspsych-fullscreen-btn",
-    //     introBtn: ".go-button",
-    //     introIters: 3,
-    //     setUpChoice: "",
-    //     clickableItem: "button",
-    //     correctChoice: "",
-    //     numIter: 4,
+    //     numIter: 2,
     // },
 ]
+
+// variant IDS
+// vocab: VimH8oXM99UToXydiDK4
+// cva: 5OaEGDBHAxU4kSebvyix 
+// letter: E89L0yam9SJ9g5vDFzzr
+// multichoice: UD9GC7PkVRX8oJvqysTL
 
---
+++ cypress/e2e/participant/keyPressGamesList.js
@@ -0,0 +1,31 @@
+export const keypressGames = [
+    // sre isnt working?
+    {
+        name: "ROAR-Sentence",
+        id: "sre",
+        startBtn: ".jspsych-btn",
+        startBtn2: ".jspsych-btn",
+        introBtn: ".go-button",
+        introIters: 0,
+        setUpChoice: "",
+        correctChoice: "",
+        numIter: 40,
+    },
+    // // swr isnt working?
+    // {
+    //     name: "ROAR-Word",
+    //     id: "swr",
+    //     startBtn: ".jspsych--btn",
+    //     introBtn: ".go-button",
+    //     introIters: 3,
+    //     setUpChoice: "",
+    //     clickableItem: "button",
+    //     correctChoice: "",
+    //     numIter: 4,
+    // },
+];
+
+// variant ids:
+
+// sre: AoMCE4POPqMjyrmXruXf
+// swr: q7Fjeyh0MmksYANJQtTT
