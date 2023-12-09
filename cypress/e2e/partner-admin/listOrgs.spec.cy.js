47f49a85dc0aac2d5c89d3a46a63e6909579d64f
Kyle <ksmontville@gmail.com>
2023-12-08 17:54:33 -0800

    Remove unused comments.

--- cypress/e2e/partner-admin/listOrgs.spec.cy.js
+++ cypress/e2e/partner-admin/listOrgs.spec.cy.js
@@ -1,32 +1,3 @@
-// const orgs = [
-//   {'tabName': 'Districts', 'orgName': Cypress.env('testPartnerDistrictName')},
-//   {'tabName': 'Schools', 'orgName': Cypress.env('testPartnerSchoolName')},
-//   {'tabName': 'Classes', 'orgName': Cypress.env('testPartnerClassName')},
-//   {'tabName': 'Groups', 'orgName': Cypress.env('testPartnerGroupName')}
-// ]
-//
-// function checkOrgsExists(orgs) {
-//   for (let org of orgs) {
-//     cy.get('ul > li', {timeout: Cypress.env('timeout')})
-//       .contains(org.tabName).click()
-//     cy.log("Tab " + org.tabName + " found.")
-//
-//     cy.get('div', {timeout: Cypress.env('timeout')})
-//       .should('contain.text', org.orgName, {timeout: Cypress.env('timeout')})
-//     cy.log(`${org.orgName} exists.`)
-//   }
-// }
-//
-// describe('The partner admin user can navigate to the list organizations page, ' +
-//     'and can see which they organizations they are associated with', () => {
-//   it('Activates the admin sidebar, clicks List Orgs, then clicks through the various tabs.', () => {
-//
-//     cy.login(Cypress.env('partnerAdminUsername'), Cypress.env('partnerAdminPassword'))
-//     cy.navigateTo('/list-orgs')
-//     checkOrgsExists(orgs)
-//   })
-// })
-
 const orgs = [
   { tabName: 'Districts', orgName: Cypress.env('testPartnerDistrictName') },
   { tabName: 'Schools', orgName: Cypress.env('testPartnerSchoolName') },
