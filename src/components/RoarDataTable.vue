f914d7a28d01835441b3cb6f35f6181797a2f322
Elijah Kelly <kellyel@stanford.edu>
2023-12-14 16:34:05 -0800

    correct tag color

--- src/components/RoarDataTable.vue
+++ src/components/RoarDataTable.vue
@@ -101,7 +101,7 @@
               :severity="_get(colData, col.severityField)"
               :value="_get(colData, col.field)"
               :icon="_get(colData, col.iconField)"
-              :style="`min-width: 2rem; color:white`"
+              :style="`background-color: ${_get(colData, col.tagColor)}; min-width: 2rem;`"
               rounded
             />
             <div
