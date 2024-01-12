b1917d8e324336db4d53b08d0a79c09407e2af84
Kyle <ksmontville@gmail.com>
2024-01-12 13:21:24 -0800

    Remove defunct tests

--- cypress/e2e/super-admin/authenticateUsernamePassword.cy.js
+++
@@ -1,8 +0,0 @@
-describe('The super admin can log in using a standard username and password.', () => {
-  it('passes', () => {
-    const username = Cypress.env('superAdminUsername');
-    const password = Cypress.env('superAdminPassword');
-
-    cy.login(username, password);
-  });
-});
