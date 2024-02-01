31e445429783d6c09637a52e03afdd8bf14ee2c9
Elijah Kelly <kellyel@stanford.edu>
2024-02-01 12:36:50 -0800

    diable sort/filter for other tables

--- src/components/ListOrgs.vue
+++ src/components/ListOrgs.vue
@@ -58,6 +58,7 @@
               :page-limit="pageLimit"
               :total-records="totalRecords"
               :loading="isLoading || isLoadingCount || isFetching || isFetchingCount"
+              :allow-filtering="false"
               @page="onPage($event)"
               @sort="onSort($event)"
               @export-all="exportAll"
@@ -234,21 +235,21 @@ const exportAll = async () => {
 
 const tableColumns = computed(() => {
   const columns = [
-    { field: 'name', header: 'Name', dataType: 'string', pinned: true },
-    { field: 'abbreviation', header: 'Abbreviation', dataType: 'string' },
-    { field: 'address.formattedAddress', header: 'Address', dataType: 'string' },
-    { field: 'tags', header: 'Tags', dataType: 'array', chip: true },
+    { field: 'name', header: 'Name', dataType: 'string', pinned: true, sort: false },
+    { field: 'abbreviation', header: 'Abbreviation', dataType: 'string', sort: false },
+    { field: 'address.formattedAddress', header: 'Address', dataType: 'string', sort: false },
+    { field: 'tags', header: 'Tags', dataType: 'array', chip: true, sort: false },
   ];
 
   if (['districts', 'schools'].includes(activeOrgType.value)) {
     columns.push(
-      { field: 'mdrNumber', header: 'MDR Number', dataType: 'string' },
-      { field: 'ncesId', header: 'NCES ID', dataType: 'string' },
+      { field: 'mdrNumber', header: 'MDR Number', dataType: 'string', sort: false },
+      { field: 'ncesId', header: 'NCES ID', dataType: 'string', sort: false },
     );
   }
 
   if (['districts', 'schools', 'classes'].includes(activeOrgType.value)) {
-    columns.push({ field: 'clever', header: 'Clever', dataType: 'boolean' });
+    columns.push({ field: 'clever', header: 'Clever', dataType: 'boolean', sort: false });
   }
 
   columns.push({
