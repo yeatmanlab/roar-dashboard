c6f8673685559cabe7ac0f873e2e55ec365aaa34
Elijah Kelly <kellyel@stanford.edu>
2024-02-02 11:08:38 -0800

    Update arguments for supportLevel function

--- src/pages/ScoreReport.vue
+++ src/pages/ScoreReport.vue
@@ -917,7 +917,7 @@ const runsByTaskId = computed(() => {
       percentScore = _get(scores, scoreFieldBelowSixth(taskId));
     }
     const grade = user?.data?.grade === 'Kindergarten' ? 0 : parseInt(user?.data?.grade);
-    const { support_level } = getSupportLevel(grade, percentScore, taskId);
+    const { support_level } = getSupportLevel(grade, percentScore, rawScore, taskId);
     const run = {
       // A bit of a workaround to properly sort grades in facetted graphs (changes Kindergarten to grade 0)
       grade: user?.data?.grade === 'Kindergarten' ? 0 : parseInt(user?.data?.grade),
