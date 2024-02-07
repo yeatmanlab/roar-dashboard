a3e97150c2c376ad2799fe668c1e95556757ce53
Emily Arteaga <62304493+Emily-ejag@users.noreply.github.com>
2024-02-07 15:10:59 -0800

    adding captcha key for this particular PR

--- src/main.js
+++ src/main.js
@@ -111,7 +111,7 @@ app.use(pinia);
 app.use(router);
 app.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyA2Q2Wq5na79apugFwoTXKyj-RTDDR1U34',
+    key: '6LefrGopAAAAADcrBVk9kAMxdTF3_0v7Ydc3UGUe',
     libraries: 'places',
   },
 });
