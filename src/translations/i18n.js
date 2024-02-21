579724cea06107804da3a189d32e4a8e65ea4ad5
Kyle <ksmontville@gmail.com>
2024-02-21 14:08:39 -0800

    Format

--- src/translations/i18n.js
+++ src/translations/i18n.js
@@ -5,26 +5,24 @@ import esCOTranslations from './es/es-co-componentTranslations.json';
 
 const locale = window.navigator.language;
 const messages = {
-  "en-US": enTranslations,
-  "en": enTranslations,
-  "es": esTranslations,
-  "es-CO": esCOTranslations,
+  'en-US': enTranslations,
+  en: enTranslations,
+  es: esTranslations,
+  'es-CO': esCOTranslations,
 };
 
 const getTranslations = () => {
   if (messages[locale]) {
-    console.log("Language supported, using " + locale + ".")
+    console.log('Language supported, using ' + locale + '.');
     return messages[locale];
+  } else if (locale.includes('es')) {
+    console.log('Language not supported, using default es.');
+    return messages['es'];
+  } else {
+    console.log('Language not supported, using default en-US.');
+    return messages['en-US'];
   }
-  else if (locale.includes("es")) {
-    console.log("Language not supported, using default es.")
-    return messages["es"];
-  }
-  else {
-    console.log("Language not supported, using default en-US.")
-    return messages["en-US"];
-  }
-}
+};
 
 export const i18n = createI18n({
   locale: locale,
