5fbd9963623f2eccfee36fb9c939d33416f05f77
Kyle <ksmontville@gmail.com>
2024-02-21 14:04:02 -0800

    Add error handling to translations.

--- src/translations/i18n.js
+++ src/translations/i18n.js
@@ -1,18 +1,34 @@
 import { createI18n } from 'vue-i18n';
 import enTranslations from './en/en-componentTranslations.json';
 import esTranslations from './es/es-componentTranslations.json';
+import esCOTranslations from './es/es-co-componentTranslations.json';
 
 const locale = window.navigator.language;
 const messages = {
-  'en-US': enTranslations,
-  en: enTranslations,
-  es: esTranslations,
+  "en-US": enTranslations,
+  "en": enTranslations,
+  "es": esTranslations,
+  "es-CO": esCOTranslations,
 };
+
+const getTranslations = () => {
+  if (messages[locale]) {
+    console.log("Language supported, using " + locale + ".")
+    return messages[locale];
+  }
+  else if (locale.includes("es")) {
+    console.log("Language not supported, using default es.")
+    return messages["es"];
+  }
+  else {
+    console.log("Language not supported, using default en-US.")
+    return messages["en-US"];
+  }
+}
+
 export const i18n = createI18n({
   locale: locale,
-  fallbackLocale: 'en',
-  messages: messages[locale],
+  fallbackLocale: 'en-US',
+  messages: getTranslations(),
   legacy: false,
 });
-
-console.log(locale, messages[locale]);
