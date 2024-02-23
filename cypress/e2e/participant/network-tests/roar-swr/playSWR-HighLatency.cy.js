56e754db7993e91afb6d15af7e790e293f885765
Kyle <ksmontville@gmail.com>
2024-02-23 15:25:05 -0800

    Activate 2G and HighLatency tests for SWR

--- cypress/e2e/participant/network-tests/roar-swr/playSWR-HighLatency.cy.js
+++ cypress/e2e/participant/network-tests/roar-swr/playSWR-HighLatency.cy.js
@@ -2,24 +2,28 @@ import { playSWRGame } from '../../../../support/helperFunctions/roar-swr/swrHel
 
 const timeout = Cypress.env('timeout');
 
-describe('Testing playthrough of SWR as a participant under a 3G Mobile connection or similar network of low bandwidth.', () => {
+describe('Testing playthrough of SWR as a participant under a 2G Mobile connection or similar network of low bandwidth.', () => {
   it('ROAR-Word Playthrough Test', () => {
     cy.login(Cypress.env('participantUsername'), Cypress.env('participantPassword'));
 
     // Add wait time to simulate a slow network connection
-    cy.wait(0.3 * timeout);
+    cy.wait(0.5 * timeout);
     cy.visit('/');
 
-    cy.get('.p-dropdown-trigger', { timeout: timeout }).should('be.visible').click();
-    cy.get('.p-dropdown-item', { timeout: timeout })
+    cy.get('.p-dropdown-trigger', { timeout: 6 * timeout })
+      .should('be.visible')
+      .click();
+    cy.get('.p-dropdown-item', { timeout: 6 * timeout })
       .contains(Cypress.env('testRoarAppsAdministration'))
       .should('be.visible')
       .click();
 
-    cy.get('.p-tabview').contains('ROAR-Word');
+    cy.get('.p-tabview', { timeout: 6 * timeout }).contains('ROAR-Word');
     cy.visit(`/game/swr`);
 
-    cy.get('.jspsych-btn', { timeout: timeout }).should('be.visible').click();
+    cy.get('.jspsych-btn', { timeout: 6 * timeout })
+      .should('be.visible')
+      .click();
 
     // handles error where full screen throws a permissions error
     cy.wait(0.1 * timeout);
