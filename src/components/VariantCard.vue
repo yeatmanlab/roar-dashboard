70a1b3e5a76f337565ad782b1b0a5cd79786fa4f
Elijah Kelly <kellyel@stanford.edu>
2024-03-07 10:23:07 -0800

    initial framework for variant card

---
+++ src/components/VariantCard.vue
@@ -0,0 +1,16 @@
+<template>
+  {{ variant.id }}
+</template>
+<script setup>
+const props = defineProps({
+  variant: {
+    required: true,
+    type: Object,
+  },
+  hasControls: {
+    required: false,
+    type: boolean,
+    default: false,
+  },
+});
+</script>
