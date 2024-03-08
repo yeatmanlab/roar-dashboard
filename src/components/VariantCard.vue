8daaaa19388c47945a49a56b58f042c5cf3321a8
Emily Arteaga <62304493+Emily-ejag@users.noreply.github.com>
2024-03-08 10:13:24 -0800

    task variant work

--- src/components/VariantCard.vue
+++ src/components/VariantCard.vue
@@ -1,8 +1,20 @@
 <template>
-  {{ variant.id }}
+  <div class="flex-1 flex flex-column gap-2">
+    <span class="font-bold" style="margin-left: 0.625rem">{{ task.name }}</span>
+    <div class="flex align-items-center gap-2">
+      <i class="pi pi-tag text-sm" style="margin-left: 0.625rem"></i>
+      <p>Variant: {{ variant.name }}</p>
+      <p>Id: {{ variant.id }}</p>
+    </div>
+  </div>
 </template>
+
 <script setup>
 const props = defineProps({
+  task: {
+    type: Object,
+    required: true
+  },
   variant: {
     required: true,
     type: Object,
