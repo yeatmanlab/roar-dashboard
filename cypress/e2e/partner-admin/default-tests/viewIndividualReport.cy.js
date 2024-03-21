e1fecebe4e2f148fff07a9816a0e6f93245b000e
Emily Arteaga <62304493+Emily-ejag@users.noreply.github.com>
2024-03-21 13:07:55 -0700

    increasing timeout

--- cypress/e2e/partner-admin/default-tests/viewIndividualReport.cy.js
+++ cypress/e2e/partner-admin/default-tests/viewIndividualReport.cy.js
@@ -39,14 +39,14 @@ describe('The partner admin can view score reports for a given administration.',
     clickScoreButton();
     cy.checkUserList(testUserList);
     checkAssignmentColumns(testAssignments);
-    cy.get('button', { timeout: 4 * timeout })
+    cy.get('button', { timeout: 6 * timeout })
       .contains('Report')
       .click();
-    cy.get('div', { timeout: 4 * timeout }).contains('Individual Score Report');
-    cy.get('button', { timeout: 4 * timeout })
+    cy.get('div', { timeout: 6 * timeout }).contains('Individual Score Report');
+    cy.get('button', { timeout: 6 * timeout })
       .contains('Expand All Sections')
       .click();
-    cy.get('button', { timeout: 4 * timeout }).contains('Export to PDF');
-    cy.get('div', { timeout: 4 * timeout }).contains('The ROAR assessements return 3 kinds of scores');
+    cy.get('button', { timeout: 6 * timeout }).contains('Export to PDF');
+    cy.get('div', { timeout: 6 * timeout }).contains('The ROAR assessements return 3 kinds of scores');
   });
 });
