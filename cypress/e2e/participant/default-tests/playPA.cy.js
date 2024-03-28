2e5b54143033ed013e88403ef3c668882e2892c7
Emily Arteaga <62304493+Emily-ejag@users.noreply.github.com>
2024-03-28 13:13:15 -0700

    prettier

--- cypress/e2e/participant/default-tests/playPA.cy.js
+++ cypress/e2e/participant/default-tests/playPA.cy.js
@@ -3,11 +3,11 @@ import { playPA } from '../../../support/helper-functions/roar-pa/paHelpers';
 const timeout = Cypress.env('timeout');
 const startText = 'In this game we are going to look for words that BEGIN with the same sound.';
 const endBlockText = {
-  endText1: "Take a break if needed",
-  endText2: "I have been swimming so much",
-  endText3: "You have helped me and all my friends!",
+  endText1: 'Take a break if needed',
+  endText2: 'I have been swimming so much',
+  endText3: 'You have helped me and all my friends!',
 };
-const breakBlockText = "Take a break if needed";
+const breakBlockText = 'Take a break if needed';
 
 describe('Testing playthrough of ROAR-Phoneme as a participant', () => {
   it(`ROAR-Phoneme Playthrough Test`, () => {
