c218dab56f003b73adc9171d4d4baebc3113a9cb
Emily Arteaga <62304493+Emily-ejag@users.noreply.github.com>
2024-05-06 14:22:57 -0700

    Update cypress.config.js
    Co-authored-by: Kyle Montville <ksmontville@gmail.com>

--- cypress.config.js
+++ cypress.config.js
@@ -81,7 +81,7 @@ module.exports = defineConfig({
     testRoarAppsAdministrationId: '',
     testOptionalRoarAppsAdministration: 'Cypress Test Optional Roar Apps Administration',
     testOptionalRoarAppsAdministrationId: '',
-    testSpanishRoarAppsAdministration: 'Cypress Test Spanish Apps Administrations',
+    testSpanishRoarAppsAdministration: 'Cypress Test Spanish Roar Apps Administration',
     testSpanishRoarAppsAdministrationId: '',
     // Generate a list of test users CypressTestStudent0, CypressTestStudent1, ..., CypressTestStudent50
     testUserList: Array.from({ length: 51 }, (_, i) => `CypressTestStudent${i}`),
