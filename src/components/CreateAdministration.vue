74a02eab4b8c20a7a40f213259d2926642db82a0
Adam Richie-Halford <richiehalford@gmail.com>
2024-05-14 16:49:37 -0700

    Disable eslint error

--- src/components/CreateAdministration.vue
+++ src/components/CreateAdministration.vue
@@ -544,6 +544,7 @@ watch([preExistingAdminInfo, allVariants], ([adminInfo, allVariantInfo]) => {
 });
 
 const removeNull = (obj) => {
+  // eslint-disable-next-line no-unused-vars
   return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v !== null));
 };
 
