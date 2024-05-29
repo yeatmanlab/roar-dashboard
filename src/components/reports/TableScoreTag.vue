628591140bd2f3707d635f339f0824c0828a9490
emily-ejag <emily.artegar@gmail.com>
2024-05-28 17:20:08 -0700

    move comment up

--- src/components/reports/TableScoreTag.vue
+++ src/components/reports/TableScoreTag.vue
@@ -115,11 +115,11 @@ function getFlags(colData, taskId) {
       const reliabilityFlags = Object.keys(flags).map((flag) => {
         return flagMessages[flag] || _lowerCase(flag);
       });
+      // Join the returned flags with a newline character, then add two newlines for spacing
       return 'Engagement Flags: ' + reliabilityFlags.join('\n') + '\n\n';
     } else {
       return 'Unreliable \n\n';
     }
-    // Join the returned flags with a newline character, then add two newlines for spacing
   } else {
     return '';
   }
