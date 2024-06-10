8d38e702b956c78ffc9e45cac84a86a7c953f8ba
Kyle <ksmontville@gmail.com>
2024-06-10 13:58:59 -0700

    remove country flags from language selector

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -1,30 +1,17 @@
 <template>
-  <div class="flex align-items-center gap-1">
+  <div class="flex flex-column align-items-center gap-1">
     <PvDropdown
       v-model="$i18n.locale"
       class=""
       :options="languageDropdownOptions"
       option-label="name"
       option-value="value"
-      placeholder="Select language"
       :highlight-on-select="true"
     >
       <template #header>
-        <div class="m-2 font-bold uppercase text-sm text-gray-500">Set Locale</div>
-      </template>
-      <template #value="locale">
-        <div v-if="locale.value" class="flex flex-row justify-content-center align-items-center">
-          <country-flag :country="getCountryFlag(locale.value)" class="" size="small" />
-        </div>
-        <span v-else>
-          {{ locale.placeholder }}
-        </span>
-      </template>
-      <template #option="country">
-        <div class="flex flex-row justify-content-start align-items-center">
-          <country-flag :country="country.option.code" class="mr-2" size="small" />
-          <span>{{ country.option.name }}</span>
-        </div>
+        <small class="text-gray-400"
+          ><strong>{{ $t('authSignIn.selectLanguage') }}</strong></small
+        >
       </template>
     </PvDropdown>
   </div>
@@ -33,7 +20,6 @@
 <script setup>
 import { computed } from 'vue';
 import { languageOptions } from '@/translations/i18n.js';
-import CountryFlag from 'vue-country-flag-next';
 
 // Convert the object to an array of [key, value] pairs
 let languageOptionsArray = Object.entries(languageOptions);
@@ -47,16 +33,13 @@ let sortedLanguageOptions = Object.fromEntries(languageOptionsArray);
 const languageDropdownOptions = computed(() => {
   return Object.entries(sortedLanguageOptions).map(([key, value]) => {
     return {
-      name: value.country,
+      name: value.language,
+      locale: value.locale,
       code: value.code,
       value: key,
     };
   });
 });
-
-const getCountryFlag = (locale) => {
-  return languageOptions[locale].code;
-};
 </script>
 
 <style scoped></style>
