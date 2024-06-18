1809f8490ba3e00534bdd3a25f3aedc1c17e18c4
Lucas Song <lucas7song@gmail.com>
2024-06-18 16:32:43 -0700

    update to only use mkcert on dev

--- vite.config.js
+++ vite.config.js
@@ -61,8 +61,7 @@ export default defineConfig({
         /* other options */
       },
     }),
-    // basic
-    mkcert(),
+    process.env.NODE_ENV === 'development' && mkcert(),
     nodePolyfills({
       globals: {
         process: true,
