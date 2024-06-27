76e286b79bdaaa1ef45e70a768b31374bcba37eb
Lucas Song <lucas7song@gmail.com>
2024-06-27 16:04:13 -0700

    update vite config for vitePWA

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,3 @@
-import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
+import { cleanupOutdatedCaches } from 'workbox-precaching';
 
 cleanupOutdatedCaches();
-
-precacheAndRoute([{ url: '/index.html', revision: '383676' }]);
--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
         background_color: '#ffffff',
         injectRegister: 'manual',
         workbox: {
-          globPatterns: ['**/*.{vue,js,css,html,ico}'],
+          globPatterns: ['**/*.{css,html,ico}'],
         },
         icons: [
           {
@@ -60,7 +60,7 @@ export default defineConfig({
       },
       /* enable sw on development */
       devOptions: {
-        enabled: true,
+        enabled: false,
         type: 'module',
         /* other options */
       },
