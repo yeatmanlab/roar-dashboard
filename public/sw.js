1d67b99e0866573cf7cd0846c4709a25c6a8375b
Lucas Song <lucas7song@gmail.com>
2024-06-27 14:25:21 -0700

    update public/sw.js to the correct service worker configuration

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,8 @@
 import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
+console.log('service worker disable before', self.__WB_DISABLE_DEV_LOGS);
 self.__WB_DISABLE_DEV_LOGS = true;
+console.log('service worker disable after', self.__WB_DISABLE_DEV_LOGS);
 cleanupOutdatedCaches();
 
 precacheAndRoute([{ url: '/index.html', revision: '383676' }]);
+// precacheAndRoute(self.__WB_MANIFEST);
