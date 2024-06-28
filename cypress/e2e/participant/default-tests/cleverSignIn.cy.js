b41fec99674cbdd76c4b56de3b075c82e94f4e70
emily-ejag <emily.artegar@gmail.com>
2024-06-28 16:45:54 -0700

    test for clever sign in working

--- cypress/e2e/participant/default-tests/cleverSignIn.cy.js
+++ cypress/e2e/participant/default-tests/cleverSignIn.cy.js
@@ -1,28 +1,38 @@
+export const timeout = Cypress.env('timeout');
+
 describe('Cypress test to login in Clever', () => {
   it('passes', () => {
-    cy.visit(
-      'https://clever.com/oauth/authorize?response_type=code&redirect_uri=https%3A%2F%2Flocalhost%3A5173%2Fauth-clever&client_id=1150d0bd8a44909bfdab',
-    );
-    // cy.visit('c');
-    cy.get('button', { timeout: 10000 }).contains('Clever').click();
+    // Generate a random state parameter
+    const state = 'testState123'; // In real scenarios, this should be a securely generated random value
+
+    // Construct the OAuth URL with localhost redirect URI
+    const url = new URL('https://clever.com/oauth/authorize');
+    url.searchParams.append('response_type', 'code');
+    url.searchParams.append('client_id', '1150d0bd8a44909bfdab');
+    url.searchParams.append('redirect_uri', 'https://localhost:5173/auth-clever');
+    url.searchParams.append('state', state);
+    url.searchParams.append('scope', 'openid email profile');
+    url.searchParams.append('nonce', '0766119dd5c7e94596a6820199de9f2505ab866e026fc44bbaa0e922aa86004e');
+    url.searchParams.append('context_uri', 'https://localhost:5173');
+
+    // Visit the constructed OAuth URL
+    cy.visit(url.toString());
+
+    // Input the school name in the specified field and press Enter
+    cy.get('input[placeholder="Type school name here..."][title="School name"]')
+      .type('61e8aee84cf0e71b14295d45')
+      .wait(0.1 * timeout)
+      .type('{enter}');
+
+    // Find the username input field and input the username
+    cy.get('input#username').type('27988125011');
+
+    // Input password
+    cy.get('input#password').type('.EWKYDvAGNdGm!@g8a_E');
+
+    // Click the login button
+    cy.get('button#UsernamePasswordForm--loginButton').click();
+
+    cy.wait(100 * timeout);
   });
 });
-
-//   cy.log('Test is disabled until a student with email/password is created in admin dev database.');
-// cy.login(Cypress.env('participantUsername'), Cypress.env('participantPassword'));
-// cy.visit('/');
-//
-
-// cy.get('button', { timeout: 10000 }).contains('Sign Out').click();
-//2Flocalhost%3A5173%2Fauth-clever&client_id=1150d0bd8a44909bfdab
-// // successfully back at Login page
-// cy.contains('Welcome to ROAR!');
-//
-// // Login via email / password
-// cy.loginWithEmail(Cypress.env('participantEmail'), Cypress.env('participantEmailPassword'));
-// cy.visit('/');
-//
-// cy.get('button', { timeout: 10000 }).contains('Sign Out').click();
-//
-// // successfully back at Login page
-// cy.contains('Welcome to ROAR!');
