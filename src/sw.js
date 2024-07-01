3b8cfa46e1e158ccc109797ad294ef9f77c230f7
Lucas Song <lucas7song@gmail.com>
2024-07-01 12:07:39 -0700

    use precacheController vs precacheAndRoute

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,40 @@
-import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
+import { cleanupOutdatedCaches, PrecacheController } from 'workbox-precaching';
 import { swrAudioURLs, swrImageUrls, swrLookupTableUrl } from '@/helpers/swrAssetsList.js';
 
 const urlsToCache = [...swrAudioURLs, ...swrImageUrls, ...swrLookupTableUrl];
 
-console.log('urlstocache', urlsToCache);
+const precacheController = new PrecacheController();
+precacheController.addToCacheList(urlsToCache);
 
-precacheAndRoute(urlsToCache);
+precacheController.addToCacheList([
+  {
+    url: '/index.html',
+    revision: 'abcd',
+  },
+]);
+
+self.addEventListener('install', (event) => {
+  // Passing in event is required in Workbox v6+
+  event.waitUntil(precacheController.install(event));
+});
+
+self.addEventListener('activate', (event) => {
+  // Passing in event is required in Workbox v6+
+  event.waitUntil(precacheController.activate(event));
+});
+
+self.addEventListener('fetch', (event) => {
+  const cacheKey = precacheController.getCacheKeyForURL(event.request.url);
+  //   event.respondWith(caches.match(cacheKey).then(...));
+  event.respondWith(caches.match(cacheKey));
+});
+
+// console.log('urlstocache', urlsToCache);
+
+// precacheAndRoute(urlsToCache, {
+//     // Ignore all URL parameters.
+//     ignoreURLParametersMatching: [/.*/],
+//   });
 
 cleanupOutdatedCaches();
 self.__WB_MANIFEST;
