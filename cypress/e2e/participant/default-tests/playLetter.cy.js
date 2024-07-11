e56f97a10528c16899889a29f6a16b70ea9ac177
Lucas Song <lucas7song@gmail.com>
2024-07-11 16:38:25 -0700

    edit description and rerun tests

--- cypress/e2e/participant/default-tests/playLetter.cy.js
+++ cypress/e2e/participant/default-tests/playLetter.cy.js
@@ -3,8 +3,8 @@ import { isCurrentVersion } from '../../../support/utils';
 
 const app = '@bdelab/roar-letter';
 
-describe('ROAR - Letra Play Through using username authentication', () => {
-  it('Plays Letra with username authentication', () => {
+describe('ROAR - Letter Play Through using username authentication', () => {
+  it('Plays Letter with username authentication', () => {
     cy.wrap(!isCurrentVersion(app)).then((isCurrentVersion) => {
       if (isCurrentVersion) {
         cy.log(`Did not detect a new version of ${app}, skipping test.`);
@@ -14,7 +14,7 @@ describe('ROAR - Letra Play Through using username authentication', () => {
       }
     });
   });
-  it('Plays Letra with Clever authentication', () => {
+  it('Plays Letter with Clever authentication', () => {
     cy.wrap(isCurrentVersion(app)).then((isCurrentVersion) => {
       if (!isCurrentVersion) {
         cy.log(`Did not detect a new version of ${app}, skipping test.`);
