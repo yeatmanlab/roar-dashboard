c35dc8d39ae5be9e5116af26af22b064526e8790
Lucas Song <lucas7song@gmail.com>
2024-07-15 11:21:14 -0700

    add network tests to yml

--- cypress/e2e/participant/network-tests/roam-fluency/playFluency-ARF-3G.cy.js
+++ cypress/e2e/participant/network-tests/roam-fluency/playFluency-ARF-3G.cy.js
@@ -6,7 +6,7 @@ const app = '@bdelab/roam-fluency';
 describe('Test playthrough of Fluency as a participant in a simulated 3G network', () => {
   it('Fluency Playthrough Test', () => {
     cy.wrap(isCurrentVersion(app)).then((isCurrentVersion) => {
-      if (!isCurrentVersion) {
+      if (isCurrentVersion) {
         cy.log(`Did not detect a new version of ${app}, skipping test.`);
       } else {
         cy.log(`Detected a new version of ${app}, running test.`);
--- cypress/e2e/participant/network-tests/roar-letter/playLetter-3G.cy.js
+++ cypress/e2e/participant/network-tests/roar-letter/playLetter-3G.cy.js
@@ -6,7 +6,7 @@ const app = '@bdelab/roar-letter';
 describe('ROAR - Letter Play Through using username authentication', () => {
   it('Plays Letter with username authentication in a simulated 3g connection', () => {
     cy.wrap(isCurrentVersion(app)).then((isCurrentVersion) => {
-      if (!isCurrentVersion) {
+      if (isCurrentVersion) {
         cy.log(`Did not detect a new version of ${app}, skipping test.`);
       } else {
         cy.log(`Detected a new version of ${app}, running test.`);
