51717eec060bf77c84a8672d6acb51a38d929252
Kyle <ksmontville@gmail.com>
2024-07-30 09:38:25 -0700

    update swr end block text

--- cypress/support/helper-functions/roar-swr/languageOptions.js
+++ cypress/support/helper-functions/roar-swr/languageOptions.js
@@ -9,7 +9,7 @@ export const languageOptions = {
     blockTerminationPhraseThree: 'You are halfway through the second block',
     blockTerminationPhraseFour: 'You have completed the second block',
     blockTerminationPhraseFive: 'You are halfway through the third block',
-    blockTerminationPhraseSix: 'Lexicality',
+    blockTerminationPhraseSix: 'Press ANY KEY to continue',
   },
   es: {
     gameTab: 'ROAR - Palabra',
