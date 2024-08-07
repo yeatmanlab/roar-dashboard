eae2d17ddd93708219a3a26ab8b3799daa0ba84d
Maximilian Oertel <maximilian@beyond-consulting.xyz>
2024-08-07 17:11:25 +0100

    Add .env.example and fix var name format

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -5,6 +5,7 @@
  * @constant {number} AUTH_SESSION_TIMEOUT_COUNTDOWN_DURATION - Session signout countdown duration (in ms).
  * @constant {number} AUTH_SESSION_SIGNOUT_THRESHOLD - Session signout threshold (in ms) before signout is triggered.
  */
-export const AUTH_SESSION_TIMEOUT_LIMIT = import.meta.env.SESSION_TIMEOUT_LIMIT || 15 * 60 * 1000;
-export const AUTH_SESSION_TIMEOUT_COUNTDOWN_DURATION = import.meta.env.SESSION_TIMEOUT_COUNTDOWN_DURATION || 60 * 1000;
+export const AUTH_SESSION_TIMEOUT_LIMIT = import.meta.env.VITE_AUTH_SESSION_TIMEOUT_LIMIT || 15 * 60 * 1000;
+export const AUTH_SESSION_TIMEOUT_COUNTDOWN_DURATION =
+  import.meta.env.VITE_AUTH_SESSION_TIMEOUT_COUNTDOWN_DURATION || 60 * 1000;
 export const AUTH_SESSION_SIGNOUT_THRESHOLD = AUTH_SESSION_TIMEOUT_LIMIT + AUTH_SESSION_TIMEOUT_COUNTDOWN_DURATION;
