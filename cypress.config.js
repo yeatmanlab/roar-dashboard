e7c09a3ff5a8c6b7479530b85540fff0a1744872
Kyle <ksmontville@gmail.com>
2024-08-09 13:25:58 -0700

    update Syntax test and Levante task component

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     baseUrl: process.env.CYPRESS_BASE_URL ?? 'https://localhost:5173',
     experimentalRunAllSpecs: true,
     experimentalMemoryManagement: true,
-    retries: 2,
+    // retries: 2,
     setupNodeEvents(on, config) {
       return require('./node_modules/cypress-fs/plugins/index.js')(on, config);
     },
