6c4f232d41835680faffc3d2639fde721d12c2ef
Maximilian Oertel <maximilian@beyond-consulting.xyz>
2024-08-23 10:06:37 +0200

    Extend FIRESTORE_COLLECTIONS

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -2,6 +2,14 @@
  * Firestore database collections
  */
 export const FIRESTORE_COLLECTIONS = Object.freeze({
-  USERS: 'users',
+  ACTIVATION_CODES: 'activationCodes',
+  ADMINISTRATIONS: 'administrations',
+  CLASSES: 'classes',
+  DISTRICTS: 'districts',
+  FAMILIES: 'families',
+  GROUPS: 'groups',
+  LEGAL: 'legal',
+  SCHOOLS: 'schools',
   USER_CLAIMS: 'userClaims',
+  USERS: 'users',
 });
