00bbea385969dc9c6fd88400bb01414cac21c7e2
Kyle <ksmontville@gmail.com>
2024-09-10 10:10:58 -0700

    Update comments

--- cypress/e2e/participant/default-tests/cleverSignIn.cy.js
+++ cypress/e2e/participant/default-tests/cleverSignIn.cy.js
@@ -6,6 +6,5 @@ describe('Cypress test to login in Clever', () => {
   it('passes', () => {
     cy.visit('/');
     signInWithClever();
-    // cy.contains('tasks completed!', { timeout: 6 * timeout }).should('be.visible');
   });
 });
--- src/config/firebaseRoar.js
+++ src/config/firebaseRoar.js
@@ -9,9 +9,9 @@ const useSandbox = import.meta.env.VITE_FIREBASE_DATA_SOURCE === 'sandbox';
 const isStaging = import.meta.env.VITE_STAGING_BUILD === 'true';
 
 function setDebugToken(config) {
-  // If the NODE_ENV = test, use the debug token from the Cypress config
-  // Otherwise, use the VITE_APPCHECK_DEBUG_TOKEN environment variable
-  // If neither are set, set a new debug token
+  // If the NODE_ENV = test, use the debug token from the Cypress config (NODE_ENV is set to test in the Cypress test GitHub action)
+  // Otherwise, use the VITE_APPCHECK_DEBUG_TOKEN environment variable (set as a local environment variable in the .env file and in the GitHub action for preview builds)
+  // If neither are set, create a new debug token which will be inactive until it is set in the Firebase App Check console
   const debugToken =
     import.meta.env.NODE_ENV === 'test'
       ? Cypress.env('appCheckDebugToken')
