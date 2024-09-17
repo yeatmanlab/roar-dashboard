6ca311d972f059da2a6bbcf8a3ea849be6cb9f18
Maximilian Oertel <maximilian@beyond-consulting.xyz>
2024-09-17 17:10:27 +0100

    Fix comment typos

--- src/composables/queries/useUserRunPageQuery.js
+++ src/composables/queries/useUserRunPageQuery.js
@@ -9,8 +9,9 @@ import { USER_RUN_PAGE_QUERY_KEY } from '@/constants/queryKeys';
 /**
  * User run page query
  *
- * @TODO: Evaluate whether this query can be replaced using more generic querie that already fetch user assessments and
- * scores. This query was implemented as part of the transition to query composables but might be redudant.
+ * @TODO: Evaluate whether this query can be replaced using more generic query that already fetches user assessments and
+ * scores. This query was implemented as part of the transition to query composables but might be redudant if we
+ * refactor the underlying database query helpers to fetch all necessary data in a single query.
  *
  * @param {string|undefined|null} userId – The user ID to fetch, set to a falsy value to fetch the current user.
  * @param {QueryOptions|undefined} queryOptions – Optional TanStack query options.
