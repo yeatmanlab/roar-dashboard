da98e57299bb5bd83f0a1f201c5008c8bc5d2ca4
Maximilian Oertel <maximilian@beyond-consulting.xyz>
2024-09-17 17:06:02 +0100

    Add useUserAdministrationAssignmentsQuery unit tests

---
+++ src/composables/queries/useUserAdministrationAssignmentsQuery.test.js
@@ -0,0 +1,145 @@
+import { ref, nextTick } from 'vue';
+import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
+import { createTestingPinia } from '@pinia/testing';
+import * as VueQuery from '@tanstack/vue-query';
+import { nanoid } from 'nanoid';
+import { withSetup } from '@/test-support/withSetup.js';
+import { useAuthStore } from '@/store/auth';
+import { fetchDocById } from '@/helpers/query/utils';
+import useUserAdministrationAssignmentsQuery from './useUserAdministrationAssignmentsQuery';
+
+vi.mock('@/helpers/query/utils', () => ({
+  fetchDocById: vi.fn().mockImplementation(() => []),
+}));
+
+vi.mock('@tanstack/vue-query', async (getModule) => {
+  const original = await getModule();
+  return {
+    ...original,
+    useQuery: vi.fn().mockImplementation(original.useQuery),
+  };
+});
+
+describe('useUserAdministrationAssignmentsQuery', () => {
+  let piniaInstance;
+  let queryClient;
+
+  beforeEach(() => {
+    piniaInstance = createTestingPinia();
+    queryClient = new VueQuery.QueryClient();
+  });
+
+  afterEach(() => {
+    queryClient?.clear();
+  });
+
+  it('should call query with correct parameters', () => {
+    const mockUserId = nanoid();
+    const mockAdministrationId = nanoid();
+
+    vi.spyOn(VueQuery, 'useQuery');
+
+    withSetup(() => useUserAdministrationAssignmentsQuery(mockUserId, mockAdministrationId), {
+      plugins: [[VueQuery.VueQueryPlugin, { queryClient }]],
+    });
+
+    expect(VueQuery.useQuery).toHaveBeenCalledWith({
+      queryKey: ['user-administration-assignments', mockUserId, mockAdministrationId],
+      queryFn: expect.any(Function),
+      enabled: expect.objectContaining({
+        _value: true,
+      }),
+    });
+
+    expect(fetchDocById).toHaveBeenCalledWith('users', `${mockUserId}/assignments/${mockAdministrationId}`);
+  });
+
+  it('should correctly control the enabled state of the query', async () => {
+    const mockUserId = nanoid();
+    const mockAdministrationId = nanoid();
+
+    const enableQuery = ref(false);
+
+    vi.spyOn(VueQuery, 'useQuery');
+
+    const queryOptions = {
+      enabled: enableQuery,
+    };
+
+    withSetup(() => useUserAdministrationAssignmentsQuery(mockUserId, mockAdministrationId, queryOptions), {
+      plugins: [[VueQuery.VueQueryPlugin, { queryClient }]],
+    });
+
+    expect(VueQuery.useQuery).toHaveBeenCalledWith({
+      queryKey: ['user-administration-assignments', mockUserId, mockAdministrationId],
+      queryFn: expect.any(Function),
+      enabled: expect.objectContaining({
+        _value: false,
+        __v_isRef: true,
+      }),
+    });
+
+    expect(fetchDocById).not.toHaveBeenCalled();
+
+    enableQuery.value = true;
+    await nextTick();
+
+    expect(fetchDocById).toHaveBeenCalledWith('users', `${mockUserId}/assignments/${mockAdministrationId}`);
+  });
+
+  it('should only fetch data if the params are set', async () => {
+    const mockUserId = ref(null);
+    const mockAdministrationId = ref(null);
+
+    const queryOptions = { enabled: true };
+
+    withSetup(() => useUserAdministrationAssignmentsQuery(mockUserId, mockAdministrationId, queryOptions), {
+      plugins: [[VueQuery.VueQueryPlugin, { queryClient }]],
+    });
+
+    expect(VueQuery.useQuery).toHaveBeenCalledWith({
+      queryKey: ['user-administration-assignments', mockUserId, mockAdministrationId],
+      queryFn: expect.any(Function),
+      enabled: expect.objectContaining({
+        _value: false,
+        __v_isRef: true,
+      }),
+    });
+
+    expect(fetchDocById).not.toHaveBeenCalled();
+
+    mockUserId.value = nanoid();
+
+    await nextTick();
+
+    expect(fetchDocById).not.toHaveBeenCalled();
+
+    mockAdministrationId.value = nanoid();
+
+    await nextTick();
+
+    expect(fetchDocById).toHaveBeenCalledWith('users', `${mockUserId}/assignments/${mockAdministrationId}`);
+  });
+
+  it('should not let queryOptions override the internally computed value', async () => {
+    const mockUserId = ref(null);
+    const mockAdministrationId = ref(nanoid());
+
+    const queryOptions = { enabled: true };
+
+    withSetup(() => useUserAdministrationAssignmentsQuery(mockUserId, mockAdministrationId, queryOptions), {
+      plugins: [[VueQuery.VueQueryPlugin, { queryClient }]],
+    });
+
+    expect(VueQuery.useQuery).toHaveBeenCalledWith({
+      queryKey: ['user-administration-assignments', mockUserId, mockAdministrationId],
+      queryFn: expect.any(Function),
+      enabled: expect.objectContaining({
+        _value: false,
+        __v_isRef: true,
+      }),
+    });
+
+    expect(fetchDocById).not.toHaveBeenCalled();
+  });
+});
