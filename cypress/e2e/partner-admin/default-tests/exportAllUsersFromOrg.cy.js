71fb217c1876105ae96258d2327cc380e4ffaad3
emily-ejag <emily.artegar@gmail.com>
2024-10-08 14:13:16 -0700

    create a command to checkOrgs

--- cypress/e2e/partner-admin/default-tests/exportAllUsersFromOrg.cy.js
+++ cypress/e2e/partner-admin/default-tests/exportAllUsersFromOrg.cy.js
@@ -2,27 +2,16 @@ const timeout = Cypress.env('timeout');
 
 const listOrgsUrl = '/list-orgs';
 
-function checkOrgExists() {
-  cy.get('ul > li', { timeout: timeout }).contains('Districts', { timeout: timeout }).click();
-
-  cy.get('div', { timeout: timeout }).should('contain.text', Cypress.env('testPartnerDistrictName'), {
-    timeout: timeout,
-  });
-  cy.log(`${Cypress.env('testPartnerDistrictName')} exists.`);
-}
-
 describe('The partner admin user', () => {
   beforeEach(() => {
     cy.login(Cypress.env('partnerAdminUsername'), Cypress.env('partnerAdminPassword'));
-    cy.wait(0.2 * timeout);
     cy.navigateTo('/');
-    cy.wait(0.2 * timeout);
-    cy.navigateTo(listOrgsUrl, { timeout: timeout });
+    cy.navigateTo(listOrgsUrl);
   });
 
   context(`when navigating to the ${Cypress.env('testPartnerDistrictName')} tab`, () => {
     it(`should see the organization ${Cypress.env('testPartnerDistrictName')}`, () => {
-      checkOrgExists();
+      cy.checkOrgExists();
       cy.get('button').contains('Export Users').click();
       cy.readFile(`${Cypress.env('cypressDownloads')}/cypress-test-district-users-export.csv`);
     });
--- cypress/e2e/partner-admin/default-tests/listOrgs.spec.cy.js
+++ cypress/e2e/partner-admin/default-tests/listOrgs.spec.cy.js
@@ -8,31 +8,16 @@ const orgs = [
 
 const listOrgsUrl = '/list-orgs';
 
-function checkOrgExists(org) {
-  cy.get('ul > li', { timeout: timeout }).contains(org.tabName, { timeout: timeout }).click();
-  cy.log('Tab ' + org.tabName + ' found.');
-
-  cy.get('div', { timeout: timeout }).should('contain.text', org.orgName, {
-    timeout: timeout,
-  });
-  cy.log(`${org.orgName} exists.`);
-}
-
 describe('The partner admin user', () => {
   beforeEach(() => {
     cy.login(Cypress.env('partnerAdminUsername'), Cypress.env('partnerAdminPassword'));
-    cy.wait(0.2 * timeout);
-    cy.navigateTo('/');
-    cy.wait(0.2 * timeout);
     cy.navigateTo(listOrgsUrl, { timeout: timeout });
-    // cy.get('button').contains('Organizations').click();
-    // cy.get('button').contains('List Organizations').click();
   });
 
   orgs.forEach((org) => {
     context(`when navigating to the ${org.tabName} tab`, () => {
       it(`should see the organization ${org.orgName}`, () => {
-        checkOrgExists(org);
+        cy.checkOrgExists(org);
       });
     });
   });
