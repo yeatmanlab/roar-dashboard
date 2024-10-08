6dcffcbfe3bb0f4782d97ad41e6d785a6cec8222
Maximilian Oertel <maximilian@beyond-consulting.xyz>
2024-10-08 23:09:42 +0100

    Add support for dotenvx

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-// ***********************************************************
-// | Initialize the app as defined in                 |
-// | setup.js and mount it to the DOM           |
-// ***********************************************************
-
 import { mountApp } from './setup.js';
 
 mountApp();
