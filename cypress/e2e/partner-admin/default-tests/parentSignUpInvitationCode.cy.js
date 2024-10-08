0798b7735805227d653566501a7d7d2cdf6bd73b
Kyle Montville <ksmontville@gmail.com>
2024-10-08 11:51:43 -0700

    Suggested changes for parent sign up e2e test (#857)
    * suggested changes for parent sign up e2e test
    
    * update workflow with new secrets
    
    * remove unncessary then()

--- cypress/e2e/partner-admin/default-tests/parentSignUpInvitationCode.cy.js
+++ cypress/e2e/partner-admin/default-tests/parentSignUpInvitationCode.cy.js
@@ -1,16 +1,16 @@
 const baseUrl = Cypress.env('baseUrl');
 const timeout = Cypress.env('timeout');
+const listOrgsUrl = '/list-orgs';
 const orgs = [
-  { tabName: 'Districts', orgName: Cypress.env('testPartnerDistrictName') },
-  { tabName: 'Schools', orgName: Cypress.env('testPartnerSchoolName') },
-  { tabName: 'Classes', orgName: Cypress.env('testPartnerClassName') },
-  { tabName: 'Groups', orgName: Cypress.env('testPartnerGroupName') },
+  {
+    tabName: 'Districts',
+    orgName: Cypress.env('testPartnerDistrictName'),
+    orgVerified: 'Districts - Cypress Test District',
+  },
 ];
 
-const listOrgsUrl = '/list-orgs';
-
 function checkOrgExists(org) {
-  // Click the tab that contains the org's name
+  // Click the tab that contains the org name
   cy.get('ul > li', { timeout: timeout }).contains(org.tabName, { timeout: timeout }).click();
   cy.log('Tab ' + org.tabName + ' found.');
 
@@ -30,76 +30,49 @@ function checkOrgExists(org) {
   cy.log(`Invite Users button clicked for ${org.orgName}.`);
 }
 
+function visitSignUpPage(activationCode) {
+  const registerUrl = `${baseUrl}/register/?code=${activationCode}`;
+  cy.visit(registerUrl);
+}
+
+function inputLoginValues() {
+  cy.get('input[name="firstName"]').type(Cypress.env('parentFirstName'));
+  cy.get('input[name="lastName"]').type(Cypress.env('parentLastName'));
+  cy.get('input[name="ParentEmail"]').type(Cypress.env('parentEmail'));
+  cy.get('input[type="password"]').first().type(Cypress.env('parentPassword'));
+  cy.get('input[type="password"]').eq(1).type(Cypress.env('parentPassword'));
+}
+
+function completeParentSignUp(org) {
+  cy.get('div.p-checkbox-box').click();
+  cy.get('button').contains('Continue').click();
+  cy.get('button').contains('Next').click();
+  cy.get('h2').should('contain.text', org.orgVerified);
+}
+
 describe('The partner admin user', () => {
   beforeEach(() => {
     cy.login(Cypress.env('partnerAdminUsername'), Cypress.env('partnerAdminPassword'));
-    cy.wait(0.2 * timeout);
-    cy.navigateTo('/');
-    cy.wait(0.2 * timeout);
-    cy.navigateTo(listOrgsUrl, { timeout: timeout });
+    cy.visit('/');
+    cy.visit(listOrgsUrl);
   });
 
   orgs.forEach((org) => {
     context(`when navigating to the ${org.tabName} tab`, () => {
       it(`should see the organization ${org.orgName} and should click on Invite Users`, () => {
         checkOrgExists(org);
-      });
-    });
-  });
 
-  after(() => {
-    // Copy the org code
-    cy.get('i[data-cy="copy-invitation"]')
-      .should('exist')
-      .parent('button') // Navigate back to the button element
-      .click();
-    cy.log('Specific copy button clicked.');
+        // Invoke the activation code input field to get the value
+        cy.get('[data-cy="input-text-activation-code"]')
+          .invoke('attr', 'value')
+          .then((value) => {
+            expect(value).to.not.be.empty;
 
-    // Access the copied activation code from the clipboard
-    cy.window().then((win) => {
-      return win.navigator.clipboard.readText().then((activationCode) => {
-        cy.log('Copied activation code: ' + activationCode);
-        expect(activationCode).to.not.be.empty;
-
-        // Construct the registration URL with the activation code
-        const registerUrl = `${baseUrl}/register/?code=${activationCode}`;
-
-        // Use cy.origin to handle the navigation to the new origin
-        cy.visit(registerUrl);
-        cy.log('Navigated to the registration URL: ' + registerUrl);
-
-        // Fill in the form fields after the page loads
-        cy.get('input[name="firstName"]').type(Cypress.env('parentFirstName'));
-        cy.log('Filled in first name.');
-
-        cy.get('input[name="lastName"]').type(Cypress.env('parentLastName'));
-        cy.log('Filled in last name.');
-
-        cy.get('input[name="ParentEmail"]').type(Cypress.env('parentEmail'));
-        cy.log('Filled in email.');
-
-        cy.get('input[type="password"]').first().type(Cypress.env('parentPassword'));
-        cy.log('Filled in password.');
-
-        cy.get('input[type="password"]').eq(1).type(Cypress.env('parentPassword'));
-        cy.log('Filled in password confirmation.');
-
-        cy.get('div.p-checkbox-box').click();
-        cy.log('Checkbox clicked.');
-
-        cy.wait(2000);
-
-        cy.get(
-          'button.p-button.p-component.p-confirm-dialog-accept.bg-primary.text-white.border-none.border-round.p-2.hover\\:bg-red-900',
-        ).click();
-        cy.log('Clicked the "Continue" button.');
-
-        cy.get('span.p-button-label').contains('Next').click();
-        cy.log('Clicked the "Next" button.');
-
-        // Verify that the org is "Groups - Cypress Test Group"
-        cy.get('h2.text-primary.h-3.m-0.p-0').should('contain.text', 'Groups - Cypress Test Group');
-        cy.log('Verified that "Groups - Cypress Test Group" is visible.');
+            // Visit the sign-up page with the activation code
+            visitSignUpPage(value);
+            inputLoginValues();
+            completeParentSignUp(org);
+          });
       });
     });
   });
