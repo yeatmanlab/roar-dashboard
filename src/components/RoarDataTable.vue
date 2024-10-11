cc2d2021637d01f136cdf1b3ae881dea64d02162
Maximilian Oertel <maximilian@beyond-consulting.xyz>
2024-10-11 12:27:40 +0100

    Combine progress and score report specs respectively

--- src/components/RoarDataTable.vue
+++ src/components/RoarDataTable.vue
@@ -51,8 +51,9 @@
           label="Export Selected"
           :badge="selectedRows?.length?.toString()"
           :disabled="selectedRows.length === 0"
-          class="m-1 m-1 h-3rem bg-primary text-white border-none border-round h-2rem text-sm hover:bg-red-900"
+          class="m-1 h-3rem bg-primary text-white border-none border-round h-2rem text-sm hover:bg-red-900"
           @click="exportCSV(true, $event)"
+          data-cy="data-table__export-selected-btn"
         />
         <PvButton
           v-if="allowExport"
@@ -60,6 +61,7 @@
           label="Export Whole Table"
           class="m-1 h-3rem bg-primary text-white border-none border-round h-2rem text-sm hover:bg-red-900"
           @click="exportCSV(false, $event)"
+          data-cy="data-table__export-table-btn"
         />
       </span>
     </div>
