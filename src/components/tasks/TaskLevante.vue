5dca789962e4147421c67a689a24b1a1f0f828b3
Maximilian Oertel <maximilian@beyond-consulting.xyz>
2024-10-14 18:39:07 +0100

    Fix Levante task loading

--- src/components/tasks/TaskLevante.vue
+++ src/components/tasks/TaskLevante.vue
@@ -25,6 +25,7 @@ let levanteTaskLauncher;
 const taskId = props.taskId;
 const { version } = packageLockJson.packages['node_modules/@levante-framework/core-tasks'];
 const router = useRouter();
+const taskStarted = ref(false);
 const gameStarted = ref(false);
 const authStore = useAuthStore();
 const gameStore = useGameStore();
@@ -76,14 +77,16 @@ onBeforeUnmount(() => {
 watch(
   [isFirekitInit, isLoadingUserData],
   async ([newFirekitInitValue, newLoadingUserData]) => {
-    if (newFirekitInitValue && !newLoadingUserData) await startTask();
+    if (newFirekitInitValue && !newLoadingUserData && !taskStarted.value) {
+      taskStarted.value = true;
+      const { selectedAdmin } = storeToRefs(gameStore);
+      await startTask(selectedAdmin);
+    }
   },
   { immediate: true },
 );
 
-const { selectedAdmin } = storeToRefs(gameStore);
-
-async function startTask() {
+async function startTask(selectedAdmin) {
   try {
     let checkGameStarted = setInterval(function () {
       // Poll for the preload trials progress bar to exist and then begin the game
