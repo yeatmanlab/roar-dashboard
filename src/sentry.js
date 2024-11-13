53031d47ac213a58ac6ec6a1f1e4f2e27b4c9060
George Kachergis <george.kachergis@gmail.com>
2024-11-13 15:43:45 -0800

    add Sentry user feedback button for LEVANTE

--- src/sentry.js
+++ src/sentry.js
@@ -10,13 +10,17 @@ const regex = /https:\/\/roar-staging(--pr\d+-\w+)?\.web\.app/;
 export function initSentry(app) {
   // skip if levante instance
   if (isLevante) {
-    return;
+    dsn = 'https://9d67b24a405feffb49477ca8002cc033@o4507250485035008.ingest.us.sentry.io/4507376476618752';
+    tracePropagationTargets = ['localhost:8080', 'https://levante-network.org/*']; // ToDo: add regex for staging/dev/demo URLs...
+  } else {
+    dsn = 'https://f15e3ff866394e93e00514b42113d03d@o4505913837420544.ingest.us.sentry.io/4506820782129152';
+    tracePropagationTargets = ['localhost:5173', 'https://roar.education/**/*', regex];
   }
   // Only initialize Sentry in production
   if (process.env.NODE_ENV === 'production' && !isLevante) {
     Sentry.init({
       app,
-      dsn: 'https://f15e3ff866394e93e00514b42113d03d@o4505913837420544.ingest.us.sentry.io/4506820782129152',
+      dsn: dsn,
       integrations: [
         Sentry.replayIntegration({
           maskAllText: true,
@@ -46,7 +50,7 @@ export function initSentry(app) {
       attachStacktrace: true,
       // Performance Monitoring
       tracesSampleRate: 0.2, // Capture 20% of the transactions
-      tracePropagationTargets: ['localhost:5173', 'https://roar.education/**/*', regex],
+      tracePropagationTargets: tracePropagationTargets,
       // Session Replay
       replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
       replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
