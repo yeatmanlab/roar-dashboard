40415226a7e45dbf1358b84215aac7df6def1e5c
Maximilian Oertel <info@maximilianoertel.de>
2024-11-22 21:14:46 +0000

    Remove obsolete logout spec

--- cypress/e2e/partner-admin/default-tests/logout.spec.cy.js
+++
@@ -1,8 +0,0 @@
-describe('The partner admin can log out.', () => {
-  it('Logs the user out', () => {
-    cy.login(Cypress.env('PARTNER_ADMIN_USERNAME'), Cypress.env('PARTNER_ADMIN_PASSWORD'));
-    cy.visit('/', { timeout: Cypress.env('timeout') });
-    cy.get('.admin-page-header').should('be.visible').and('contain', 'View Administrations');
-    cy.logout();
-  });
-});
