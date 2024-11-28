c182e786015262e2750ace4480d0ae50597ac813
Maximilian Oertel <info@maximilianoertel.de>
2024-11-28 12:48:18 +0000

    Decouple NavBar presentation and logic into separate components

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,12 +3,12 @@
     <nav class="flex flex-row align-items-center justify-content-between w-full">
       <div id="navBarRightEnd" class="flex flex-row align-items-center justify-content-start w-full gap-1">
         <div class="flex align-items-center justify-content-center w-full">
-          <PvMenubar :model="computedItems" class="w-full">
+          <PvMenubar :model="menuItems" class="w-full">
             <template #start>
               <router-link :to="{ path: APP_ROUTES.HOME }">
                 <div class="navbar-logo mx-3">
-                  <PvImage v-if="isLevante" src="/LEVANTE/Levante_Logo.png" alt="LEVANTE Logo" width="200" />
-                  <ROARLogo v-else />
+                  <ROARLogo v-if="!logo" data-cy="navbar__logo" />
+                  <PvImage v-else :src="logo" alt="Logo" width="200" height="auto" data-cy="navbar__logo" />
                 </div>
               </router-link>
             </template>
@@ -25,30 +25,32 @@
             <template #end>
               <div class="flex gap-2 align-items-center justify-content-center mr-3">
                 <div v-if="isWideScreen" class="nav-user-wrapper flex align-items-center gap-2 bg-gray-100">
-                  <div class="text-lg font-bold text-gray-600" data-cy="user-display-name">
-                    {{ $t('navBar.greeting') }}, {{ userDisplayName }}!
+                  <div class="text-lg font-bold text-gray-600" data-cy="navbar__display-name">
+                    {{ $t('navBar.greeting') }}, {{ displayName }}!
                   </div>
+
                   <PvButton
                     text
-                    data-cy="button-sign-out"
+                    data-cy="navbar__signout-btn-desktop"
                     class="no-underline h-2 p-1 m-0 text-primary border-none border-round h-2rem text-sm hover:bg-red-900 hover:text-white"
-                    @click="signOut"
+                    @click="() => onSignOut()"
                     >{{ $t('navBar.signOut') }}
                   </PvButton>
                 </div>
 
                 <PvButton
                   v-else
-                  data-cy="button-sign-out"
+                  data-cy="navbar__signout-btn-mobile"
                   class="no-underline m-0 bg-primary text-white border-none border-round h-2rem text-sm hover:bg-red-900"
-                  @click="signOut"
-                  >{{ $t('navBar.signOut') }}
+                  @click="() => onSignOut()"
+                >
+                  {{ $t('navBar.signOut') }}
                 </PvButton>
 
-                <div v-if="authStore.isUserAdmin" class="nav-user-wrapper bg-gray-100">
+                <div v-if="showAccountSettingsLink" class="nav-user-wrapper bg-gray-100">
                   <router-link :to="{ path: APP_ROUTES.ACCOUNT_PROFILE }">
                     <button
-                      data-cy="button-profile-info"
+                      data-cy="navbar__account-settings-btn"
                       class="no-underline p-1 m-0 text-primary border-none border-round cursor-pointer h-2rem w-2rem text-sm hover:bg-red-900 hover:text-white"
                     >
                       <i class="pi pi-cog"></i></button
@@ -69,43 +71,39 @@
 
 <script setup>
 import { ref, computed, onMounted, onUnmounted } from 'vue';
-import { useRouter } from 'vue-router';
-import { storeToRefs } from 'pinia';
-import _isEmpty from 'lodash/isEmpty';
-import _union from 'lodash/union';
 import PvButton from 'primevue/button';
 import PvImage from 'primevue/image';
 import PvMenubar from 'primevue/menubar';
-import { useAuthStore } from '@/store/auth';
-import { getSidebarActions } from '@/router/sidebarActions';
-import useUserClaimsQuery from '@/composables/queries/useUserClaimsQuery';
-import useSignOutMutation from '@/composables/mutations/useSignOutMutation';
+import LanguageSelector from '@/components/LanguageSelector.vue';
 import { APP_ROUTES } from '@/constants/routes';
 import ROARLogo from '@/assets/RoarLogo.vue';
-import LanguageSelector from '@/components/LanguageSelector.vue';
 
-const router = useRouter();
-const authStore = useAuthStore();
-const { roarfirekit } = storeToRefs(authStore);
+// Define props
+defineProps({
+  displayName: {
+    type: String,
+    required: true,
+  },
+  logo: {
+    type: [String, Object],
+    default: null,
+  },
+  menuItems: {
+    type: Array,
+    required: true,
+  },
+  onSignOut: {
+    type: Function,
+    required: true,
+  },
+  showAccountSettingsLink: {
+    type: Boolean,
+    default: true,
+  },
+});
 
-const initialized = ref(false);
 const menu = ref();
 const screenWidth = ref(window.innerWidth);
-
-const isLevante = import.meta.env.MODE === 'LEVANTE';
-
-const { mutate: signOut } = useSignOutMutation();
-
-let unsubscribe;
-
-const init = () => {
-  if (unsubscribe) unsubscribe();
-  initialized.value = true;
-};
-
-unsubscribe = authStore.$subscribe(async (mutation, state) => {
-  if (state.roarfirekit.restConfig) init();
-});
 
 // @TODO: Replace screen-size handlers with Tailwind/CSS media queries. Currently not possible due to an outdated
 // PrimeVue and Tailwind version. If we cannot update PrimeVue/Tailwind, we should throttle the resize events.
@@ -118,122 +116,12 @@ const handleResize = () => {
 };
 
 onMounted(() => {
-  if (roarfirekit.value.restConfig) init();
   window.addEventListener('resize', handleResize);
 });
 
 onUnmounted(() => {
   window.removeEventListener('resize', handleResize);
 });
-
-const { isLoading: isLoadingClaims, data: userClaims } = useUserClaimsQuery({
-  enabled: initialized,
-});
-
-const computedItems = computed(() => {
-  const items = [];
-  const headers = ['Administrations', 'Organizations', 'Users'];
-  for (const header of headers) {
-    const headerItems = rawActions.value
-      .filter((action) => action.category === header)
-      .map((action) => {
-        return {
-          label: action.title,
-          icon: action.icon,
-          command: () => {
-            router.push(action.buttonLink);
-          },
-        };
-      });
-    if (headerItems.length > 0) {
-      items.push({
-        label: header,
-        items: headerItems,
-      });
-    }
-  }
-  return items;
-});
-
-const userDisplayName = computed(() => {
-  if (!isLoadingClaims) {
-    return '';
-  } else {
-    let email = authStore?.userData?.email;
-    if (email && email.split('@')[1] === 'roar-auth.com') {
-      email = email.split('@')[0];
-    }
-    const displayName = authStore?.userData?.displayName;
-    const username = authStore?.userData?.username;
-    const firstName = authStore?.userData?.name?.first;
-    const userType = isAdmin.value ? 'Admin' : 'User';
-    return ` ${firstName || displayName || username || email || userType}`;
-  }
-});
-
-// @TODO: Replace isAdmin and isSuperAdmin with useUserType composable
-const isAdmin = computed(() => {
-  if (userClaims.value?.claims?.super_admin) return true;
-  if (_isEmpty(_union(...Object.values(userClaims.value?.claims?.minimalAdminOrgs ?? {})))) return false;
-  return true;
-});
-
-const isSuperAdmin = computed(() => Boolean(userClaims.value?.claims?.super_admin));
-
-const isAtHome = computed(() => {
-  return router.currentRoute.value.fullPath === '/';
-});
-
-const rawActions = computed(() => {
-  return getSidebarActions({
-    isSuperAdmin: isSuperAdmin.value,
-    isAdmin: authStore.isUserAdmin,
-    includeHomeLink: !isAtHome.value,
-  });
-});
-
-let dropdownItems = ref([
-  {
-    label: authStore.isAuthenticated ? 'Home' : 'Log in',
-    icon: authStore.isAuthenticated ? 'pi pi-user' : 'pi pi-sign-in',
-    command: () => {
-      authStore.isAuthenticated ? router.push({ path: APP_ROUTES.HOME }) : router.push({ path: APP_ROUTES.SIGN_IN });
-    },
-  },
-  {
-    label: 'Sign Out',
-    icon: 'pi pi-sign-out',
-    command: () => signOut(),
-  },
-]);
-
-if (authStore.isAuthenticated && roarfirekit.value?.userData?.userType === 'admin') {
-  dropdownItems.value.splice(
-    1,
-    0,
-    {
-      label: 'Student Upload',
-      icon: 'pi pi-users',
-      command: () => {
-        router.push({ name: 'RegisterStudents' });
-      },
-    },
-    {
-      label: 'Query',
-      icon: 'pi pi-cloud-download',
-      command: () => {
-        router.push({ name: 'Query' });
-      },
-    },
-    {
-      label: 'Score Report',
-      icon: 'pi pi-upload',
-      command: () => {
-        router.push({ name: 'UploadScores' });
-      },
-    },
-  );
-}
 
 const toggleMenu = (event) => {
   menu.value.toggle(event);
---
+++ src/containers/Navigation/Navigation.vue
@@ -0,0 +1,145 @@
+<template>
+  <NavBar
+    v-if="displayNavbar"
+    :displayName
+    :menuItems
+    :logo="logoOverride"
+    :onSignOut="signOut"
+    :showAccountSettingsLink="isAdmin.value"
+  />
+</template>
+
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import { storeToRefs } from 'pinia';
+import { useAuthStore } from '@/store/auth';
+import useUserType from '@/composables/useUserType';
+import useUserClaimsQuery from '@/composables/queries/useUserClaimsQuery';
+import useSignOutMutation from '@/composables/mutations/useSignOutMutation';
+import { getSidebarActions } from '@/router/sidebarActions';
+import NavBar from '@/components/NavBar.vue';
+
+const router = useRouter();
+const route = useRoute();
+const authStore = useAuthStore();
+const { roarfirekit } = storeToRefs(authStore);
+
+const initialized = ref(false);
+
+const isLevante = import.meta.env.MODE === 'LEVANTE';
+
+let unsubscribe;
+
+const init = () => {
+  if (unsubscribe) unsubscribe();
+  initialized.value = true;
+};
+
+unsubscribe = authStore.$subscribe(async (mutation, state) => {
+  if (state.roarfirekit.restConfig) init();
+});
+
+const { mutate: signOut } = useSignOutMutation();
+
+const { isLoading: isLoadingClaims, data: userClaims } = useUserClaimsQuery({
+  enabled: initialized,
+});
+
+const { isAdmin, isSuperAdmin } = useUserType(userClaims);
+
+// @TODO: Move the navbar blacklist to route meta definitions.
+const navbarBlacklist = [
+  'SignIn',
+  'Register',
+  'Maintenance',
+  'PlayApp',
+  'SWR',
+  'SWR-ES',
+  'SRE',
+  'SRE-ES',
+  'PA',
+  'PA-ES',
+  'Letter',
+  'Letter-ES',
+  'Vocab',
+  'Multichoice',
+  'Morphology',
+  'Cva',
+  'Fluency-ARF',
+  'Fluency-ARF-ES',
+  'Fluency-CALF',
+  'Fluency-CALF-ES',
+  'Fluency-Alpaca',
+  'Fluency-Alpaca-ES',
+  'RAN',
+  'Crowding',
+  'MEP',
+];
+
+const displayNavbar = computed(() => {
+  return !navbarBlacklist.includes(route.name);
+});
+
+const logoOverride = computed(() => {
+  return isLevante ? '/LEVANTE/Levante_Logo.png' : null;
+});
+
+const displayName = computed(() => {
+  if (!isLoadingClaims) {
+    return '';
+  } else {
+    let email = authStore?.userData?.email;
+
+    if (email && email.split('@')[1] === 'roar-auth.com') {
+      email = email.split('@')[0];
+    }
+    const displayName = authStore?.userData?.displayName;
+    const username = authStore?.userData?.username;
+    const firstName = authStore?.userData?.name?.first;
+
+    const userType = isAdmin.value ? 'Admin' : 'User';
+
+    return `${firstName || displayName || username || email || userType}`;
+  }
+});
+
+const defaultMenuActions = computed(() => {
+  return getSidebarActions({
+    isSuperAdmin: isSuperAdmin.value,
+    isAdmin: isSuperAdmin.value || isAdmin.value,
+  });
+});
+
+const menuItems = computed(() => {
+  const items = [];
+  const menuEntries = ['Administrations', 'Organizations', 'Users'];
+
+  for (const entry of menuEntries) {
+    const headerItems = defaultMenuActions.value
+      .filter((action) => action.category === entry)
+      .map((action) => {
+        return {
+          label: action.title,
+          icon: action.icon,
+          command: () => {
+            router.push(action.buttonLink);
+          },
+        };
+      });
+
+    if (headerItems.length > 0) {
+      items.push({
+        label: entry,
+        items: headerItems,
+      });
+    }
+  }
+
+  return items;
+});
+
+onMounted(() => {
+  if (roarfirekit.value.restConfig) init();
+});
+</script>
