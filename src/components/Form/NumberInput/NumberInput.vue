6ad798c97bb2babf857e6605fd58d4974955bbad
Maximilian Oertel <info@maximilianoertel.de>
2024-12-11 15:00:09 +0000

    Introduce NumberInput component

---
+++ src/components/Form/NumberInput/NumberInput.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="form-section">
+    <div class="p-input-icon-right">
+      <label :for="id" class="block mb-1" :class="{ 'sr-only': labelHidden }">
+        <small class="text-gray-500 font-bold">{{ label }}</small>
+        <span v-if="required" class="ml-1 text-gray-500">*</span>
+      </label>
+
+      <PvInputNumber
+        :id="id"
+        v-model="model"
+        :class="{ 'p-invalid border-red-500': isInvalid || hasErrors }"
+        :placeholder="placeholder"
+        :disabled="disabled"
+      />
+    </div>
+    <span v-if="hasErrors" class="absolute">
+      <span v-for="(error, index) of errors" :key="index">
+        <small class="text-xs p-error">{{ error.$message }}</small>
+      </span>
+    </span>
+  </div>
+</template>
+
+<script setup>
+import PvInputNumber from 'primevue/inputnumber';
+import { computed } from 'vue';
+
+const model = defineModel({ required: true, type: String });
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  labelHidden: {
+    type: Boolean,
+    default: false,
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+  isInvalid: {
+    type: Boolean,
+    required: true,
+  },
+  errors: {
+    type: Array,
+    required: true,
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const hasErrors = computed(() => props.errors?.length > 0);
+</script>
+
+<style scoped>
+/* @TODO: Replace in favour of TailwindCSS class overrides once yeatmanlab/roar-project-management/issues/321 is resolved */
+.p-component:disabled {
+  background: var(--surface-a);
+  border: 1px solid var(--surface-300);
+  transition: none;
+  appearance: none;
+  border-radius: var(--border-radius);
+  opacity: 0.6;
+  color: var(--text-color);
+}
+</style>
