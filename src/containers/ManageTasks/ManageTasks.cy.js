66e378432fb61f08ec82c8ebc12ab10aea5ea19b
Maximilian Oertel <info@maximilianoertel.de>
2024-12-16 17:36:09 +0000

    Switch to testId attributes

--- src/containers/ManageTasks/ManageTasks.cy.js
+++ src/containers/ManageTasks/ManageTasks.cy.js
@@ -5,37 +5,37 @@ describe('<ManageTasks />', () => {
     cy.mount(ManageTasks, {
       global: {
         stubs: {
-          CreateTaskForm: { template: '<div data-test="create-task-form-stub">Create Form Stub</div>' },
-          UpdateTaskForm: { template: '<div data-test="update-task-form-stub">Update Form Stub</div>' },
+          CreateTaskForm: { template: '<div data-testId="create-task-form-stub">Create Form Stub</div>' },
+          UpdateTaskForm: { template: '<div data-testId="update-task-form-stub">Update Form Stub</div>' },
         },
       },
     });
 
-    cy.get('[data-test="create-task-form-stub"]').should('exist').should('be.visible');
-    cy.get('[data-test="update-task-form-stub"]').should('exist').should('not.be.visible');
+    cy.findByTestId('create-task-form-stub').should('exist').should('be.visible');
+    cy.findByTestId('update-task-form-stub').should('exist').should('not.be.visible');
   });
 
   it('Lets the user switch between create and update forms', () => {
     cy.mount(ManageTasks, {
       global: {
         stubs: {
-          CreateTaskForm: { template: '<div data-test="create-task-form-stub">Create Form Stub</div>' },
-          UpdateTaskForm: { template: '<div data-test="update-task-form-stub">Update Form Stub</div>' },
+          CreateTaskForm: { template: '<div data-testId="create-task-form-stub">Create Form Stub</div>' },
+          UpdateTaskForm: { template: '<div data-testId="update-task-form-stub">Update Form Stub</div>' },
         },
       },
     });
 
-    cy.get('[data-test="create-task-form-stub"]').should('exist').should('be.visible');
-    cy.get('[data-test="update-task-form-stub"]').should('exist').should('not.be.visible');
+    cy.findByTestId('create-task-form-stub').should('exist').should('be.visible');
+    cy.findByTestId('update-task-form-stub').should('exist').should('not.be.visible');
 
     cy.findByTestId('manage-tasks__view-select').findByText('Update Task').click();
 
-    cy.get('[data-test="create-task-form-stub"]').should('exist').should('not.be.visible');
-    cy.get('[data-test="update-task-form-stub"]').should('exist').should('be.visible');
+    cy.findByTestId('create-task-form-stub').should('exist').should('not.be.visible');
+    cy.findByTestId('update-task-form-stub').should('exist').should('be.visible');
 
     cy.findByTestId('manage-tasks__view-select').findByText('Create Task').click();
 
-    cy.get('[data-test="create-task-form-stub"]').should('exist').should('be.visible');
-    cy.get('[data-test="update-task-form-stub"]').should('exist').should('not.be.visible');
+    cy.findByTestId('create-task-form-stub').should('exist').should('be.visible');
+    cy.findByTestId('update-task-form-stub').should('exist').should('not.be.visible');
   });
 });
