beab694abbe633910ba513ca02fafce15e0d6b41
Elijah Kelly <kellyel@stanford.edu>
2025-02-13 13:23:33 -0800

    Simplify unauthorized page

--- src/pages/Unauthorized.vue
+++ src/pages/Unauthorized.vue
@@ -1,12 +1,6 @@
 <template>
-  <div id="signin-container">
-    <section id="signin">
-      <header>
-        <div class="signin-logo">
-          <PvImage v-if="isLevante" src="/LEVANTE/Levante_Logo.png" alt="LEVANTE Logo" width="200" />
-          <ROARLogoShort v-else />
-        </div>
-      </header>
+  <div>
+    <section id="signin" class="w-full">
       <section class="px-3 flex flex-column gap-3 text-center align-items-center">
         <h1>Oops!</h1>
         <span class="font-light font-md">You do not have sufficient permission to view that page.</span>
@@ -16,7 +10,6 @@
   </div>
 </template>
 <script setup>
-import { onMounted, onBeforeUnmount } from 'vue';
 import PvButton from 'primevue/button';
 import ROARLogoShort from '@/assets/RoarLogo-Short.vue';
 import { useRouter } from 'vue-router';
@@ -26,12 +19,4 @@ const router = useRouter();
 function goBack() {
   router.go(-1);
 }
-
-onMounted(() => {
-  document.body.classList.add('page-signin');
-});
-
-onBeforeUnmount(() => {
-  document.body.classList.remove('page-signin');
-});
 </script>
