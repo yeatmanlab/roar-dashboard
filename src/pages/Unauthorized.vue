ac44d206259545b0c740e8f7fe10ea97f8cfca03
Elijah Kelly <kellyel@stanford.edu>
2025-02-25 13:24:49 -0800

    Update unauthorized.vue

--- src/pages/Unauthorized.vue
+++ src/pages/Unauthorized.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <section id="signin" class="w-full">
+    <main class="w-full">
       <section class="px-3 flex flex-column gap-3 text-center align-items-center">
         <h1>Oops!</h1>
         <span class="font-light font-md">You do not have sufficient permission to view that page.</span>
         <PvButton label="Go Back" @click="goBack" />
       </section>
-    </section>
+    </main>
   </div>
 </template>
 <script setup>
