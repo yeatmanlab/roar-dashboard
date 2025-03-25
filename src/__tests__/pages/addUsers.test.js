1a229dc029b300185327749c4e6a1e0cd68dffe4
Zio-4 <philipz4848@gmail.com>
2025-03-25 13:13:59 -0700

    more test stuff

--- src/__tests__/pages/addUsers.test.js
+++ src/__tests__/pages/addUsers.test.js
@@ -1,10 +1,21 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { mount } from '@vue/test-utils';
+import { createPinia, setActivePinia } from 'pinia';
+import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
 import AddUsers from '../../pages/LEVANTE/addUsers.vue';
 
 describe('Add Users Page', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
   it('should render the add users page', () => {
-    const wrapper = mount(AddUsers);
+    const wrapper = mount(AddUsers, {
+      global: {
+        plugins: [PrimeVue, ToastService]
+      }
+    });
     expect(wrapper.html()).toMatchSnapshot();
   });
 });
