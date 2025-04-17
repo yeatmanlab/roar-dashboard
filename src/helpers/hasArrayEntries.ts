89ce7b35c17abb8dcba7c4092a1ac51ff42aae7e
digital-pro <github@proshooters.com>
2025-04-17 15:52:43 -0700

    additional files

--- src/helpers/hasArrayEntries.ts
+++ src/helpers/hasArrayEntries.ts
@@ -1,11 +1,12 @@
-import { toValue } from 'vue';
+import { toValue, MaybeRefOrGetter } from 'vue';
 
 /**
  * Test if an array has entries.
  *
- * @param {Array} array – The array to check for entries.
+ * @param {MaybeRefOrGetter<any[]>} array – The array (or ref/getter) to check for entries.
  * @returns {boolean} Whether the array has entries.
  */
-export const hasArrayEntries = (array) => {
-  return Array.isArray(toValue(array)) && toValue(array).length > 0;
+export const hasArrayEntries = (array: MaybeRefOrGetter<any[]>): boolean => {
+  const value = toValue(array);
+  return Array.isArray(value) && value.length > 0;
 };
