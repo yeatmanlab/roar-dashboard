f729cee0525b91daaffc9dcf00d1f5d6b0948365
Emily Arteaga-Garcia <62304493+Emily-ejag@users.noreply.github.com>
2025-05-07 15:51:34 -0700

    any external qualtrics taskId pass  a participantId param (#1181)

--- src/components/GameTabs.vue
+++ src/components/GameTabs.vue
@@ -303,8 +303,8 @@ async function routeExternalTask(game) {
   // Mark the assessment as complete immediately if external roar task and clicked the button
   if (game.taskData.external) {
     await authStore.completeAssessment(selectedAdmin.value.id, game.taskId);
-    if (game.taskId === 'qualtrics-experience') {
-      // this was a request for this specific task
+    if (game.taskId.includes('qualtrics')) {
+      // this was a request for a qualtrics-based task
       window.open(`${game.taskData.taskURL}/?participantID=${props?.userData?.assessmentPid}`, '_blank').focus();
     }
     window.open(game.taskData.taskURL, '_blank').focus();
