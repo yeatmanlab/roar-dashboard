cf6a134f9760a22922df0ccd8e319df3d215bbcd
Elijah Kelly <kellyel@stanford.edu>
2025-05-28 13:22:13 -0700

    Remove incorrect AI change suggestion

--- src/composables/queries/useUserAssignmentsQuery.js
+++ src/composables/queries/useUserAssignmentsQuery.js
@@ -30,7 +30,7 @@ const useUserAssignmentsQuery = (queryOptions = undefined, userId = null, orgTyp
 
   return useQuery({
     queryKey: [USER_ASSIGNMENTS_QUERY_KEY, uid, orgType, orgIds],
-    queryFn: () => getUserAssignments(uid.value, orgType, orgIds, isTestUser),
+    queryFn: () => getUserAssignments(uid.value, orgType?.value, orgIds?.value, isTestUser),
     // Refetch on window focus for MEFS assessments as those are opened in a separate tab.
     refetchOnWindowFocus: 'always',
     enabled: isQueryEnabled,
