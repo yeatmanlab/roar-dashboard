db9cd4f9767e06793b1ad771a8fc070eacfa8f59
Elijah Kelly <kellyel@stanford.edu>
2025-06-03 13:53:40 -0700

    Make end dates overlap with next preset

--- src/components/AdministrationDatePicker/presets.js
+++ src/components/AdministrationDatePicker/presets.js
@@ -3,24 +3,24 @@ export const datePresets = Object.freeze({
     // June 16th to July 31st
     label: 'Summer',
     start: new Date(new Date().getFullYear(), 5, 16),
-    end: new Date(new Date().getFullYear(), 6, 31),
+    end: new Date(new Date().getFullYear(), 7, 1),
   },
   fall: {
     // August 1st to November 30th
     label: 'Fall',
     start: new Date(new Date().getFullYear(), 7, 1),
-    end: new Date(new Date().getFullYear(), 11, 30),
+    end: new Date(new Date().getFullYear(), 11, 1),
   },
   winter: {
     // December 1st to March 31st
     label: 'Winter',
     start: new Date(new Date().getFullYear(), 11, 1),
-    end: new Date(new Date().getFullYear() + 1, 2, 31),
+    end: new Date(new Date().getFullYear() + 1, 3, 1),
   },
   spring: {
     // April 1st to June 15th
     label: 'Spring',
     start: new Date(new Date().getFullYear() + 1, 3, 1),
-    end: new Date(new Date().getFullYear() + 1, 5, 15),
+    end: new Date(new Date().getFullYear() + 1, 5, 16),
   },
 });
