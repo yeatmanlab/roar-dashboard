ffc181d0c99c8945ad746ac86b336861ac50a62e
Rodrigo Olivares <rolivares6793@gmail.com>
2025-06-04 12:29:49 -0300

    added unit test for Report an Issue in UserActions

---
+++ src/components/tests/UserActions.test.js
@@ -0,0 +1,64 @@
+import UserActions from '@/components/UserActions.vue';
+import * as VueQuery from '@tanstack/vue-query';
+import { mount } from '@vue/test-utils';
+import { createPinia, setActivePinia } from 'pinia';
+import PvButton from 'primevue/button';
+import PrimeVue from 'primevue/config';
+import PvSelect from 'primevue/select';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { ref } from 'vue';
+import { createI18n } from 'vue-i18n';
+
+vi.mock('@/composables/queries/useUserClaimsQuery', () => ({
+  default: vi.fn(() => ({
+    data: ref({
+      //
+    }),
+  })),
+}));
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  messages: {
+    en: {
+      navBar: {
+        signOut: 'Sign Out',
+      },
+    },
+  },
+});
+
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+beforeEach(() => {
+  setActivePinia(createPinia());
+});
+
+describe('UserActions', () => {
+  it('should redirect user to notion after selecting Report an Issue in the dropdown', async () => {
+    const windowOpenSpy = vi.spyOn(window, 'open').mockImplementation(() => {});
+
+    const wrapper = mount(UserActions, {
+      global: {
+        plugins: [VueQuery.VueQueryPlugin, PrimeVue, i18n],
+        components: {
+          PvButton,
+          PvSelect,
+        },
+      },
+    });
+
+    const selects = wrapper.findAllComponents(PvSelect);
+    const helpSelect = selects[0];
+
+    helpSelect.vm.$emit('change', { value: 'reportAnIssue' });
+
+    expect(windowOpenSpy).toHaveBeenCalledWith(
+      'https://watery-wrench-dee.notion.site/13c244e26d9b8005adbde4522455edfd',
+      '_blank',
+    );
+  });
+});
