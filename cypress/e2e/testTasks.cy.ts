61b090149e98b7d1920e6e98cb2f99d38ed1020c
zwatson2001 <zwatson@vassar.edu>
2025-06-09 13:38:06 -0700

    fix bug in task selection and enter full screen for each task

--- cypress/e2e/testTasks.cy.ts
+++ cypress/e2e/testTasks.cy.ts
@@ -1,17 +1,26 @@
+import 'cypress-real-events';
+
 const dashboardUrl = "https://localhost:5173/signin";
+const username = "57xpeiz2b4@levante.com"; 
+const password = "u31p03umu5"; 
 
 // starts each task and checks that it has loaded (the 'OK' button is present)
 function startTask(tasksRemaining: number) {
   cy.get("[data-pc-section=tablist]", {timeout: 30000}).children().then((taskTabs) => {
-      
-    cy.wrap(taskTabs.eq(tasksRemaining)).click(); 
+   
+    // start task
+    cy.wrap(taskTabs.eq(tasksRemaining)).click();
     cy.scrollTo('bottomLeft');
-    cy.contains("click to start", {matchCase: false}).click({force: true});
+    cy.get("[data-pc-name=tabpanel][data-p-active=true]").children().contains("Click to start").click();
 
-    cy.contains('OK', {timeout: 120000}).should('exist');
+    // enter fullscreen and check that first instruction trial has loaded
+    cy.contains('OK', {timeout: 240000}).should('exist').realClick().then(() => {
+      cy.contains('OK').should('exist');
+    });
 
+    // return to dashboard
     cy.go('back');
-    cy.get("[data-pc-section=tablist]", {timeout: 120000}).should('exist').then(() => {
+    cy.get("[data-pc-section=tablist]", {timeout: 240000}).should('exist').then(() => {
       if (tasksRemaining === 0) {
         return; 
       } else {
@@ -28,20 +37,20 @@ describe('test core tasks from dashboard', () => {
 
     // input username
     cy.get("input").should("have.length", 2).then((inputs) => {
-        cy.wrap(inputs[0]).type("57xpeiz2b4@levante.com");
+        cy.wrap(inputs[0]).type(username);
     });
 
     // input password
     cy.get("input").should("have.length", 2).then((inputs) => {
-        cy.wrap(inputs[1]).type("u31p03umu5");
+        cy.wrap(inputs[1]).type(password);
     });
 
     // click go button
     cy.get("button").filter("[data-pc-name=button]").click();
 
     // check that each task loads
-    cy.get("[data-pc-section=tablist]", {timeout: 120000}).children().then((children) => {
-        const tasksToComplete: number = Array.from(children).length - 1; 
+    cy.get("[data-pc-section=tablist]", {timeout: 240000}).children().then((children) => {
+        const tasksToComplete: number = Array.from(children).length - 2; 
         startTask(tasksToComplete);
     }); 
 
