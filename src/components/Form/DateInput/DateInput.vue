51a68fa42fe6d6d4ea1190725f671fb34cc11c7e
Elijah Kelly <127352507+kellyel@users.noreply.github.com>
2025-06-17 10:11:58 -0700

    Introduce presets for Administration Dates (#1229)
    * Introduce duplication mode to administration form
    
    * Set variable name to formType
    
    * Correct typo
    
    * Save form types as constants
    
    * Fix OrgPicker not displaying pre-selected orgs
    
    * Fix variants not appearing on first load
    
    * Stylize loading message
    
    * remove duplicated class
    
    * lint
    
    * 3.9.6
    
    * Move TaskPicker and VariantCard to component folders
    
    * Create DatePicker components
    
    * Set date presets
    
    * Style AdministrationDatePicker component
    
    * Handle dates being passed in
    
    * Nudge icon css
    
    * Create tests for AdministrationDatePicker
    
    * Create component test for DatePicker
    
    * Create tests for DateInput
    
    * Make end dates overlap with next preset
    
    * Linting
    
    * Prevent layout thrashing
    
    * Correct auto-import mistakes
    
    * Change test data attribute, create enum for datepicker modes
    
    * Change preset generation to use UTC
    
    * Update test
    
    * Make toggle button full width
    
    * Remove need for eslint-disable
    
    * Make component responsive while minimizing screen reflows for desktop
    
    * Make preset cards semantically significant and present in the tabOrder
    
    * Create tests for generateDatePreset function
    
    * Rename test file, remove fixed height
    
    ---------
    
    Co-authored-by: Adam Richie-Halford <richford@users.noreply.github.com>

---
+++ src/components/Form/DateInput/DateInput.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="col-12 md:col-6">
+    <PvFloatLabel>
+      <PvDatePicker
+        v-model="model"
+        class="w-full"
+        show-icon
+        :min-date="minDate"
+        :max-date="maxDate"
+        :manual-input="manualInput"
+        icon="pi pi-calendar text-primary p-1"
+        input-id="datePicker"
+        show-button-bar
+        :data-testid="testId"
+      />
+      <label for="datePicker">{{ label }}</label>
+      <small v-if="hasError" class="p-error">{{ errorMessage }}</small>
+    </PvFloatLabel>
+  </div>
+</template>
+
+<script setup>
+import PvDatePicker from 'primevue/datepicker';
+import PvFloatLabel from 'primevue/floatlabel';
+
+const model = defineModel({ type: Date });
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  testId: {
+    type: String,
+    required: true,
+    default: 'date-input',
+  },
+  hasError: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  errorMessage: {
+    type: String,
+    required: false,
+    default: 'Please select a valid date',
+  },
+  minDate: {
+    type: Date,
+    required: false,
+    default: null,
+  },
+  maxDate: {
+    type: Date,
+    required: false,
+    default: null,
+  },
+  manualInput: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+</script>
