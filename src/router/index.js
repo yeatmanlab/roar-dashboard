aa4079bdf13add1407826847f33ed8ed577ebe42
Amy Yeung <ayeung21@stanford.edu>
2025-06-18 13:21:22 -0400

    Validate query parameters

--- src/router/index.js
+++ src/router/index.js
@@ -787,8 +787,11 @@ router.beforeEach(async (to, from, next) => {
     if (to.fullPath === '/') {
       next({ name: 'SignIn' });
       return;
-    } else {
+    } else if (to.fullPath.startsWith('/') && !to.fullPath.startsWith('//') && !to.fullPath.match(/^[a-z]+:/i)) {
       next({ name: 'SignIn', query: { redirect_to: to.fullPath } });
+      return;
+    } else {
+      next({ name: 'Unauthorized' });
       return;
     }
   }
