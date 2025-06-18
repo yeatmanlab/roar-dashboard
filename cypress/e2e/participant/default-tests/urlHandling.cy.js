ad785994dbd3d24153e78b43abf8c35f0576c015
Amy Yeung <ayeung21@stanford.edu>
2025-06-18 17:34:58 -0400

    Add few tests and revise check logic

--- cypress/e2e/participant/default-tests/urlHandling.cy.js
+++ cypress/e2e/participant/default-tests/urlHandling.cy.js
@@ -1,7 +1,7 @@
 describe('Participant: URL Handling', () => {
   it('Redirects to login when unauthenticated user visits home', () => {
     cy.visit('/');
-    cy.wait(.2 *Cypress.env('timeout'));
+    cy.wait(0.2 * Cypress.env('timeout'));
     cy.url().should('eq', `${Cypress.config().baseUrl}/signin`);
   });
 
@@ -9,13 +9,13 @@ describe('Participant: URL Handling', () => {
     // Randomized score and group values
     const randomizedUrl = '/scores/1xzvQB2LuEtOpRyMmsIRD/group/U0CJq53s79w8HShlOEDy';
     cy.visit(randomizedUrl);
-    cy.wait(.2 * Cypress.env('timeout'));
+    cy.wait(0.2 * Cypress.env('timeout'));
     cy.url().should('eq', `${Cypress.config().baseUrl}/signin?redirect_to=${randomizedUrl}`);
   });
 
   it('Redirects to login with redirect_to=/unauthorized when unauthenticated user appends protocol prefixes', () => {
     cy.visit('/https://example.com');
-    cy.wait(.2 * Cypress.env('timeout'));
+    cy.wait(0.2 * Cypress.env('timeout'));
     cy.url().should('eq', `${Cypress.config().baseUrl}/signin?redirect_to=/unauthorized`);
   });
-})
+});
