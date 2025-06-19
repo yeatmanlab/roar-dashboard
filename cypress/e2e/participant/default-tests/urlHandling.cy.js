e4691674c10611fcd9777f4a0f9e0ea394095bb2
Amy Yeung <ayeung21@stanford.edu>
2025-06-19 12:41:18 -0400

    Update test url

--- cypress/e2e/participant/default-tests/urlHandling.cy.js
+++ cypress/e2e/participant/default-tests/urlHandling.cy.js
@@ -7,10 +7,8 @@ describe('Participant: URL Handling', () => {
   });
 
   it('Redirects to login with redirect_to set to previous path when unauthenticated user visits a protected route', () => {
-    // Randomized score and group values
-    const randomizedUrl = '/scores/1xzvQB2LuEtOpRyMmsIRD/group/U0CJq53s79w8HShlOEDy';
-    cy.visit(randomizedUrl);
+    cy.visit('/list-orgs');
     cy.wait(0.2 * Cypress.env('timeout'));
-    cy.url().should('eq', `${Cypress.config().baseUrl}/signin?redirect_to=${randomizedUrl}`);
+    cy.url().should('eq', `${Cypress.config().baseUrl}/signin?redirect_to=/list-orgs`);
   });
 });
