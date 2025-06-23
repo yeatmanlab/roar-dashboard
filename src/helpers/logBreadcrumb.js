f934cd42e989dcfdc389d85d8743516d7443c308
Amy Yeung <ayeung21@stanford.edu>
2025-06-23 18:49:45 -0400

    Add description of params and expected values

--- src/helpers/logBreadcrumb.js
+++ src/helpers/logBreadcrumb.js
@@ -1,5 +1,17 @@
 import { addBreadcrumb } from '@sentry/vue';
 
+/**
+ * Logs a Sentry breadcrumb.
+ *
+ * @param {Object} options
+ * @param {string} options.category - Event context.
+ * @param {Object} options.data - Expected fields by category:
+ *   - auth: { roarUid: string, userType: string, provider: string, details?: object }
+ *   - admin: { adminUid: string, role: string }
+ *   - assignment: { assignmentId: string, userId: string }
+ * @param {string} options.message - Short event summary.
+ * @param {string} [options.level='info'] - Severity, defaults to 'info'.
+ */
 export const logBreadcrumb = ({ category, data, message, level = 'info' }) => {
   addBreadcrumb({
     category,
