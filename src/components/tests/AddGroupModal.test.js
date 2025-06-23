3f51d07038e3a4d6d40425b072457d8cbab1048b
Rodrigo Olivares <rolivares6793@gmail.com>
2025-06-23 15:36:41 -0300

    updated unit test and added another one for the new composable

--- src/components/tests/AddGroupModal.test.js
+++ src/components/tests/AddGroupModal.test.js
@@ -1,6 +1,6 @@
 import AddGroupModal from '@/components/modals/AddGroupModal.vue';
 import * as VueQuery from '@tanstack/vue-query';
-import { mount } from '@vue/test-utils';
+import { flushPromises, mount } from '@vue/test-utils';
 import { createPinia, setActivePinia } from 'pinia';
 import PvAutoComplete from 'primevue/autocomplete';
 import PvButton from 'primevue/button';
@@ -13,13 +13,22 @@ import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { nextTick } from 'vue';
 
 const mockUseUpsertOrgMutation = vi.fn();
+let mockOrgNameExists = false;
 
 vi.mock('@/composables/mutations/useUpsertOrgMutation', () => ({
-  default: () => ({
+  default: vi.fn(() => ({
     mutate: mockUseUpsertOrgMutation,
     isPending: false,
     error: null,
-  }),
+  })),
+}));
+
+vi.mock('@/composables/queries/useOrgNameExistsQuery', () => ({
+  default: vi.fn(() => ({
+    refetch: vi.fn().mockResolvedValue({
+      data: mockOrgNameExists,
+    }),
+  })),
 }));
 
 vi.mock('primevue/usetoast', () => ({
@@ -47,6 +56,7 @@ const mountOptions = {
 };
 
 beforeEach(() => {
+  mockOrgNameExists = false;
   setActivePinia(createPinia());
 });
 
@@ -98,6 +108,54 @@ describe('AddGroupModal.vue', () => {
     wrapper.unmount();
   });
 
+  it('should NOT allow users to create a 2 or more groups with the same name', async () => {
+    mockOrgNameExists = true;
+
+    const wrapper = mount(AddGroupModal, mountOptions);
+    await nextTick();
+
+    // First, we select the orgType dropdown
+    const orgTypeDropdown = document.querySelector('[data-cy="dropdown-org-type"]');
+    expect(orgTypeDropdown).not.toBeNull();
+    // Then we click it
+    await orgTypeDropdown.click();
+    await nextTick();
+
+    // Now, select the options to make sure the click action worked
+    const orgTypeDropdownOptions = document.querySelectorAll('.p-select-option');
+    // We must have 4 options: Site, School, Class and Cohort
+    expect(orgTypeDropdownOptions.length).toBe(4);
+
+    // Select the Site option and click on it
+    const siteOption = orgTypeDropdownOptions.find((option) => option.textContent === 'Site');
+    expect(siteOption).not.toBeNull();
+    await siteOption.click();
+    await nextTick();
+
+    // Now we provide a site name
+    const orgName = document.querySelector('[data-cy="input-org-name"]');
+    expect(orgName).not.toBeNull();
+    orgName.value = 'Test Site';
+    orgName.dispatchEvent(new Event('input'));
+    await nextTick();
+
+    // Mocking the vuelidate
+    wrapper.vm.v$.$validate = () => Promise.resolve(true);
+
+    // After that, we select the submit button and check if it says "Add Site"
+    const submitBtn = document.querySelector('[data-testid="submitBtn"]');
+    expect(submitBtn).not.toBeNull();
+    expect(submitBtn.textContent).toContain('Add Site');
+
+    await submitBtn.click();
+    await flushPromises();
+
+    // It should NOT call the upsertOrg mutation
+    expect(mockUseUpsertOrgMutation).toHaveBeenCalledTimes(0);
+
+    wrapper.unmount();
+  });
+
   it('should submit the form if everything is ok', async () => {
     const wrapper = mount(AddGroupModal, mountOptions);
     await nextTick();
@@ -136,6 +194,7 @@ describe('AddGroupModal.vue', () => {
     expect(submitBtn.textContent).toContain('Add Site');
 
     await submitBtn.click();
+    await flushPromises();
 
     const errorMessages = document.querySelectorAll('.p-error');
     // We should NOT have any errors
