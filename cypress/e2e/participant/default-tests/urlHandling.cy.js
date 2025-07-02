2219a79e64314a396a30507cf4e26c670298c348
Amy Yeung <ayeung21@stanford.edu>
2025-07-02 18:03:52 -0400

    Update prettier format

--- cypress/e2e/participant/default-tests/urlHandling.cy.js
+++ cypress/e2e/participant/default-tests/urlHandling.cy.js
@@ -21,22 +21,22 @@ describe('Participant: URL Handling', () => {
 
   it('Redirects to redirect_to path after successfully authenticating with email', () => {
     cy.visit('/game/core-tasks/trog');
-    
+
     cy.get('[data-cy="sign-in__username"]').type(PARTICIPANT_USERNAME, { log: false });
     cy.get('[data-cy="sign-in__password"]').type(PARTICIPANT_PASSWORD, { log: false });
-    
+
     cy.get('button').contains('Go!').click();
-    
+
     cy.wait(0.2 * Cypress.env('timeout'));
     cy.url().should('eq', `${Cypress.config().baseUrl}/game/core-tasks/trog`);
-  }); 
+  });
 
   it('Redirects to redirect_to path after successfully authenticating with Clever SSO', () => {
     const CLEVER_SSO_URL = Cypress.env('cleverOAuthLink');
-    
+
     cy.visit('/game/core-tasks/trog');
     cy.get('[data-cy="sign-in__clever-sso"]').contains('Clever').click();
-  
+
     cy.origin(
       CLEVER_SSO_URL,
       {
@@ -49,17 +49,17 @@ describe('Participant: URL Handling', () => {
       ({ schoolName, username, password }) => {
         cy.get('input[title="School name"]').type(schoolName);
         cy.get('ul > li').contains(schoolName).click();
-  
+
         cy.get('input#username').type(username);
         cy.get('input#password').type(password, { log: false });
         cy.wait(1000); // Add a delay to simulate user input, as Clever SSO is sensitive to rapid input.
         cy.get('button#UsernamePasswordForm--loginButton').click();
       },
     );
-    
+
     cy.wait(0.2 * Cypress.env('timeout'));
     cy.url().should('eq', `${Cypress.config().baseUrl}/game/core-tasks/trog`);
-    cy.visit('/')
+    cy.visit('/');
     return;
   });
 });
