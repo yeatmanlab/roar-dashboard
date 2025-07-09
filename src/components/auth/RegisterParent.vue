89d75dfbca56ac703340a98af814683a9c19b5e3
Ḻuçås Ṩøng <lucas7song@gmail.com>
2025-07-09 11:29:32 -0700

    Update src/components/auth/RegisterParent.vue
    Co-authored-by: Maximilian Oertel <info@maximilianoertel.de>

--- src/components/auth/RegisterParent.vue
+++ src/components/auth/RegisterParent.vue
@@ -170,7 +170,7 @@
         </div>
       </form>
     </div>
-    <div class="flex align-items-center justify-content-center py-3 mt-3 border-top-1 border-gray-200">
+    <div class="text-center mt-3 border-top-1 border-gray-200">
       <RouterLink :to="APP_ROUTES.SIGN_IN" class="text-sm transition text-gray-600 hover:text-gray-800 p-3">
         Already have an account? Click here to sign in
       </RouterLink>
