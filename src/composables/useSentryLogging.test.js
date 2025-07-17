3b3c7ba045a3f393e5d5ac8bac9838bcbc8d443d
Amy Yeung <ayeung21@stanford.edu>
2025-07-17 14:45:01 -0700

    Add spacing

--- src/composables/useSentryLogging.test.js
+++ src/composables/useSentryLogging.test.js
@@ -1,7 +1,9 @@
 import { describe, it, expect, vi } from 'vitest';
 import { addBreadcrumb } from '@sentry/vue';
 import useSentryLogging from './useSentryLogging';
+
 const { logEvent, logNavEvent, logMediaEvent, logAuthEvent } = useSentryLogging();
+
 vi.mock('@sentry/vue', () => ({
   addBreadcrumb: vi.fn(),
 }));
