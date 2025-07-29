a3e997b0fea6ef8908cbee832b991b74c21a1200
Maximilian Oertel <info@maximilianoertel.de>
2025-07-29 16:45:56 +0200

    Fix testInviteGroupName

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -167,7 +167,7 @@ module.exports = defineConfig({
     testPartnerClassInitials: 'CTD-CTS-CTC',
     testPartnerGroupName: 'Cypress Test Group',
     testPartnerGroupInitials: 'CTG',
-    testInviteGroupName: 'Groups - testgrouplucas3',
+    testInviteGroupName: 'Districts - Cypress Test District',
     testGrade: 'Grade 5',
     stanfordUniversityAddress: '450 Jane Stanford Way, Stanford, CA 94305, USA',
     testTag: 'stanford university',
