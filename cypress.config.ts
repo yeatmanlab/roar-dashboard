2202e83a8db1cffeb4d71e0862d70da470b0c1e7
digital-pro <github@proshooters.com>
2025-08-13 16:20:14 -0700

    test: default E2E_BASE_URL to http://localhost:5173/signin for Cypress runs

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     },
     supportFile: false,
     env: {
-      E2E_BASE_URL: process.env.E2E_BASE_URL,
+      E2E_BASE_URL: process.env.E2E_BASE_URL || 'http://localhost:5173/signin',
       E2E_TEST_EMAIL: process.env.E2E_TEST_EMAIL,
       E2E_TEST_PASSWORD: process.env.E2E_TEST_PASSWORD,
     },
