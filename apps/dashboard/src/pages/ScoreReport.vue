0c926eaa69c14eaa7c2e6f0c88aae7e482a93d0e
Amy Yeung <ayeung21@stanford.edu>
2025-08-25 14:09:10 -0400

    Replace undefined with null

--- apps/dashboard/src/pages/ScoreReport.vue
+++ apps/dashboard/src/pages/ScoreReport.vue
@@ -850,11 +850,10 @@ const computeAssignmentAndRunData = computed(() => {
               }
             });
 
-            // Passing undefined for empty incorrectSkill arrays to prevent it from rendering under TableScoreTag.vue conditions.
+            // Passing null for empty incorrectSkill arrays to prevent it from rendering under TableScoreTag.vue conditions.
             currRowScores[taskId].composite = {
               ...scores.composite,
-              incorrectSkills:
-                scores.composite.incorrectSkills?.length > 0 ? scores.composite.incorrectSkills : undefined,
+              incorrectSkills: scores.composite.incorrectSkills?.length > 0 ? scores.composite.incorrectSkills : null,
               gradeEstimate: scores.composite.gradeEstimate ? _round(scores.composite.gradeEstimate, 2) : '',
               tagColor: getTagColor(scores.composite.supportLevel),
             };
