576655c220b3b8c9bbbfa8d51304d27c44f4636f
Maximilian Oertel <info@maximilianoertel.de>
2025-09-05 16:12:40 +0100

    Automate CSP injection (#1320)
    * Enhance CSP maintainability
    
    * Remove .prettierignore
    
    * Remove firebase-target input in CI
    
    ---------
    
    Co-authored-by: Elijah Kelly <127352507+kellyel@users.noreply.github.com>

--- packages/config-eslint/vue.js
+++ packages/config-eslint/vue.js
@@ -1,11 +1,11 @@
 import { config as baseConfig } from "./index.js";
-import vue from 'eslint-plugin-vue';
-import cypress from 'eslint-plugin-cypress';
-import globals from 'globals';
+import vue from "eslint-plugin-vue";
+import cypress from "eslint-plugin-cypress";
+import globals from "globals";
 
 export const config = [
   ...baseConfig,
-  ...vue.configs['flat/recommended'],
+  ...vue.configs["flat/recommended"],
 
   // Resolve Vite alias '@' -> './src' for JS/Vue projects
   {
@@ -22,49 +22,49 @@ export const config = [
 
   // General frontend JS
   {
-    files: ['**/src/**/*.js', '**/cypress/**/*.js'],
+    files: ["**/src/**/*.js", "**/cypress/**/*.js"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
         ...globals.browser,
-        process: 'readonly'
-      }
-    }
+        process: "readonly",
+      },
+    },
   },
 
   // Vue-specific config
   {
-    files: ['**/src/**/*.vue'],
+    files: ["**/src/**/*.vue"],
     plugins: { vue },
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
         ...globals.browser,
-        process: 'readonly' // @TODO: Remove and fix code.
-      }
+        process: "readonly", // @TODO: Remove and fix code.
+      },
     },
     rules: {
       // TODO: Re-enable and fix violations of the following rules:
-      'vue/max-attributes-per-line': 'off',
-      'vue/html-indent': 'off',
-      'vue/html-self-closing': 'off',
-      'vue/html-closing-bracket-newline': 'off',
-      'vue/singleline-html-element-content-newline': 'off',
-      'vue/multiline-html-element-content-newline': 'off',
+      "vue/max-attributes-per-line": "off",
+      "vue/html-indent": "off",
+      "vue/html-self-closing": "off",
+      "vue/html-closing-bracket-newline": "off",
+      "vue/singleline-html-element-content-newline": "off",
+      "vue/multiline-html-element-content-newline": "off",
       // @TODO: End.
 
-      'vue/multi-word-component-names': 'off',
-      'vue/no-undef-components': [
-        'error',
+      "vue/multi-word-component-names": "off",
+      "vue/no-undef-components": [
+        "error",
         {
           ignorePatterns: [
-            'AppSpinner',
-            'GMapAutocomplete', // vue-google-maps-community-fork
-            'i18n-t', // vue-i18n
-            'router-link', // vue-router
-            'router-view', // vue-router
+            "AppSpinner",
+            "GMapAutocomplete", // vue-google-maps-community-fork
+            "i18n-t", // vue-i18n
+            "router-link", // vue-router
+            "router-view", // vue-router
           ],
         },
       ],
@@ -74,45 +74,44 @@ export const config = [
   // Cypress-specific config
   cypress.configs.recommended,
   {
-    files: ['**/cypress/**/*.cy.js', '**/cypress/**/*.js'],
+    files: ["**/cypress/**/*.cy.js", "**/cypress/**/*.js"],
     plugins: { cypress },
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
     rules: {
       // @TODO: Re-enable and fix violations of the following rules
-      'cypress/no-unnecessary-waiting': 'warn',
-      'cypress/unsafe-to-chain-command': 'warn',
+      "cypress/no-unnecessary-waiting": "warn",
+      "cypress/unsafe-to-chain-command": "warn",
     },
   },
-
 
   // Cypress config
   {
     files: ['**/cypress.config.cjs'],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
         ...globals.node,
-        process: 'readonly',
-        __dirname: 'readonly',
-      }
-    }
+        process: "readonly",
+        __dirname: "readonly",
+      },
+    },
   },
 
   // Vite and Vitest configs
   {
-    files: ['**/vite.config.js', '**/vitest.config.js', '**/vitest.setup.js'],
+    files: ["**/vite.config.js", "**/vitest.config.js", "**/vitest.setup.js"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
         ...globals.node,
-        process: 'readonly',
-        __dirname: 'readonly',
-      }
-    }
-  }
+        process: "readonly",
+        __dirname: "readonly",
+      },
+    },
+  },
 ];
