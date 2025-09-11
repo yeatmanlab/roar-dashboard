c8e154dcee7910646e417131d76dec19f594249c
Maximilian Oertel <info@maximilianoertel.de>
2025-09-11 14:03:06 +0100

    Add users table
    (cherry picked from commit 343831e2f96e153eed5235f3c657ac2d8f5dfc28)
    (cherry picked from commit b6edbed20b5d75dfce0113d7b4fc1ff0b916327a)

---
+++ apps/backend/migrations/core/0004_add_users_table.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "app"."users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"assessment_pid" text NOT NULL,
+	"auth_provider" "app"."auth_provider"[] DEFAULT '{}'::"app"."auth_provider"[],
+	"auth_id" text,
+	"username" text,
+	"email" text,
+	"user_type" "app"."user_type",
+	"dob" date,
+	"grade" "app"."grade",
+	"school_level" "app"."school_level" GENERATED ALWAYS AS (app.get_school_level_from_grade(grade)) STORED,
+	"status_ell" boolean,
+	"status_frl" "app"."free_reduced_lunch_status",
+	"status_iep" boolean,
+	"student_id" text,
+	"sis_id" text,
+	"state_id" text,
+	"local_id" text,
+	"gender" text,
+	"race" text,
+	"hispanic_ethnicity" boolean,
+	"home_language" text,
+	"exclude_from_research" boolean DEFAULT false NOT NULL,
+	"updated_at" timestamp with time zone,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "users_assessmentPid_unique" UNIQUE("assessment_pid"),
+	CONSTRAINT "users_authId_unique" UNIQUE("auth_id"),
+	CONSTRAINT "users_username_unique" UNIQUE("username"),
+	CONSTRAINT "users_dob_in_past" CHECK ("app"."users"."dob" IS NULL OR "app"."users"."dob" <= now()::date)
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX "users_email_lower_uniqIdx" ON "app"."users" USING btree (lower("email")) WHERE "app"."users"."email" IS NOT NULL;
+--> statement-breakpoint
+
+-- Manual edit: 
+-- Add trigger to update updated_at
+DROP TRIGGER IF EXISTS users_set_updated_at ON app.users;
+CREATE TRIGGER users_set_updated_at
+BEFORE INSERT OR UPDATE ON app.users
+FOR EACH ROW
+EXECUTE FUNCTION app.set_updated_at();
--- apps/backend/src/db/schema/core/users.ts
+++ apps/backend/src/db/schema/core/users.ts
@@ -1,17 +1,72 @@
 import * as p from 'drizzle-orm/pg-core';
 import { sql } from 'drizzle-orm';
 import { timestamps } from '../common';
+import { authProviderEnum, gradeEnum, schoolLevelEnum, freeReducedLunchStatusEnum, userTypeEnum } from '../enums';
 
-export const users = p.pgTable('users', {
-  id: p
-    .uuid()
-    .default(sql`gen_random_uuid()`)
-    .primaryKey(),
-  authId: p.varchar().unique().notNull(),
-  email: p.varchar().unique().notNull(),
-  username: p.varchar(),
-  ...timestamps,
-});
+const db = p.pgSchema('app');
+
+/**
+ * Users Table
+ *
+ * Stores information about users in the system.
+ *
+ * - `assessmentPid`: The assessmentPid is a unique identifier for the user in the system. It is generated automatically
+ *   during the rostering process and usually includes the district and school prefixes.
+ *
+ * - `authId`: The authId should ideally be not nullable, but currently has to be nullable due to the way the rostering
+ *   process currently handles user syncing and auth record creation.
+ */
+
+export const users = db.table(
+  'users',
+  {
+    id: p
+      .uuid()
+      .default(sql`gen_random_uuid()`)
+      .primaryKey(),
+    assessmentPid: p.text().notNull().unique(),
+    authProvider: authProviderEnum()
+      .array()
+      .default(sql`'{}'::"app"."auth_provider"[]`),
+    authId: p.text().unique(),
+
+    username: p.text().unique(),
+    email: p.text(),
+    userType: userTypeEnum(),
+
+    dob: p.date(),
+    grade: gradeEnum(),
+    schoolLevel: schoolLevelEnum().generatedAlwaysAs(sql`app.get_school_level_from_grade(grade)`),
+
+    statusEll: p.boolean(),
+    statusFrl: freeReducedLunchStatusEnum(),
+    statusIep: p.boolean(),
+
+    studentId: p.text(),
+    sisId: p.text(),
+    stateId: p.text(),
+    localId: p.text(),
+
+    gender: p.text(),
+    race: p.text(),
+    hispanicEthnicity: p.boolean(),
+    homeLanguage: p.text(),
+
+    excludeFromResearch: p.boolean().notNull().default(false),
+
+    ...timestamps,
+  },
+  (table) => [
+    // Ensure case-insensitive email uniqueness
+    p
+      .uniqueIndex('users_email_lower_uniqIdx')
+      .on(sql`lower(${table.email})`)
+      .where(sql`${table.email} IS NOT NULL`),
+
+    // Ensure DOB is in the past
+    p.check('users_dob_in_past', sql`${table.dob} IS NULL OR ${table.dob} <= now()::date`),
+  ],
+);
 
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
