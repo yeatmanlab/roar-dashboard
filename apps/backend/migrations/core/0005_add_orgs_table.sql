8ad0c9289cd861fe0d716249dcc06fc5083a6026
Maximilian Oertel <info@maximilianoertel.de>
2025-09-12 11:26:39 +0100

    Add orgs table
    (cherry picked from commit 0da8bc8cb55d5dc18504e46d1d75422994d07655)
    (cherry picked from commit df2b27452d29d2b579e3773b56f8567655dd498c)

---
+++ apps/backend/migrations/core/0005_add_orgs_table.sql
@@ -0,0 +1,43 @@
+CREATE TABLE "app"."orgs" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"abbreviation" varchar(10) NOT NULL,
+	"name" text NOT NULL,
+	"org_type" "app"."org_type" NOT NULL,
+	"parent_org_id" uuid,
+	"location_address_line1" text,
+	"location_address_line2" text,
+	"location_city" text,
+	"location_state_province" text,
+	"location_postal_code" text,
+	"location_country" varchar(2),
+	"location_lat_long" "point",
+	"mdr_number" text,
+	"nces_id" text,
+	"state_id" text,
+	"school_number" text,
+	"is_rostering_root_org" boolean DEFAULT false NOT NULL,
+	"rostering_provider" "app"."rostering_provider" NOT NULL,
+	"rostering_provider_id" text,
+	"rostering_ended" timestamp with time zone,
+	"updated_at" timestamp with time zone,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "orgs_mdrNumber_unique" UNIQUE("mdr_number"),
+	CONSTRAINT "orgs_ncesId_unique" UNIQUE("nces_id"),
+	CONSTRAINT "orgs_stateId_unique" UNIQUE("state_id"),
+	CONSTRAINT "orgs_schoolNumber_unique" UNIQUE("school_number"),
+	CONSTRAINT "orgs_abbreviation_format" CHECK ("app"."orgs"."abbreviation" ~ '^[A-Za-z0-9]+$')
+);
+--> statement-breakpoint
+ALTER TABLE "app"."orgs" ADD CONSTRAINT "orgs_parent_org_id_orgs_id_fk" FOREIGN KEY ("parent_org_id") REFERENCES "app"."orgs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "orgs_parent_idx" ON "app"."orgs" USING btree ("parent_org_id");--> statement-breakpoint
+CREATE INDEX "orgs_parent_type_idx" ON "app"."orgs" USING btree ("parent_org_id","org_type");--> statement-breakpoint
+CREATE INDEX "orgs_name_lower_idx" ON "app"."orgs" USING btree (("name"));
+
+
+-- Manual edit: 
+-- Add trigger to update updated_at
+DROP TRIGGER IF EXISTS orgs_set_updated_at ON app.orgs;
+CREATE TRIGGER orgs_set_updated_at
+BEFORE INSERT OR UPDATE ON app.orgs
+FOR EACH ROW
+EXECUTE FUNCTION app.set_updated_at();
---
+++ apps/backend/src/db/schema/core/orgs.ts
@@ -0,0 +1,66 @@
+import * as p from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+import { timestamps } from '../common';
+import { orgTypeEnum, rosteringProviderEnum } from '../enums';
+import type { AnyPgColumn } from 'drizzle-orm/pg-core';
+
+const db = p.pgSchema('app');
+
+/**
+ * Orgs Table
+ *
+ * Stores information about orgs in the system. The orgs table contains hierarchical information about the orgs
+ * (district,school,local,state,region) in the system, defined by the parent_org_id column.
+ */
+
+export const orgs = db.table(
+  'orgs',
+  {
+    id: p
+      .uuid()
+      .default(sql`gen_random_uuid()`)
+      .primaryKey(),
+
+    abbreviation: p.varchar({ length: 10 }).notNull(),
+    name: p.text().notNull(),
+
+    orgType: orgTypeEnum().notNull(),
+    parentOrgId: p.uuid().references((): AnyPgColumn => orgs.id),
+
+    locationAddressLine1: p.text(),
+    locationAddressLine2: p.text(),
+    locationCity: p.text(),
+    locationStateProvince: p.text(),
+    locationPostalCode: p.text(),
+    locationCountry: p.varchar({ length: 2 }),
+    locationLatLong: p.point(),
+
+    mdrNumber: p.text().unique(),
+    ncesId: p.text().unique(),
+    stateId: p.text().unique(),
+    schoolNumber: p.text().unique(),
+
+    isRosteringRootOrg: p.boolean().notNull().default(false),
+    rosteringProvider: rosteringProviderEnum().notNull(),
+    rosteringProviderId: p.text(),
+    rosteringEnded: p.timestamp({ withTimezone: true }),
+
+    ...timestamps,
+  },
+  (table) => [
+    // Constraints
+    // - Ensure abbreviation format only contains letters and numbers
+    p.check('orgs_abbreviation_format', sql`${table.abbreviation} ~ '^[A-Za-z0-9]+$'`),
+
+    // Indexes
+    // - Hierachical lookups
+    p.index('orgs_parent_idx').on(table.parentOrgId),
+    p.index('orgs_parent_type_idx').on(table.parentOrgId, table.orgType),
+
+    // - Name equality or prefix lookups
+    p.index('orgs_name_lower_idx').on(sql`(${table.name})`),
+  ],
+);
+
+export type Org = typeof orgs.$inferSelect;
+export type NewOrg = typeof orgs.$inferInsert;
