7311c99651366cbf97afe7c17dc5ff52e1ea50a8
Maximilian Oertel <info@maximilianoertel.de>
2025-09-15 14:31:02 +0100

    Add users_classes table
    (cherry picked from commit 4528dd61b45aadaa18985deada5a43c76fa83e6e)
    (cherry picked from commit 16fdda275a7a044c6eeffa5c00fdab166e44ea96)

--- apps/backend/src/db/schema/core/relations.ts
+++ apps/backend/src/db/schema/core/relations.ts
@@ -1,9 +1,11 @@
 import { relations } from 'drizzle-orm';
+import { classes } from './classes';
 import { groups } from './groups';
-import { users } from './users';
 import { orgs } from './orgs';
-import { usersOrgs } from './users-orgs';
+import { users } from './users';
+import { usersClasses } from './users-classes';
 import { usersGroups } from './users-groups';
+import { usersOrgs } from './users-orgs';
 
 /**
  * User Relations
@@ -11,8 +13,28 @@ import { usersGroups } from './users-groups';
  * Drizzle relation definition for any membership of a user in a foreign table.
  */
 export const usersRelations = relations(users, ({ many }) => ({
-  org: many(usersOrgs),
-  group: many(usersGroups),
+  orgs: many(usersOrgs),
+  groups: many(usersGroups),
+  classes: many(usersClasses),
+}));
+
+/**
+ * Class Relations
+ *
+ * Drizzle relation definition for any membership of a class in a foreign table.
+ */
+export const classesRelations = relations(classes, ({ many }) => ({
+  users: many(usersClasses),
+}));
+
+/**
+ * User Classes relationship
+ *
+ * Drizzle relation definition for users <> users_classes <> classes, where users_classes is the join table.
+ */
+export const usersClassesRelations = relations(usersClasses, ({ one }) => ({
+  user: one(users, { fields: [usersClasses.userId], references: [users.id] }),
+  class: one(classes, { fields: [usersClasses.classId], references: [classes.id] }),
 }));
 
 /**
@@ -21,7 +43,7 @@ export const usersRelations = relations(users, ({ many }) => ({
  * Drizzle relation definition for any membership of a group in a foreign table.
  */
 export const groupsRelations = relations(groups, ({ many }) => ({
-  user: many(usersGroups),
+  users: many(usersGroups),
 }));
 
 /**
@@ -40,7 +62,7 @@ export const usersGroupsRelations = relations(usersGroups, ({ one }) => ({
  * Drizzle relation definition for any membership of an org in a foreign table.
  */
 export const orgsRelations = relations(orgs, ({ many }) => ({
-  user: many(usersOrgs),
+  users: many(usersOrgs),
 }));
 
 /**
