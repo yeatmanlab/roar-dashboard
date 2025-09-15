86bc079037111c9de5376392ee58f3bdcdbeda72
Maximilian Oertel <info@maximilianoertel.de>
2025-09-15 23:56:55 +0100

    Add families and users_families tables
    (cherry picked from commit bc2c7a5306f97b5313c6664e388d90c3d6718cb2)

---
+++ apps/backend/migrations/core/0019_add_family_tables.sql
@@ -0,0 +1,44 @@
+CREATE TABLE "app"."families" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"location_address_line1" text,
+	"location_address_line2" text,
+	"location_city" text,
+	"location_state_province" text,
+	"location_postal_code" text,
+	"location_country" varchar(2),
+	"location_lat_long" "point",
+	"rostering_ended" timestamp with time zone,
+	"updated_at" timestamp with time zone,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "app"."users_families" (
+	"user_id" uuid NOT NULL,
+	"family_id" uuid NOT NULL,
+	"role" "app"."user_role" NOT NULL,
+	"joined_on" timestamp with time zone DEFAULT now() NOT NULL,
+	"left_on" timestamp with time zone,
+	"updated_at" timestamp with time zone,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "users_families_pk" PRIMARY KEY("user_id","family_id")
+);
+--> statement-breakpoint
+ALTER TABLE "app"."users_families" ADD CONSTRAINT "users_families_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "app"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "app"."users_families" ADD CONSTRAINT "users_families_family_id_families_id_fk" FOREIGN KEY ("family_id") REFERENCES "app"."families"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "users_families_family_idx" ON "app"."users_families" USING btree ("family_id");
+
+-- Manual edit: 
+-- Add trigger to update families.updated_at
+DROP TRIGGER IF EXISTS families_set_updated_at ON app.families;
+CREATE TRIGGER families_set_updated_at
+BEFORE INSERT OR UPDATE ON app.families
+FOR EACH ROW
+EXECUTE FUNCTION app.set_updated_at();
+
+
+-- Add trigger to update users_families.updated_at
+DROP TRIGGER IF EXISTS users_families_set_updated_at ON app.users_families;
+CREATE TRIGGER users_families_set_updated_at
+BEFORE INSERT OR UPDATE ON app.users_families
+FOR EACH ROW
+EXECUTE FUNCTION app.set_updated_at();
---
+++ apps/backend/src/db/schema/core/users-families.ts
@@ -0,0 +1,52 @@
+import * as p from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+import type { AnyPgColumn } from 'drizzle-orm/pg-core';
+import { timestamps } from '../common';
+import { users } from './users';
+import { families } from './families';
+import { userRoleEnum } from '../enums';
+
+const db = p.pgSchema('app');
+
+/**
+ * Users Families Table
+ *
+ * Stores information about the membership of a user in a family. By definition, a single user can be a member of
+ * multiple families (depending on their user role) and a single family can have multiple users.
+ */
+export const usersFamilies = db.table(
+  'users_families',
+  {
+    userId: p
+      .uuid()
+      .notNull()
+      .references((): AnyPgColumn => users.id),
+    familyId: p
+      .uuid()
+      .notNull()
+      .references((): AnyPgColumn => families.id),
+
+    role: userRoleEnum().notNull(),
+
+    joinedOn: p
+      .timestamp({ withTimezone: true })
+      .default(sql`now()`)
+      .notNull(),
+    leftOn: p.timestamp({ withTimezone: true }),
+
+    ...timestamps,
+  },
+  (table) => [
+    // Primary key
+    p.primaryKey({ name: 'users_families_pk', columns: [table.userId, table.familyId] }),
+    +(
+      // Indexes
+      // - Lookups from either side
+      p.index('users_families_user_idx').on(table.userId)
+    ),
+    p.index('users_families_family_idx').on(table.familyId),
+  ],
+);
+
+export type UserFamily = typeof usersFamilies.$inferSelect;
+export type NewUserFamily = typeof usersFamilies.$inferInsert;
