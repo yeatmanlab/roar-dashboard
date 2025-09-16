5f1568339e0e7089e885b9c40ae0a59da1005a98
Maximilian Oertel <info@maximilianoertel.de>
2025-09-16 16:12:32 +0100

    Add run demographics table
    (cherry picked from commit e0b9b3c76f4d8b6f6c0b00d507122cf3b3b77425)

---
+++ apps/backend/migrations/core/0040_add_run_demographics_table.sql
@@ -0,0 +1,24 @@
+CREATE TABLE "app"."run_demographics" (
+	"run_id" uuid PRIMARY KEY NOT NULL,
+	"status_ell" boolean,
+	"status_frl" "app"."free_reduced_lunch_status",
+	"status_iep" boolean,
+	"gender" text,
+	"race" text,
+	"hispanic_ethnicity" boolean,
+	"home_language" text,
+	"age_in_months" integer,
+	"grade" "app"."grade",
+	"school_level" "app"."school_level" GENERATED ALWAYS AS (app.get_school_level_from_grade(grade)) STORED,
+	"updated_at" timestamp with time zone,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+-- Manual edit: 
+-- Add trigger to update run_demographics.updated_at
+DROP TRIGGER IF EXISTS run_demographics_set_updated_at ON app.run_demographics;
+CREATE TRIGGER run_demographics_set_updated_at
+BEFORE INSERT OR UPDATE ON app.run_demographics
+FOR EACH ROW
+EXECUTE FUNCTION app.set_updated_at();
