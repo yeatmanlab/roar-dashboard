6c0a992fdd59cee51fc4516646a33597164fc6ae
Lucas Song <lucas7song@gmail.com>
2025-09-24 11:42:51 -0700

    add es translations

--- apps/dashboard/vite.config.js
+++ apps/dashboard/vite.config.js
@@ -192,7 +192,13 @@ export default defineConfig(({ mode }) => {
       headers: {
         ...responseHeaders,
       },
-      https: false, // Disabled HTTPS for local development
+      https:
+        process.env.NODE_ENV === 'development'
+          ? {
+              key: fs.readFileSync(path.resolve(__dirname, '../../certs/roar-local.key')),
+              cert: fs.readFileSync(path.resolve(__dirname, '../../certs/roar-local.crt')),
+            }
+          : false,
     },
 
     preview: {
