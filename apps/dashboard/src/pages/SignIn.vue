3d9efb1ce003c037ddef9ccff594a9345e6c2313
Elijah Kelly <kellyel@stanford.edu>
2025-10-03 14:36:01 -0700

    Add NYCPS to sign in options on landing page

--- apps/dashboard/src/pages/SignIn.vue
+++ apps/dashboard/src/pages/SignIn.vue
@@ -53,6 +53,15 @@
               <img src="../assets/provider-classlink-logo.png" alt="The ClassLink Logo" class="flex mr-2 w-2" />
               <span>ClassLink</span>
             </PvButton>
+            <PvButton
+              class="flex p-1 mr-2 ml-2 w-3 text-black surface-0 border-black-alpha-10 justify-content-center hover:border-primary hover:surface-ground"
+              style="border-radius: 3rem; height: 3rem; color: black"
+              data-cy="sign-in__classlink-sso"
+              @click="authWithNYCPS"
+            >
+              <!-- <img src="../assets/provider-classlink-logo.png" alt="The ClassLink Logo" class="flex mr-2 w-2" /> -->
+              <span>NYCPS</span>
+            </PvButton>
           </div>
         </section>
         <!-- <section class="signin-option-container signin-option-providers">
