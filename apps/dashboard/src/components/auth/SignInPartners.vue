5af567721e3a02b2cf0f8a525dfcc62f37147667
emily-ejag <emily.artegar@gmail.com>
2025-10-08 13:36:18 -0700

    WIP making partners pages

---
+++ apps/dashboard/src/components/auth/SignInPartners.vue
@@ -0,0 +1,335 @@
+<template>
+  <div class="card">
+    <form class="p-fluid" @submit.prevent="handleFormSubmit(!v$.$invalid)">
+      <!-- Role switch (Student / Educator) -->
+      <div class="mt-2 mb-3">
+        <PvSelectButton
+          v-model="state.role"
+          :options="roles"
+          option-label="label"
+          option-value="value"
+          :allow-empty="false"
+          :class="['border-round w-full']"
+          fluid
+        >
+          <template #option="{ option }">
+            <div class="seg p-0">
+              <i :class="`${option.icon} mr-2`" aria-hidden="true"></i>
+              <span>{{ option.label }}</span>
+            </div>
+          </template>
+        </PvSelectButton>
+      </div>
+      <div class="mt-2 field">
+        <div class="p-input-icon-right">
+          <PvInputText
+            :id="$t('authSignIn.emailId')"
+            v-model="v$.email.$model"
+            :class="['w-full', { 'p-invalid': invalid }]"
+            aria-describedby="email-error"
+            :placeholder="$t('authSignIn.emailPlaceholder')"
+            data-cy="sign-in__username"
+            @keyup="checkForCapsLock"
+            @click="checkForCapsLock"
+          />
+        </div>
+        <small v-if="invalid" class="p-error">{{ $t('authSignIn.incorrectEmailOrPassword') }}</small>
+      </div>
+      <div class="mt-4 mb-5 field">
+        <div>
+          <!-- Email is currently being evaluated (loading state) -->
+          <span v-if="evaluatingEmail">
+            <PvSkeleton height="2.75rem" />
+          </span>
+          <!-- Email is entered, Password is desired -->
+          <div v-else-if="allowPassword && allowLink">
+            <PvPassword
+              :id="$t('authSignIn.passwordId')"
+              v-model="v$.password.$model"
+              :class="['w-full', { 'p-invalid': invalid }]"
+              :feedback="false"
+              :placeholder="$t('authSignIn.passwordPlaceholder')"
+              :input-props="{ autocomplete: 'current-password' }"
+              toggle-mask
+              show-icon="pi pi-eye-slash"
+              hide-icon="pi pi-eye"
+              data-cy="sign-in__password"
+              @keyup="checkForCapsLock"
+              @click="checkForCapsLock"
+            />
+            <small
+              class="text-link sign-in-method-link"
+              @click="
+                allowPassword = false;
+                state.usePassword = false;
+              "
+              >{{ $t('authSignIn.signInWithEmailLinkInstead') }}</small
+            >
+            <small class="text-link sign-in-method-link" @click="handleForgotPassword">Forgot password?</small>
+          </div>
+          <!-- Username is entered, Password is desired -->
+          <PvPassword
+            v-else-if="allowPassword"
+            :id="$t('authSignIn.passwordId')"
+            v-model="v$.password.$model"
+            :class="['w-full', { 'p-invalid': invalid }]"
+            toggle-mask
+            show-icon="pi pi-eye-slash"
+            hide-icon="pi pi-eye"
+            :feedback="false"
+            :placeholder="$t('authSignIn.passwordPlaceholder')"
+            data-cy="sign-in__password"
+            @keyup="checkForCapsLock"
+            @click="checkForCapsLock"
+          >
+            <template #header>
+              <h6>{{ $t('authSignIn.pickPassword') }}</h6>
+            </template>
+            <template #footer="sp">
+              {{ sp.level }}
+              <PvDivider />
+              <p class="mt-2">{{ $t('authSignIn.suggestions') }}</p>
+              <ul class="pl-2 mt-0 ml-2" style="line-height: 1.5">
+                <li>{{ $t('authSignIn.atLeastOneLowercase') }}</li>
+                <li>{{ $t('authSignIn.atLeastOneUppercase') }}</li>
+                <li>{{ $t('authSignIn.atLeastOneNumeric') }}</li>
+                <li>{{ $t('authSignIn.minimumCharacters') }}</li>
+              </ul>
+            </template>
+          </PvPassword>
+          <!-- Email is entered, MagicLink is desired login -->
+          <div v-else-if="allowLink">
+            <PvPassword :placeholder="$t('authSignIn.signInWithEmailLinkPlaceHolder')" class="w-full" disabled />
+            <small
+              class="text-link sign-in-method-link"
+              @click="
+                allowPassword = true;
+                state.usePassword = true;
+              "
+              >{{ $t('authSignIn.signInWithPasswordInstead') }}</small
+            >
+          </div>
+          <!-- Email is entered, however it is an invalid email (prevent login) -->
+          <div v-else>
+            <PvPassword
+              disabled
+              class="w-full text-red-600 p-invalid"
+              :placeholder="$t('authSignIn.invalidEmailPlaceholder')"
+            />
+          </div>
+          <div v-if="capsLockEnabled" class="mt-2 p-error">⇪ Caps Lock is on!</div>
+        </div>
+      </div>
+      <PvButton
+        type="submit"
+        class="flex p-3 mt-5 w-5 border-none border-round hover:bg-black-alpha-20"
+        :label="$t('authSignIn.buttonLabel') + ' &rarr;'"
+        data-cy="sign-in__submit"
+      />
+      <hr class="mt-5 opacity-20" />
+    </form>
+  </div>
+  <RoarModal
+    :is-enabled="forgotPasswordModalOpen"
+    title="Forgot Password"
+    subtitle="Enter your email to reset your password"
+    small
+    @modal-closed="forgotPasswordModalOpen = false"
+  >
+    <template #default>
+      <div class="flex flex-column">
+        <label>Email</label>
+        <PvInputText v-model="forgotEmail" />
+      </div>
+    </template>
+    <template #footer>
+      <PvButton
+        tabindex="0"
+        class="p-2 bg-white border-none border-round text-primary hover:surface-200"
+        text
+        label="Cancel"
+        outlined
+        @click="closeForgotPasswordModal"
+      ></PvButton>
+      <PvButton
+        tabindex="0"
+        class="p-2 text-white border-none border-round bg-primary hover:surface-400"
+        label="Send Reset Email"
+        @click="sendResetEmail"
+      ></PvButton>
+    </template>
+  </RoarModal>
+</template>
+
+<script setup>
+import { reactive, ref, watch } from 'vue';
+import { storeToRefs } from 'pinia';
+import { required, requiredUnless } from '@vuelidate/validators';
+import { useVuelidate } from '@vuelidate/core';
+import _debounce from 'lodash/debounce';
+import PvButton from 'primevue/button';
+import PvDivider from 'primevue/divider';
+import PvInputText from 'primevue/inputtext';
+import PvPassword from 'primevue/password';
+import PvSkeleton from 'primevue/skeleton';
+import { useAuthStore } from '@/store/auth';
+import RoarModal from '../modals/RoarModal.vue';
+
+import PvSelectButton from 'primevue/selectbutton'; // add this import
+
+// options for the SelectButton
+const roles = [
+  { label: 'Student', value: 'student', icon: 'pi pi-graduation-cap' },
+  { label: 'Educator', value: 'educator', icon: 'pi pi-user' },
+];
+
+const authStore = useAuthStore();
+const { roarfirekit } = storeToRefs(authStore);
+
+const emit = defineEmits(['submit', 'update:email']);
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  invalid: { type: Boolean, required: false, default: false },
+});
+
+const state = reactive({
+  email: '',
+  password: '',
+  useLink: false,
+  usePassword: true,
+  role: 'student', // default selection
+});
+
+const rules = {
+  email: { required },
+  password: {
+    requiredIf: requiredUnless(() => state.useLink),
+  },
+};
+const submitted = ref(false);
+const v$ = useVuelidate(rules, state);
+const capsLockEnabled = ref(false);
+const forgotPasswordModalOpen = ref(false);
+
+const handleFormSubmit = (isFormValid) => {
+  submitted.value = true;
+  if (!isFormValid) {
+    return;
+  }
+  emit('submit', state);
+};
+
+const isValidEmail = (email) => {
+  var re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
+const evaluatingEmail = ref(false);
+const allowPassword = ref(true);
+const allowLink = ref(true);
+
+const validateRoarEmail = _debounce(
+  async (email) => {
+    await roarfirekit.value.isEmailAvailable(email).then(async (emailAvail) => {
+      if (emailAvail) {
+        console.log(`Email ${email} is available`);
+        allowPassword.value = true;
+        allowLink.value = false;
+      } else {
+        if (roarfirekit.value.isRoarAuthEmail(email)) {
+          // Roar auth email are made up, so sign-in link is not allowed.
+          allowLink.value = false;
+          allowPassword.value = true;
+        } else {
+          allowLink.value = true;
+          allowPassword.value = true;
+        }
+      }
+      state.useLink = allowLink.value;
+      evaluatingEmail.value = false;
+    });
+  },
+  250,
+  { maxWait: 1000 },
+);
+
+function checkForCapsLock(e) {
+  // Make sure the event is a keyboard event.
+  // Using password autofill will trigger a regular
+  //   event which does not have a getModifierState method.
+  if (e instanceof KeyboardEvent) {
+    capsLockEnabled.value = e.getModifierState('CapsLock');
+  }
+}
+
+const forgotEmail = ref('');
+function handleForgotPassword() {
+  console.log('Opening modal for forgot password');
+  forgotPasswordModalOpen.value = true;
+  // e.preventDefault();
+}
+function closeForgotPasswordModal() {
+  forgotPasswordModalOpen.value = false;
+  forgotEmail.value = '';
+}
+function sendResetEmail() {
+  console.log('Submitting forgot password with email', forgotEmail.value);
+  roarfirekit.value.sendPasswordResetEmail(forgotEmail.value);
+  closeForgotPasswordModal();
+}
+
+watch(
+  () => state.email,
+  async (email) => {
+    emit('update:email', email);
+    if (isValidEmail(email)) {
+      evaluatingEmail.value = true;
+      validateRoarEmail(email);
+    } else {
+      // In this case, assume that the input is a username
+      // Password is allowed. Sign-in link is not allowed.
+      allowPassword.value = true;
+      allowLink.value = false;
+      state.useLink = allowLink.value;
+    }
+  },
+);
+</script>
+<style scoped>
+.submit-button {
+  margin-top: 0.5rem;
+  display: flex;
+  background-color: #e5e5e5;
+  color: black;
+  border: none;
+  width: 11.75rem;
+}
+
+.submit-button:hover {
+  background-color: #b7b5b5;
+  color: black;
+}
+.text-link {
+  cursor: pointer;
+  color: var(--text-color-secondary);
+  font-weight: bold;
+  text-decoration: underline;
+}
+
+.text-link:hover {
+  color: var(--primary-color-text);
+}
+.sign-in-method-link {
+  margin-top: 0.5rem;
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+}
+.p-togglebutton {
+  background: var(--primary-color) !important;
+}
+.p-togglebutton-content {
+  background: var(--primary-color) !important;
+}
+</style>
---
+++ apps/dashboard/src/pages/SignInPartners.vue
@@ -0,0 +1,381 @@
+<template>
+  <div v-if="spinner" class="loading-blur">
+    <AppSpinner />
+  </div>
+  <div id="signin-container">
+    <section id="signin">
+      <header>
+        <div class="signin-logo">
+          <ROARLogoShort />
+        </div>
+      </header>
+      <h1>{{ $t('pageSignIn.welcome') }}</h1>
+      <h1>PARTNERS</h1>
+      <section class="signin-options">
+        <section class="signin-option-container signin-option-userpass">
+          <h4 class="signin-option-title">{{ $t('pageSignIn.login') }}</h4>
+          <div id="languageSelect" class="flex m-4 justify-content-center">
+            <LanguageSelector class="w-7" />
+          </div>
+          <SignInPartners :invalid="incorrect" @submit="authWithEmail" @update:email="email = $event" />
+        </section>
+        <section class="flex w-full flex-column">
+          <h4
+            class="flex flex-wrap-reverse mt-1 mb-3 font-bold align-content-center justify-content-center text-md text-500"
+          >
+            {{ $t('pageSignIn.loginWith') }}
+          </h4>
+          <div class="flex flex-row w-full align-content-center justify-content-center">
+            <PvButton
+              label="Sign in with Google"
+              class="flex p-1 mr-2 ml-2 w-3 text-center text-black surface-0 border-black-alpha-10 justify-content-center hover:border-primary hover:surface-ground"
+              style="border-radius: 3rem; height: 3rem; color: black"
+              data-cy="sign-in__google-sso"
+              @click="authWithGoogle"
+            >
+              <img src="../assets/provider-google-logo.svg" alt="The Google Logo" class="flex mr-2 w-2" />
+              <span>Google</span>
+            </PvButton>
+            <PvButton
+              class="flex p-1 mr-2 ml-2 w-3 surface-0 border-black-alpha-10 justify-content-center hover:border-primary hover:surface-ground"
+              style="border-radius: 3rem; height: 3rem; color: black"
+              data-cy="sign-in__clever-sso"
+              @click="authWithClever"
+            >
+              <img src="../assets/provider-clever-logo.svg" alt="The Clever Logo" class="flex mr-2 w-2" />
+              <span>Clever</span>
+            </PvButton>
+            <PvButton
+              class="flex p-1 mr-2 ml-2 w-3 text-black surface-0 border-black-alpha-10 justify-content-center hover:border-primary hover:surface-ground"
+              style="border-radius: 3rem; height: 3rem; color: black"
+              data-cy="sign-in__classlink-sso"
+              @click="authWithClassLink"
+            >
+              <img src="../assets/provider-classlink-logo.png" alt="The ClassLink Logo" class="flex mr-2 w-2" />
+              <span>ClassLink</span>
+            </PvButton>
+            <PvButton
+              class="flex p-1 mr-2 ml-2 w-3 text-black surface-0 border-black-alpha-10 justify-content-center hover:border-primary hover:surface-ground"
+              style="border-radius: 3rem; height: 3rem; color: black"
+              data-cy="sign-in__classlink-sso"
+              @click="authWithNYCPS"
+            >
+              <!-- NYCPS Logo needs to be slightly wider as it is not a square -->
+              <img
+                src="../assets/provider-nycps-logo.jpg"
+                alt="The NYC Public Schools Logo"
+                class="flex mr-2"
+                style="width: 21%"
+              />
+              <span>NYCPS</span>
+            </PvButton>
+          </div>
+        </section>
+        <!-- <section class="signin-option-container signin-option-providers">
+            <div class="flex flex-row w-full justify-content-center">
+              <p class="text-sm signin-option-title">Don't have an account yet?</p>
+              <PvButton label="Register" class="w-3 signin-button" @click="router.push({ name: 'Register' })" />
+            </div>
+          </section> -->
+      </section>
+      <footer style="display: none">
+        <!-- TODO: figure out a link for this -->
+        <a href="#trouble">{{ $t('pageSignIn.havingTrouble') }}</a>
+      </footer>
+    </section>
+  </div>
+  <RoarModal
+    :is-enabled="warningModalOpen"
+    title="Email is already associated with an account"
+    subtitle=""
+    icon="pi-exclamation-triangle"
+    small
+    @modal-closed="handleWarningModalClose"
+  >
+    <template #default>
+      The email <span class="font-bold">{{ email }}</span> is already in use using
+      {{ displaySignInMethods.slice(0, -1).join(', ') + ' or ' + displaySignInMethods.slice(-1) }}. If this is you,
+      click to sign in below.
+      <div class="flex gap-2 my-2 align-items-center flex-column">
+        <div v-if="signInMethods.includes('google')" class="flex">
+          <PvButton
+            label="Sign in with Google"
+            class="flex p-1 mr-1 text-center surface-0 border-black-alpha-10 justify-content-center hover:border-primary hover:surface-ground"
+            style="border-radius: 3rem; height: 3rem"
+            @click="authWithGoogle"
+          >
+            <img src="../assets/provider-google-logo.svg" alt="The Google Logo" class="flex mr-2 w-2" />
+            <span>Google</span>
+          </PvButton>
+        </div>
+        <div v-if="signInMethods.includes(AUTH_SSO_PROVIDERS.CLEVER)">
+          <PvButton
+            class="flex p-1 mr-1 surface-0 border-black-alpha-10 justify-content-center hover:border-primary hover:surface-ground"
+            style="border-radius: 3rem; height: 3rem"
+            @click="authWithClever"
+          >
+            <img src="../assets/provider-clever-logo.svg" alt="The Clever Logo" class="flex mr-2 w-2" />
+            <span>Clever</span>
+          </PvButton>
+        </div>
+        <div v-if="signInMethods.includes(AUTH_SSO_PROVIDERS.CLASSLINK)">
+          <PvButton
+            class="flex p-1 mr-1 surface-0 border-black-alpha-10 justify-content-center hover:border-primary hover:surface-ground"
+            style="border-radius: 3rem; height: 3rem"
+            @click="authWithClassLink"
+          >
+            <img src="../assets/provider-classlink-logo.png" alt="The ClassLink Logo" class="flex mr-2 w-2" />
+            <span>ClassLink</span>
+          </PvButton>
+        </div>
+        <div v-if="signInMethods.includes('password')" class="flex flex-row gap-2">
+          <PvPassword
+            v-model="modalPassword"
+            placeholder="Password"
+            :input-props="{ autocomplete: 'current-password' }"
+            :feedback="false"
+          />
+          <PvButton
+            class="flex p-3 border-none border-round hover:bg-black-alpha-20"
+            :label="$t('authSignIn.buttonLabel') + ' &rarr;'"
+            @click="authWithEmail({ email, password: modalPassword, useLink: false, usePassword: true })"
+          />
+        </div>
+      </div>
+      You will then be directed to your profile page where you can link different authentication providers.
+    </template>
+    <template #footer>
+      <PvButton
+        tabindex="0"
+        class="p-2 bg-white border-none border-round text-primary hover:surface-200"
+        text
+        label="Back to Sign In"
+        outlined
+        @click="handleWarningModalClose"
+      ></PvButton>
+    </template>
+  </RoarModal>
+</template>
+
+<script setup>
+import { onMounted, ref, toRaw, onBeforeUnmount, computed } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useRoute, useRouter } from 'vue-router';
+import { setUser } from '@sentry/vue';
+import PvButton from 'primevue/button';
+import PvPassword from 'primevue/password';
+import ROARLogoShort from '@/assets/RoarLogo-Short.vue';
+import { useAuthStore } from '@/store/auth';
+import { isMobileBrowser } from '@/helpers';
+import { redirectSignInPath } from '@/helpers/redirectSignInPath';
+import { fetchDocById } from '@/helpers/query/utils';
+import { AUTH_SSO_PROVIDERS } from '@/constants/auth';
+import { APP_ROUTES } from '@/constants/routes';
+import RoarModal from '@/components/modals/RoarModal.vue';
+import SignInPartners from '@/components/auth/SignInPartners.vue';
+import LanguageSelector from '@/components/LanguageSelector.vue';
+
+const incorrect = ref(false);
+const authStore = useAuthStore();
+const router = useRouter();
+const route = useRoute();
+
+const { spinner, ssoProvider, routeToProfile, roarfirekit } = storeToRefs(authStore);
+const warningModalOpen = ref(false);
+
+authStore.$subscribe(() => {
+  if (authStore.uid) {
+    if (ssoProvider.value) {
+      router.push({ path: APP_ROUTES.SSO });
+    } else if (routeToProfile.value) {
+      router.push({ path: APP_ROUTES.ACCOUNT_PROFILE });
+    } else {
+      router.push({ path: redirectSignInPath(route) });
+    }
+  }
+});
+
+const authWithGoogle = () => {
+  if (isMobileBrowser()) {
+    authStore.signInWithGoogleRedirect();
+  } else {
+    authStore
+      .signInWithGooglePopup()
+      .then(async () => {
+        if (authStore.uid) {
+          const userClaims = await fetchDocById('userClaims', authStore.uid);
+          authStore.userClaims = userClaims;
+        }
+        if (authStore.roarUid) {
+          const userData = await fetchDocById('users', authStore.roarUid);
+          authStore.userData = userData;
+          setUser({ id: authStore.roarUid, userType: userData.userType });
+        }
+      })
+      .catch((e) => {
+        const errorCode = e.code;
+        if (errorCode === 'auth/email-already-in-use') {
+          // User tried to register with an email that is already linked to a firebase account.
+          openWarningModal();
+          spinner.value = false;
+        } else {
+          spinner.value = false;
+        }
+      });
+
+    spinner.value = true;
+  }
+};
+
+const modalPassword = ref('');
+
+const authWithClever = () => {
+  if (process.env.NODE_ENV === 'development' && !window.Cypress) {
+    authStore.signInWithCleverPopup().then(async () => {
+      if (authStore.uid) {
+        const userClaims = await fetchDocById('userClaims', authStore.uid);
+        authStore.userClaims = userClaims;
+      }
+      if (authStore.roarUid) {
+        const userData = await fetchDocById('users', authStore.roarUid);
+        authStore.userData = userData;
+        setUser({ id: authStore.roarUid, userType: userData.userType });
+      }
+    });
+  } else {
+    authStore.signInWithCleverRedirect();
+  }
+  spinner.value = true;
+};
+
+const authWithClassLink = () => {
+  if (isMobileBrowser()) {
+    authStore.signInWithClassLinkRedirect();
+    spinner.value = true;
+  } else {
+    authStore.signInWithClassLinkRedirect();
+    spinner.value = true;
+  }
+};
+
+const authWithNYCPS = () => {
+  if (process.env.NODE_ENV === 'development' && !window.Cypress) {
+    authStore.signInWithNYCPSPopup().then(async () => {
+      if (authStore.uid) {
+        const userClaims = await fetchDocById('userClaims', authStore.uid);
+        authStore.userClaims = userClaims;
+      }
+      if (authStore.roarUid) {
+        const userData = await fetchDocById('users', authStore.roarUid);
+        authStore.userData = userData;
+        setUser({ id: authStore.roarUid, userType: userData.userType });
+      }
+    });
+  } else {
+    authStore.signInWithNYCPSRedirect();
+  }
+  spinner.value = true;
+};
+
+const authWithEmail = (state) => {
+  // If username is supplied instead of email
+  // turn it into our internal auth email
+  incorrect.value = false;
+  let creds = toRaw(state);
+  if (creds.useLink && !creds.usePassword) {
+    authStore.initiateLoginWithEmailLink({ email: creds.email }).then(() => {
+      router.push({ name: 'AuthEmailSent' });
+    });
+  } else {
+    if (!creds.email.includes('@')) {
+      creds.email = `${creds.email}@roar-auth.com`;
+    }
+
+    authStore
+      .logInWithEmailAndPassword(creds)
+      .then(async () => {
+        if (authStore.uid) {
+          const userClaims = await fetchDocById('userClaims', authStore.uid);
+          authStore.userClaims = userClaims;
+        }
+        if (authStore.roarUid) {
+          const userData = await fetchDocById('users', authStore.roarUid);
+          authStore.userData = userData;
+          setUser({ id: authStore.roarUid, userType: userData.userType });
+        }
+
+        spinner.value = true;
+      })
+      .catch((e) => {
+        incorrect.value = true;
+        if (['auth/user-not-found', 'auth/wrong-password'].includes(e.code)) {
+          return;
+        } else {
+          throw e;
+        }
+      });
+  }
+};
+
+const handleWarningModalClose = () => {
+  authStore.routeToProfile = true;
+  warningModalOpen.value = false;
+};
+
+const email = ref('');
+
+const signInMethods = ref([]);
+
+const openWarningModal = async () => {
+  signInMethods.value = await roarfirekit.value.fetchEmailAuthMethods(email.value);
+  warningModalOpen.value = true;
+};
+
+const displaySignInMethods = computed(() => {
+  return signInMethods.value.map((method) => {
+    if (method === 'password') return 'Password';
+    if (method === AUTH_SSO_PROVIDERS.GOOGLE) return 'Google';
+    if (method === AUTH_SSO_PROVIDERS.CLEVER) return 'Clever';
+    if (method === AUTH_SSO_PROVIDERS.CLASSLINK) return 'ClassLink';
+  });
+});
+
+onMounted(() => {
+  document.body.classList.add('page-signin');
+  if (authStore.cleverOAuthRequested) {
+    authStore.cleverOAuthRequested = false;
+    authWithClever();
+  }
+  if (authStore.classLinkOAuthRequested) {
+    authStore.classLinkOAuthRequested = false;
+    authWithClassLink();
+  }
+  if (authStore.nycpsOAuthRequested) {
+    authStore.nycpsOAuthRequested = false;
+    authWithNYCPS();
+  }
+});
+
+onBeforeUnmount(() => {
+  document.body.classList.remove('page-signin');
+});
+</script>
+
+<style scoped>
+.loading-blur {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  background-color: rgba(255, 255, 255, 0.7);
+  padding-top: 21vh;
+}
+input.p-inputtext.p-component.p-password-input {
+  width: 100%;
+}
+div#password {
+  width: 100%;
+}
+</style>
