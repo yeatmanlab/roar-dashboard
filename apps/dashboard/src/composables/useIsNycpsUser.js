0c959198f9049343ff780184f137fe55aab3e3d0
Elijah Kelly <kellyel@stanford.edu>
2025-10-08 21:12:40 -0700

    Add docstring to Nycps composable

--- apps/dashboard/src/composables/useIsNycpsUser.js
+++ apps/dashboard/src/composables/useIsNycpsUser.js
@@ -1,5 +1,11 @@
 import { computed } from 'vue';
 
+/**
+ * Determine if the user is affiliated with NYCPS.
+ *
+ * @param {Object} userData - The user data object.
+ * @returns {boolean} Whether the user is affiliated with NYCPS.
+ */
 export default function useIsNycpsUser(userData) {
   const isNycpsUser = computed(() => {
     if (!userData?.value) return false;
