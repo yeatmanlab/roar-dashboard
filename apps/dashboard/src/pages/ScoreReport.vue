09014b1f98f6b1dec8772e2d6a2503553db66962
Maximilian Oertel <info@maximilianoertel.de>
2025-10-16 12:17:25 +0100

    Remove old score report page (#1413)

--- apps/dashboard/src/pages/ScoreReport.vue
+++ apps/dashboard/src/pages/ScoreReport.vue
@@ -1551,7 +1551,7 @@ const scoreReportColumns = computed(() => {
   tableColumns.push({
     header: 'Report',
     link: true,
-    routeName: 'StudentReport',
+    routeName: 'StudentScoreReport',
     routeTooltip: 'Student Score Report',
     routeIcon: 'pi pi-chart-bar border-none text-primary hover:text-white',
     sort: false,
