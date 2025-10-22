c8cdcdf1987471fc88318339256c72e61edf3ba1
emily-ejag <emily.artegar@gmail.com>
2025-10-21 17:41:07 -0700

    Language selector changes

--- apps/dashboard/src/components/LanguageSelector.vue
+++ apps/dashboard/src/components/LanguageSelector.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="language-picker">
-    <a
-      href="#"
-      class="text-400 w-full inline-block text-left text-sm pt-2 rounded-md underline hover:text-primary"
-      aria-label="Change language"
-      @click.prevent="toggleMenu($event)"
-    >
-      <span>{{ currentLanguageLabel }}</span>
-      <i class="pi pi-chevron-down text-sm pl-2" />
-    </a>
+    <template v-if="props.styleProp === 'home'">
+      <PvButton
+        v-tooltip.top="$t('navBar.changeLanguage')"
+        icon="pi pi-globe"
+        class="m-0 p-0 text-primary bg-gray-100 border-none border-round cursor-pointer h-3rem w-3rem text-sm hover:bg-red-900 hover:text-white border-style"
+        aria-label="Change language"
+        @click="toggleMenu($event)"
+      />
+    </template>
+
+    <template v-else>
+      <a
+        href="#"
+        class="text-400 w-full inline-block text-left text-sm pt-2 rounded-md underline hover:text-primary"
+        aria-label="Change language"
+        @click.prevent="toggleMenu($event)"
+      >
+        <span>{{ currentLanguageLabel }}</span>
+        <i class="pi pi-chevron-down text-sm pl-2" />
+      </a>
+    </template>
 
     <PvMenu ref="menu" :model="menuItems" popup />
   </div>
@@ -17,8 +29,16 @@
 <script setup>
 import { ref, computed } from 'vue';
 import PvMenu from 'primevue/menu';
+import PvButton from 'primevue/button';
 import { useI18n } from 'vue-i18n';
 import { languageOptions } from '@/translations/i18n.js';
+
+const props = defineProps({
+  styleProp: {
+    type: String,
+    default: 'default', // or 'home'
+  },
+});
 
 const { locale } = useI18n({ useScope: 'global' });
 const menu = ref(null);
@@ -46,3 +66,9 @@ function toggleMenu(event) {
   menu.value.toggle(event);
 }
 </script>
+
+<style scoped>
+.border-style {
+  outline: 1.2px solid rgba(0, 0, 0, 0.1);
+}
+</style>
