22e19eab500032c1b99d19ef7cb65d2d02c90bd9
Maximilian Oertel <info@maximilianoertel.de>
2026-01-02 23:38:42 +0000

    Extract and enhance global error handler

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,8 +1,8 @@
 import express from 'express';
-import type { NextFunction, Request, Response } from 'express';
-import { isHttpError } from 'http-errors';
+import type { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { registerAllRoutes } from './routes';
+import { errorHandler } from './error-handler';
 import { API_ERROR_CODES } from './constants/api-error-codes';
 
 const app = express();
@@ -22,25 +22,6 @@ app.use((_req: Request, res: Response) => {
 });
 
 // Handle errors
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: unknown, _req: Request, res: Response, _next: NextFunction) => {
-  // Convert HTTP errors created within the application to JSON responses
-  if (isHttpError(err)) {
-    return res.status(err.statusCode).json({
-      error: {
-        message: err.message,
-        code: err.code,
-      },
-    });
-  }
-
-  // Fallback for unexpected errors
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-    error: {
-      message: 'An unexpected error occurred.',
-      code: API_ERROR_CODES.INTERNAL,
-    },
-  });
-});
+app.use(errorHandler);
 
 export default app;
