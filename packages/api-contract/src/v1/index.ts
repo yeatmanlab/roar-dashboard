f71f65d1d83e05478d71af7f3657a7bd99cc83c6
Maximilian Oertel <info@maximilianoertel.de>
2026-01-03 13:40:04 +0000

    Add administrations api-contract

--- packages/api-contract/src/v1/index.ts
+++ packages/api-contract/src/v1/index.ts
@@ -1,5 +1,6 @@
 import { initContract } from '@ts-rest/core';
 import { MeContract } from './me/index';
+import { AdministrationsContract } from './administrations/index';
 export * from './response';
 export * from './common/index';
 
@@ -7,6 +8,8 @@ const c = initContract();
 
 export const ApiContractV1 = c.router({
   me: MeContract,
+  administrations: AdministrationsContract,
 });
 
 export * from './me/index';
+export * from './administrations/index';
