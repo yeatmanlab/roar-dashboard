89014fbc24adc7a9050604fa06a83256a4cf55bc
Lucas Song <lucas7song@gmail.com>
2026-02-09 10:33:57 -0800

    remove addinteraction alias

--- packages/assessment-sdk/src/types/add-interaction.ts
+++ packages/assessment-sdk/src/types/add-interaction.ts
@@ -2,14 +2,8 @@
  * Firekit-compatible input for appkit.addInteraction
  * Matches: addInteraction(interaction: InteractionEvent)
  */
-export type AddInteractionInput = InteractionEvent;
+export type AddInteractionInput = {
+  [key: string]: unknown;
+};
 
 export type AddInteractionOutput = void;
-
-/**
- * InteractionEvent type from Firekit
- * Represents a user interaction during an assessment
- */
-export interface InteractionEvent {
-  [key: string]: unknown;
-}
