814422362f3026980cb3b74bf38efd599f8c7516
Amy Yeung <ayeung21@stanford.edu>
2026-03-04 14:22:45 -0800

    Fix ClassUsersResponseSchema

--- packages/api-contract/src/v1/classes/schema.ts
+++ packages/api-contract/src/v1/classes/schema.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
 import { UsersResponseSchema } from '../common/user';
 
-export const ClassUsersResponseSchema = UsersResponseSchema.extend({
+export const ClassUsersResponseSchema = z.object({
   classId: z.string().uuid(),
+  users: z.array(UsersResponseSchema),
 });
 
 export type ClassUsersResponse = z.infer<typeof ClassUsersResponseSchema>;
