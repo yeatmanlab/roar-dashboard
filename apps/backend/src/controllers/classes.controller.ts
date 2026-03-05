72cff1469ae307b577364f6f873147b1721ee156
Amy Yeung <ayeung21@stanford.edu>
2026-03-05 08:52:44 -0800

    Implement basic class controller

---
+++ apps/backend/src/controllers/classes.controller.ts
@@ -0,0 +1,31 @@
+import { StatusCodes } from 'http-status-codes';
+import { ClassService } from '../services/class/class.service';
+import type { UsersQuery } from '@roar-dashboard/api-contract';
+import type { AuthContext } from '../types/auth-context';
+
+const classService = ClassService();
+
+/**
+ * TODO:
+ * - handleSubResourceResponse - extract to separate reusable function for other endpoints?
+ * - What fields to return, sort
+ * - classId in items? { classId: string, users: User[]} - for other endpoints {schoolId: string} etc?
+ */
+export const ClassesController = {
+  listUsers: async (authContext: AuthContext, classId: string, query: UsersQuery) => {
+    try {
+      const result = await classService.listUsers(authContext, classId, query);
+      return {
+        statusCode: StatusCodes.OK,
+        body: result,
+      };
+    } catch (error) {
+      return {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        body: {
+          error,
+        },
+      };
+    }
+  },
+};
