cd9d6933e00daae872ff13adbb071f9bc8cdf10a
Kyle <ksmontville@gmail.com>
2026-04-29 13:47:42 -0700

    clarify userFamilyRole and remove type assertions, update users schema to discriminate between org membership and family membership

--- apps/backend/src/enums/user-role.enum.ts
+++ apps/backend/src/enums/user-role.enum.ts
@@ -1,10 +1,18 @@
+import { userRoleEnum, userFamilyRoleEnum } from '../db/schema/enums';
+import { pgEnumToConst } from './utils';
+
 /**
  * User Role Enum
  *
  * Enumerates the available roles a user can be assigned.
  */
-import { userRoleEnum } from '../db/schema/enums';
-import { pgEnumToConst } from './utils';
-
 export const UserRole = pgEnumToConst(userRoleEnum);
 export type UserRole = (typeof userRoleEnum.enumValues)[number];
+
+/**
+ * User Family Role Enum
+ *
+ * Enumerates the available roles a user can be assigned in a family context.
+ */
+export const UserFamilyRole = pgEnumToConst(userFamilyRoleEnum);
+export type UserFamilyRole = (typeof userFamilyRoleEnum.enumValues)[number];
