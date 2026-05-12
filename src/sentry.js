46b8a1060bf14d704017078510686d66e8838c49
Kyle <ksmontville@gmail.com>
2026-05-12 10:51:12 -0700

    Squashed 'apps/assessments/roar-pa/' content from commit 32412eb0f
    git-subtree-dir: apps/assessments/roar-pa
    git-subtree-split: 32412eb0fc02f49c4c4001f7618b3666a7b73e9c

---
+++ src/sentry.js
@@ -0,0 +1,43 @@
+import * as Sentry from '@sentry/browser';
+import { captureConsoleIntegration, contextLinesIntegration, extraErrorDataIntegration } from '@sentry/integrations';
+
+const game = 'roar-phoneme';
+const gameShortened = 'pa';
+const regexRoarApp = new RegExp(`^https:\\/\\/${game}--pr\\d+-\\w+\\.web\\.app\\/`);
+const regexRoarStaging = new RegExp(
+  `^https:\\/\\/roar-staging\\.web\\.app\\/game\\/${gameShortened}|https:\\/\\/roar-staging--pr\\d+-\\w+\\.web\\.app\\/`,
+);
+
+export function initSentry() {
+  Sentry.init({
+    dsn: 'https://26a28547199e506fbdb3f1bd7b419da7@o4505913837420544.ingest.sentry.io/4505915574517760',
+    integrations: [
+      Sentry.replayIntegration({
+        maskAllText: true,
+        maskAllInputs: true,
+      }),
+      Sentry.browserTracingIntegration(),
+      captureConsoleIntegration({
+        levels: ['warning', 'error', 'debug', 'assert'],
+      }),
+      contextLinesIntegration(),
+      extraErrorDataIntegration(),
+    ],
+    attachStacktrace: true,
+    // Performance Monitoring
+    tracesSampleRate: 0.2, // Capture 20% of the transactions
+    tracePropagationTargets: [
+      regexRoarApp,
+      regexRoarStaging,
+      `https://roar.education/game/${gameShortened}`,
+      'localhost',
+    ],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+
+    beforeSend(event) {
+      return event;
+    },
+  });
+}
