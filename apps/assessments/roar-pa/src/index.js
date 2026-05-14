a33d703224541754914c831ee29b1585e5385ba9
Kyle <ksmontville@gmail.com>
2026-05-14 13:33:52 -0700

    implement firekit compat

--- apps/assessments/roar-pa/src/index.js
+++ apps/assessments/roar-pa/src/index.js
@@ -1,24 +1,23 @@
 /* eslint-disable import/extensions */
+import { startRun, abortRun } from '@yeatmanlab/assessment-sdk/compat/firekit';
 import { initConfig } from './experiment/config/config';
 import { buildExperiment } from './experiment/experiment';
 import './experiment/styles/roar.css';
-import { waitFor } from './experiment/experimentHelpers';
 import { initSentry } from './sentry';
 
 class RoarPA {
-  constructor(firekit, gameParams, userParams, displayElement) {
+  constructor(gameParams, userParams, displayElement) {
     // TODO: Add validation of params so that if any are missing, we throw an error
     this.gameParams = gameParams;
     this.userParams = userParams;
-    this.firekit = firekit;
     this.displayElement = displayElement;
     this.jsPsych = null;
   }
 
   async init() {
     initSentry();
-    await this.firekit.startRun();
-    const config = await initConfig(this.firekit, this.gameParams, this.userParams, this.displayElement);
+    await startRun();
+    const config = await initConfig(this.gameParams, this.userParams, this.displayElement);
     return buildExperiment(config);
   }
 
@@ -26,12 +25,10 @@ class RoarPA {
     const { jsPsych, timeline } = await this.init();
     this.jsPsych = jsPsych;
     this.jsPsych.run(timeline);
-
-    await waitFor(() => this.firekit.run.completed === true);
   }
 
   async abort() {
-    this.firekit.abortRun();
+    abortRun();
     document.querySelectorAll('audio').forEach((el) => el.pause());
     if (this.jsPsych) {
       this.jsPsych.endExperiment();
