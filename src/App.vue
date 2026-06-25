b4edad374059c306c150cb41388bcf670f928320
Kyle <ksmontville@gmail.com>
2026-06-25 11:59:04 -0700

    Squashed 'apps/assessments/roar-survey/' content from commit dfce2aa5d
    git-subtree-dir: apps/assessments/roar-survey
    git-subtree-split: dfce2aa5d9532fe790eda580843769e6a0bedd5b

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import SurveyRunner from './components/SurveyRunner.vue'
+import { firekit } from './main'
+
+defineProps({
+  surveyData: {
+    type: Object,
+    default: () => ({})
+  }
+})
+</script>
+
+<template>
+  <SurveyRunner :appkit="firekit" standalone :surveyData="surveyData"/>
+</template>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
