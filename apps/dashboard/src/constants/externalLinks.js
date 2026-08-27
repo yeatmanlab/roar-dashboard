89c2eb1cd9dae7e3fc9def8324b6dba47b294cbe
Elijah Kelly <kellyel@stanford.edu>
2026-08-27 12:21:54 -0700

    Update target of Report an Issue button

---
+++ apps/dashboard/src/constants/externalLinks.js
@@ -0,0 +1,6 @@
+/**
+ * External links used throughout the dashboard.
+ */
+
+// @TODO Replace with the real external ticket-creation URL once it is finalized.
+export const REPORT_ISSUE_URL = 'https://roareducation.zendesk.com/hc/en-us/requests/new';
