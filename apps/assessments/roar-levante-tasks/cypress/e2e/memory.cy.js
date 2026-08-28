f05a846463a45f6bc6f84f96106a53441e8be6f4
Amy Yeung <ayeung21@stanford.edu>
2026-08-28 14:00:57 -0400

    Update memory game cypress test - buttons are not enabled until audio is finished

--- apps/assessments/roar-levante-tasks/cypress/e2e/memory.cy.js
+++ apps/assessments/roar-levante-tasks/cypress/e2e/memory.cy.js
@@ -21,7 +21,7 @@ function handleInstructions() {
     const corsiBlocks = content.find('.jspsych-corsi-block');
 
     if (corsiBlocks.length === 0) {
-      cy.contains('OK').click();
+      cy.contains('OK').should('not.be.disabled').click();
     }
   });
   return;
